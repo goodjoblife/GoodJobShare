d1db8319d8b7138812ef196fbfb5b074825f2a3e
xxhomey19 <xxhomey19@gmail.com>
2017-01-19 23:06:39 +0800

    add webpack

---
+++ webpack.config.base.js
@@ -0,0 +1,36 @@
+const autoprefixer = require('autoprefixer');
+const nested = require('postcss-nested');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const simpleVars = require('postcss-simple-vars');
+
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        loaders: ['url-loader?limit=20000'],
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      inject: true,
+    }),
+  ],
+
+  postcss: [
+    autoprefixer({
+      browsers: ['last 2 version', 'ie >= 10'],
+    }),
+    simpleVars,
+    nested,
+  ],
+};
---
+++ webpack.config.prod.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const config = require('./webpack.config.base');
+
+module.exports = merge.smart(config, {
+  entry: './src/index',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle-[chunkhash].js',
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        screw_ie8: true,
+        warnings: false,
+      },
+    }),
+    new ExtractTextPlugin('style-[contenthash].css', { allChunks: true }),
+  ],
+  module: {
+    loaders: [
+      {
+        test: /^((?!\.module).)*\.css$/,
+        loader: ExtractTextPlugin.extract(
+          'style-loader',
+          'css-loader'
+        ),
+      },
+      {
+        test: /\.module\.css$/,
+        loader: ExtractTextPlugin.extract(
+          'style-loader',
+          'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader' // eslint-disable-line max-len
+        ),
+      },
+    ],
+  },
+});
