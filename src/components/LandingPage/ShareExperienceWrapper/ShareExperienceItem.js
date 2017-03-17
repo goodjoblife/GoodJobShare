5f2f308c96fd97a9069c22b7e21f7eb78340dcda
tin <tin@gcim.com.tw>
2017-03-17 23:47:57 +0800

    hover should use css, change style order

--- src/components/LandingPage/ShareExperienceWrapper/ShareExperienceItem.js
+++ src/components/LandingPage/ShareExperienceWrapper/ShareExperienceItem.js
@@ -6,33 +6,13 @@ import homeBanner from '../../images/home-banner.png';
 import styles from './ShareExperienceItem.module.css';
 
 class ShareExperienceItem extends React.Component {
-  state = {
-    isHover: false,
-  };
-
-  handleHover(isHover) {
-    this.setState({
-      isHover,
-    });
-  }
-
   render() {
-    const { isHover } = this.state;
-
     return (
-      <div
-        className={styles.share_experience_item}
-        onMouseOver={() => this.handleHover(true)}
-        onMouseEnter={() => this.handleHover(true)}
-        onMouseLeave={() => this.handleHover(false)}
-      >
+      <div className={styles.share_experience_item}>
         <Link to="/" className={styles.image}>
           <img src={homeBanner} alt="" />
         </Link>
-        <button
-          className={cn(styles.link,
-          isHover ? styles.isHover : null)}
-        >
+        <button className={styles.link}>
           <span>連結連結連結連結</span>
         </button>
       </div>
