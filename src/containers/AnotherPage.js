63196ea2aa7bd953a9f950e9b86a7bbd943a1d4d
chejen <jkopre.qek@gmail.com>
2017-04-05 23:21:00 +0800

    added a few font classes

--- src/containers/AnotherPage.js
+++ src/containers/AnotherPage.js
@@ -4,6 +4,38 @@ import React from 'react';
 const AnotherPage = () => (
   <div>
     <h1>Hello World</h1>
+    <div className="heading-l">熱門分享 (heading-l)</div>
+    <div className="heading-l">熱門分享 (heading-l)</div>
+
+    <div className="heading-m">日月光半導體面試經驗分享 (heading-m)</div>
+    <div className="heading-m">日月光半導體面試經驗分享 (heading-m)</div>
+
+    <div className="subheading-l">日月光半導體面試經驗分享 (subheading-l)</div>
+    <div className="subheading-l">日月光半導體面試經驗分享 (subheading-l)</div>
+
+    <div className="subheading-m">在日月光待了6年 (subheading-m)</div>
+    <div className="subheading-m">在日月光待了6年 (subheading-m)</div>
+
+    <div className="p-l">留下工時或薪資 (p-l)</div>
+    <div className="p-l">留下工時或薪資 (p-l)</div>
+
+    <div className="p-m">第一間面試我的公司 (p-m)</div>
+    <div className="p-m">第一間面試我的公司 (p-m)</div>
+
+    <div className="p-s">第一間面試我的公司 (p-s)</div>
+    <div className="p-s">第一間面試我的公司 (p-s)</div>
+
+    <div className="p-l-bold">留下工時或薪資 (p-l-bold)</div>
+    <div className="p-l-bold">留下工時或薪資 (p-l-bold)</div>
+
+    <div className="p-m-bold">第一間面試我的公司 (p-m-bold)</div>
+    <div className="p-m-bold">第一間面試我的公司 (p-m-bold)</div>
+
+    <div className="p-s-bold">第一間面試我的公司 (p-s-bold)</div>
+    <div className="p-s-bold">第一間面試我的公司 (p-s-bold)</div>
+
+    <div className="form-label">公司/單位 或 統一編號 (form-label)</div>
+    <div className="form-label">公司/單位 或 統一編號 (form-label)</div>
   </div>
 );
 
