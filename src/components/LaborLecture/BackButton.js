4080822a0356fa2e31255d47ed938d99ee6f60cc
peteranny <peteranny2@gmail.com>
2017-04-07 00:34:07 +0800

    link on back-button

--- src/components/LaborLecture/BackButton.js
+++ src/components/LaborLecture/BackButton.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { IndexLink } from 'react-router';
 import styles from './Lecture.module.css';
 
 const BackButton = () => (
-  <div className={styles.back}>
-    返回
-  </div>
+  <IndexLink to="/labor-lecture">
+    <div className={styles.back}>
+      返回
+    </div>
+  </IndexLink>
 );
 
 export default BackButton;
