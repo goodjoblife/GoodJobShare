c89c2355d64644e7988ff46e7998d2884db4e326
peteranny <peteranny2@gmail.com>
2017-04-06 23:59:30 +0800

    labor-lecture fetch contentful on mounted

--- src/components/LaborLecture/index.js
+++ src/components/LaborLecture/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Helmet from 'react-helmet';
+import * as contentful from 'contentful';
 
 import Desktop from './Desktop';
 import BackButton from './BackButton';
@@ -10,41 +11,73 @@ import Footer from './Footer';
 import Seperator from './Seperator';
 import styles from './Lecture.module.css';
 
-const Lecture = ({ title, content }) => (
-  <main>
-    <Helmet title="勞動小教室" />
-    <Desktop>
-      <BackButton />
-      <h3 className={styles.header}>{title}</h3>
-      <Content>
-        {
-          content.map(({ type, data }, i) => (
-            <p key={i} className={styles.content_para}>
-              {
-                (
-                  type === 'text' && (
-                    (data === '' && '\u00A0') || data
-                  )
-                ) || (
-                  type === 'image' && (
-                    <img
-                      className={styles.content_image}
-                      role="presentation"
-                    />
-                  )
-                )
-              }
-            </p>
-          ))
-        }
-        <Feedback />
-        <Seperator />
-        <Pagers />
-      </Content>
-      <Footer />
-    </Desktop>
-  </main>
-);
+class Lecture extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      content: [],
+    };
+  }
+
+  componentDidMount() {
+    const SPACE_ID = 'siutzcg6nl4g';
+    const ACCESS_TOKEN =
+      'ef08dee7812e4bbd8c9856776426ade160ea263c2972d19b381b29aae95e4c61';
+
+    const client = contentful.createClient({
+      space: SPACE_ID,
+      accessToken: ACCESS_TOKEN,
+    });
+
+    client.getEntry('jn1NJzf0swcwoGwwwwyKi').then(
+      ({ fields: { title, content } }) => {
+        this.setState({
+          title,
+          content,
+        });
+      }
+    ).catch(() => {});
+  }
+
+  render() {
+    return (
+      <main>
+        <Helmet title="勞動小教室" />
+        <Desktop>
+          <BackButton />
+          <h3 className={styles.header}>{this.state.title}</h3>
+          <Content>
+            {
+              this.state.content.map(({ type, data }, i) => (
+                <p key={i} className={styles.content_para}>
+                  {
+                    (
+                      type === 'text' && (
+                        (data === '' && '\u00A0') || data
+                      )
+                    ) || (
+                      type === 'image' && (
+                        <img
+                          className={styles.content_image}
+                          role="presentation"
+                        />
+                      )
+                    )
+                  }
+                </p>
+              ))
+            }
+            <Feedback />
+            <Seperator />
+            <Pagers />
+          </Content>
+          <Footer />
+        </Desktop>
+      </main>
+    );
+  }
+}
 
 Lecture.propTypes = {
   title: React.PropTypes.string,
