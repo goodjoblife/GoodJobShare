1c418a81f0bd047e8f2bdbca38a69b8615c663bf
chejen <jkopre.qek@gmail.com>
2017-04-08 13:25:38 +0800

    Font class (#31)
    * added a few font classes
    
    * adjusted font classes to camel case (#29)

--- src/containers/AnotherPage.js
+++ src/containers/AnotherPage.js
@@ -4,6 +4,38 @@ import React from 'react';
 const AnotherPage = () => (
   <div>
     <h1>Hello World</h1>
+    <div className="headingL">熱門分享 (headingL)</div>
+    <div className="headingL">熱門分享 (headingL)</div>
+
+    <div className="headingM">日月光半導體面試經驗分享 (headingM)</div>
+    <div className="headingM">日月光半導體面試經驗分享 (headingM)</div>
+
+    <div className="subheadingL">日月光半導體面試經驗分享 (subheadingL)</div>
+    <div className="subheadingL">日月光半導體面試經驗分享 (subheadingL)</div>
+
+    <div className="subheadingM">在日月光待了6年 (subheadingM)</div>
+    <div className="subheadingM">在日月光待了6年 (subheadingM)</div>
+
+    <div className="pL">留下工時或薪資 (pL)</div>
+    <div className="pL">留下工時或薪資 (pL)</div>
+
+    <div className="pM">第一間面試我的公司 (pM)</div>
+    <div className="pM">第一間面試我的公司 (pM)</div>
+
+    <div className="pS">第一間面試我的公司 (pS)</div>
+    <div className="pS">第一間面試我的公司 (pS)</div>
+
+    <div className="pLBold">留下工時或薪資 (pLBold)</div>
+    <div className="pLBold">留下工時或薪資 (pLBold)</div>
+
+    <div className="pMBold">第一間面試我的公司 (pMBold)</div>
+    <div className="pMBold">第一間面試我的公司 (pMBold)</div>
+
+    <div className="pSBold">第一間面試我的公司 (pSBold)</div>
+    <div className="pSBold">第一間面試我的公司 (pSBold)</div>
+
+    <div className="formLabel">公司/單位 或 統一編號 (formLabel)</div>
+    <div className="formLabel">公司/單位 或 統一編號 (formLabel)</div>
   </div>
 );
 
