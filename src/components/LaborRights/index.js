62e2a53ab340f35559ed6e9999da40b87159086b
peteranny <peteranny2@gmail.com>
2017-04-14 02:53:36 +0800

    simplify componentdidmount in labor-rights

--- src/components/LaborRights/index.js
+++ src/components/LaborRights/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Helmet from 'react-helmet';
-import * as contentful from 'contentful';
 
 import Container from './Container';
 import BackButton from './BackButton';
@@ -13,32 +12,8 @@ import Seperator from './Seperator';
 import styles from './LaborRights.module.css';
 
 class Lecture extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      title: '',
-      content: [],
-    };
-  }
-
   componentDidMount() {
-    const SPACE_ID = 'siutzcg6nl4g';
-    const ACCESS_TOKEN =
-      'ef08dee7812e4bbd8c9856776426ade160ea263c2972d19b381b29aae95e4c61';
-
-    const client = contentful.createClient({
-      space: SPACE_ID,
-      accessToken: ACCESS_TOKEN,
-    });
-
-    client.getEntry(this.props.id).then(
-      ({ fields: { title, content } }) => {
-        this.setState({
-          title,
-          content,
-        });
-      }
-    ).catch(() => {});
+    this.props.download();
   }
 
   render() {
@@ -48,11 +23,11 @@ class Lecture extends React.Component {
         <Container>
           <BackButton />
           <h3 className={`headingL ${styles.header}`}>
-            {this.state.title}
+            {this.props.item.title}
           </h3>
           <Content>
             {
-              this.state.content.map(({ type, data }, i) => (
+              this.props.item.content.map(({ type, data }, i) => (
                 <p key={i} className={`pLBold ${styles.para}`}>
                   {
                     (
--- src/containers/LaborRights.js
+++ src/containers/LaborRights.js
@@ -1,9 +1,21 @@
 import { connect } from 'react-redux';
-
+import { loadLaborRights } from '../actions/laborRights';
 import LaborRights from '../components/LaborRights';
 
-const mapStateToProps = (state, { params: { id } }) => ({
-  id,
-});
-
 export default connect(mapStateToProps)(LaborRights);
+
+export default connect(
+  (state, { params: { id }}) => {
+    const items = state.laborRights.toJS().filter(({ id:_id }) => id==_id);
+    const item = items.length == 1? items[0]: null;
+    return {
+      id,
+      item,
+    }
+  },
+  dispatch => ({
+    download: () => {
+      dispatch(loadLaborRights());
+    },
+  })
+)(LaborRights);
