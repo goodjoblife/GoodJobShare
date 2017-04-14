a176eb18e785cf9f4dc951385b1e9a89c7b1f3a3
Wendell Liu <cuk.bas@gmail.com>
2017-04-14 19:30:27 +0800

    add textInput

--- src/components/ShareExperience/InterviewForm/InterviewInfo.js
+++ src/components/ShareExperience/InterviewForm/InterviewInfo.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import TextInput from 'common/form/TextInput';
+
 import styles from './InterviewInfo.module.css';
 
 import InputTitle from '../common/InputTitle';
 
+import {
+  checkWordingLength,
+} from '../utils';
+
 class InterviewInfo extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.handleCompany = this.handleCompany.bind(this);
+
+    this.state = {
+      companyName: '',
+    };
+  }
+
+  handleCompany(companyName) {
+    this.setState({
+      companyName,
+    });
+  }
+
   render() {
     return (
       <div
@@ -22,6 +44,13 @@ class InterviewInfo extends React.PureComponent {
           <InputTitle
             text="公司/單位 或 統一編號"
             must
+          />
+          <TextInput
+            placeholder="ＯＯ 股份有限公司"
+            value={this.state.companyName}
+            onChange={e => { this.handleCompany(e.target.value); }}
+            isWarning={!checkWordingLength(10)(this.state.companyName)}
+            warningWording="請輸入10個字以內"
           />
         </div>
       </div>
---
+++ src/components/common/form/TextInput.js
@@ -0,0 +1,41 @@
+import React, { PropTypes } from 'react';
+
+import styles from './TextInput.module.css';
+
+const TextInput = (
+  { value, placeholder, onChange, isWarning, warningWording }
+) => (
+  <div>
+    <input
+      type="text"
+      placeholder={placeholder}
+      className={isWarning ? styles.warning : styles.container}
+      value={value}
+      onChange={onChange}
+    />
+    {
+      warningWording ?
+        <p
+          className={`
+            pS ${styles.warning__text} ${isWarning ? styles.isWarning : ''}
+          `}
+        >
+          { warningWording }
+        </p> : null
+    }
+  </div>
+);
+
+
+TextInput.propTypes = {
+  value: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+  ]),
+  placeholder: PropTypes.string,
+  onChange: PropTypes.func,
+  isWarning: PropTypes.bool,
+  warningWording: PropTypes.string,
+};
+
+export default TextInput;
