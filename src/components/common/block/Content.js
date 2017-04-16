039987fd1a146e2c8bd2c936f95104f848da85e8
chejen <jkopre.qek@gmail.com>
2017-04-16 10:07:06 +0800

    added some styles for Block

--- src/components/common/block/Content.js
+++ src/components/common/block/Content.js
@@ -1,7 +1,9 @@
 import React, { PropTypes } from 'react';
 
+import styles from './Content.module.css';
+
 const Content = ({ children }) => (
-  <div>
+  <div className={styles.content}>
     {children}
   </div>
 );
--- src/components/common/block/Preview.js
+++ src/components/common/block/Preview.js
@@ -2,22 +2,41 @@ import React, { PropTypes } from 'react';
 
 import styles from './Preview.module.css';
 
-// const test = () => {
-//   console.log('test');
-// };
+class Preview extends React.Component {
+  static propTypes = {
+    children: PropTypes.node,
+  }
 
-const Preview = ({ children }) => (
-  <button
-    className={styles.preview} onClick={() => {
-      console.log('why');
-    }}
-  >
-    Preview: {children}
-  </button>
-);
+  // static defaultProps = {
+  //   closeIn: 3000,
+  // }
 
-Preview.propTypes = {
-  children: PropTypes.node,
-};
+  constructor() {
+    super();
+    this.state = {
+      isExpanded: false,
+    };
+  }
+
+  render() {
+    const cnMore = this.state.isExpanded
+      ? `${styles.more} ${styles.expanded}`
+      : `${styles.more}`;
+    return (
+      <button
+        className={styles.preview} onClick={() => {
+          this.setState({
+            isExpanded: !this.state.isExpanded,
+          });
+        }}
+      >
+        <div className={styles.heading}>
+          {this.props.children}
+        </div>
+        <div className={cnMore} />
+      </button>
+    );
+  }
+}
 
 export default Preview;
