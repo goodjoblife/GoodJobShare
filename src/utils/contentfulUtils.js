e56b9543eda072ad466ecb1fba01fb5ba186110b
peteranny <peteranny2@gmail.com>
2017-04-16 22:17:01 +0800

    global client

--- src/utils/contentfulUtils.js
+++ src/utils/contentfulUtils.js
@@ -1,18 +1,13 @@
 import * as contentful from 'contentful';
 
-function initClient() {
-  const accessToken = process.env.CONTENTFUL_TOKEN;
-  const space = process.env.CONTENTFUL_LABOR_RIGHTS_SPACE;
+const accessToken = process.env.CONTENTFUL_TOKEN;
+const space = process.env.CONTENTFUL_LABOR_RIGHTS_SPACE;
 
-  return contentful.createClient({
-    accessToken,
-    space,
-  });
-}
+const client = contentful.createClient({
+  accessToken,
+  space,
+});
 
 export default {
-  fetchLaborRights: () => {
-    const client = initClient();
-    return client.getEntries();
-  },
+  fetchLaborRights: () => client.getEntries(),
 };
