441f184dc90877e7561e72a6abe7ee9bc6652dcc
peteranny <peteranny2@gmail.com>
2017-04-17 01:42:11 +0800

    seperate labor-rights id-list and data-map

--- src/containers/LaborRightsMenu.js
+++ src/containers/LaborRightsMenu.js
@@ -1,11 +1,22 @@
+import { List } from 'immutable'; // TODO
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as actionCreators from '../actions/laborRights';
 import LaborRightsMenu from '../components/LaborRightsMenu';
 
 export default connect(
-  state => ({
-    items: state.laborRights.valueSeq(),
-  }),
+  state => {
+    if (state.laborRights.size === 0) { // TODO
+      console.log('ohoh');
+      return {
+        items: List(),
+      };
+    }
+    return {
+      items: state.laborRights.get('idList').map(id =>
+        state.laborRights.getIn(['dataMapById', id])
+      ),
+    };
+  },
   dispatch => bindActionCreators(actionCreators, dispatch),
 )(LaborRightsMenu);
--- src/reducers/laborRights.js
+++ src/reducers/laborRights.js
@@ -1,15 +1,22 @@
-import { Map, fromJS } from 'immutable';
+import { List, Map } from 'immutable';
 import createReducer from 'utils/createReducer';
 import { SET_LABOR_RIGHTS } from '../actions/laborRights';
 
-const preloadedState = Map();
+const preloadedState = Map({
+  idList: List(),
+  dataMapById: Map(),
+});
 
 export default createReducer(preloadedState, {
   [SET_LABOR_RIGHTS]: (state, { items }) => {
-    const map = items.reduce((partialMap, item) => ({
+    const idList = List(items.map(item => item.id));
+    const dataMapById = Map(items.reduce((partialMap, item) => ({
       ...partialMap,
-      [item.id]: item,
-    }), {});
-    return fromJS(map);
+      [item.id]: Map(item),
+    }), {}));
+    return Map({
+      idList,
+      dataMapById,
+    });
   },
 });
