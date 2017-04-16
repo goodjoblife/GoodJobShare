30b2104dac9f04231501900f58c421a63190f7fe
peteranny <peteranny2@gmail.com>
2017-04-17 02:10:32 +0800

    update labor-rights-single according to id-list and data-map

--- src/containers/LaborRightsSingle.js
+++ src/containers/LaborRightsSingle.js
@@ -5,14 +5,20 @@ import LaborRightsSingle from '../components/LaborRightsSingle';
 
 export default connect(
   (state, { params: { id } }) => {
-    const title = state.laborRights.getIn([id, 'title']);
-    const content = state.laborRights.getIn([id, 'content']);
-    const ids = state.laborRights.keySeq();
+    if (state.laborRights.size === 0) { // TODO
+      return {
+        title: '',
+        content: '',
+      };
+    }
+    const title = state.laborRights.getIn(['dataMapById', id, 'title']);
+    const content = state.laborRights.getIn(['dataMapById', id, 'content']);
+    const ids = state.laborRights.get('idList');
     const index = ids.indexOf(id);
     const prevId = index > 0 ? ids.get(index - 1) : undefined;
     const nextId = index < ids.count() - 1 ? ids.get(index + 1) : undefined;
-    const prev = state.laborRights.get(prevId);
-    const next = state.laborRights.get(nextId);
+    const prev = state.laborRights.getIn(['dataMapById', prevId]);
+    const next = state.laborRights.getIn(['dataMapById', nextId]);
     return {
       title,
       content,
