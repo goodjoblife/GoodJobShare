57b5853cca0cfd8fa30f42751ea79b17c20330ec
peteranny <peteranny2@gmail.com>
2017-04-17 23:31:29 +0800

    apply contentful data to helmet, etc

--- src/components/LaborRightsSingle/index.js
+++ src/components/LaborRightsSingle/index.js
@@ -5,6 +5,7 @@ import ImmutablePropTypes from 'react-immutable-proptypes';
 import Container from './Container';
 import BackButton from './BackButton';
 import Body from './Body';
+import HidingText from './HidingText';
 import MarkdownParser from './MarkdownParser';
 import Feedback from './Feedback';
 import Pagers from './Pagers';
@@ -19,16 +20,35 @@ class LaborRightsSingle extends React.Component {
   }
 
   render() {
+    const {
+      title,
+      description,
+      content,
+      coverUrl,
+    } = this.props.item ? this.props.item.toJS() : {};
+    const {
+      seoTitle = title,
+      seoDescription,
+      hidingText,
+    } = this.props.item ? this.props.item.toJS() : {};
     return (
       <main>
-        <Helmet title="勞動小教室" />
+        <Helmet
+          title={`${seoTitle} | 工時薪資透明化運動`}
+          meta={[
+            { name: 'description', content: seoDescription },
+            { property: 'og:image', content: coverUrl },
+          ]}
+        />
         <Container>
           <BackButton />
-          <h3 className={`headingL ${styles.header}`}>
-            {this.props.title}
-          </h3>
+          <h1 className={`headingL ${styles.header}`}>
+            {title}
+          </h1>
           <Body>
-            <MarkdownParser content={this.props.content} />
+            <HidingText content={hidingText} />
+            <MarkdownParser content={description} />
+            <MarkdownParser content={content} />
             <Feedback />
             <Seperator />
             <Pagers
@@ -44,8 +64,7 @@ class LaborRightsSingle extends React.Component {
 }
 
 LaborRightsSingle.propTypes = {
-  title: React.PropTypes.string.isRequired,
-  content: React.PropTypes.string.isRequired,
+  item: ImmutablePropTypes.map,
   prev: ImmutablePropTypes.map,
   next: ImmutablePropTypes.map,
   loadLaborRights: React.PropTypes.func.isRequired,
