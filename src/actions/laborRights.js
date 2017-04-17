e56cb4c647e4f504e12c5e1d8de73c6b1d633c82
peteranny <peteranny2@gmail.com>
2017-04-18 00:05:38 +0800

    fetching status on labor-rights

--- src/actions/laborRights.js
+++ src/actions/laborRights.js
@@ -1,13 +1,27 @@
 import contentfulUtils from '../utils/contentfulUtils';
 
-export const SET_LABOR_RIGHTS = 'SET_LABOR_RIGHTS';
+export const FETCH_LABOR_RIGHTS = '@@laborRights/FETCH_LABOR_RIGHTS';
+export const SET_LABOR_RIGHTS = '@@laborRights/SET_LABOR_RIGHTS';
+
+export const status = {
+  UNFETCHED: 'UNFETCHED',
+  FETCHED: 'FETCHED',
+  FETCHING: 'FETCHING',
+};
 
 const setLaborRights = items => ({
   type: SET_LABOR_RIGHTS,
   items,
 });
 
-export const loadLaborRights = () => dispatch =>
+const fetchLaborRights = (done, err) => ({
+  type: FETCH_LABOR_RIGHTS,
+  done,
+  err,
+});
+
+export const loadLaborRights = () => dispatch => {
+  dispatch(fetchLaborRights(false));
   contentfulUtils.fetchLaborRights().then(({ items }) =>
     items.map(({
       sys: { id },
@@ -32,4 +46,8 @@ export const loadLaborRights = () => dispatch =>
     }))
   ).then(items => {
     dispatch(setLaborRights(items));
-  }).catch(() => {});
+    dispatch(fetchLaborRights(true));
+  }).catch(err => {
+    dispatch(fetchLaborRights(true, err));
+  });
+};
--- src/reducers/laborRights.js
+++ src/reducers/laborRights.js
@@ -1,20 +1,29 @@
 import { List, Map } from 'immutable';
 import createReducer from 'utils/createReducer';
-import { SET_LABOR_RIGHTS } from '../actions/laborRights';
+import {
+  FETCH_LABOR_RIGHTS,
+  SET_LABOR_RIGHTS,
+  status,
+} from '../actions/laborRights';
 
 const preloadedState = Map({
   idList: List(),
   dataMapById: Map(),
+  status: status.UNFETCHED,
 });
 
 export default createReducer(preloadedState, {
+  [FETCH_LABOR_RIGHTS]: (state, { done, err }) =>
+    state
+      .set('status', done ? status.FETCHED : status.FETCHING)
+      .set('error', err),
   [SET_LABOR_RIGHTS]: (state, { items }) => {
     const idList = List(items.map(item => item.id));
     const dataMapById = Map(items.reduce((partialMap, item) => ({
       ...partialMap,
       [item.id]: Map(item),
     }), {}));
-    return Map({
+    return state.merge({
       idList,
       dataMapById,
     });
