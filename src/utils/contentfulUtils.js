2a88dfc8009c49ec4871e495b07ca7b72515c9ca
Yen-chi Chen <mark86092@gmail.com>
2017-04-22 01:12:23 +0800

    Contentful: 加上開發用的 token, space
    如果想要指定自己的空間，設定下面兩個環境變數
    
    * CONTENTFUL_TOKEN
    * CONTENTFUL_LABOR_RIGHTS_SPACE

--- src/utils/contentfulUtils.js
+++ src/utils/contentfulUtils.js
@@ -1,7 +1,8 @@
 import * as contentful from 'contentful';
 
-const accessToken = process.env.CONTENTFUL_TOKEN;
-const space = process.env.CONTENTFUL_LABOR_RIGHTS_SPACE;
+const accessToken = process.env.CONTENTFUL_TOKEN
+  || '92728b01e5f9536eccb6d4caff0ea92561bc1c742faea65732690f38f5840426';
+const space = process.env.CONTENTFUL_LABOR_RIGHTS_SPACE || 'rbeukh8vheqy';
 
 const client = contentful.createClient({
   accessToken,
