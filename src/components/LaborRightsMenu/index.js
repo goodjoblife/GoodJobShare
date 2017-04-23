81f9ac9289b929ea6d679e2fd82976c876ca09aa
Tin <wutingy@gmail.com>
2017-04-23 21:28:34 +0800

    labor-rights-menu style

--- src/components/LaborRightsMenu/index.js
+++ src/components/LaborRightsMenu/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import cn from 'classnames';
 import Helmet from 'react-helmet';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 
 import Columns from '../common/Columns';
 import Container from './Container';
 import LaborRightsEntry from './LaborRightsEntry';
+import styles from './LaborRightsMenu.module.css';
 
 class LaborRightsMenu extends React.Component {
   componentDidMount() {
@@ -13,10 +15,10 @@ class LaborRightsMenu extends React.Component {
 
   render() {
     return (
-      <main>
+      <main className="wrapperL">
         <Helmet title="勞動小教室" />
         <Container>
-          <p className="headingL">勞動小教室</p>
+          <h1 className={cn(styles.heading, 'headingL')}>勞動小教室</h1>
           <Columns
             Item={LaborRightsEntry}
             items={this.props.items.toJS()}
