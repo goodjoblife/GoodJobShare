5de1f17c0bb3659872a2d4af90e02b1e2f95f126
peteranny <peteranny2@gmail.com>
2017-04-24 01:38:56 +0800

    move item resolution from actions to contentful-utils

--- src/actions/laborRights.js
+++ src/actions/laborRights.js
@@ -13,19 +13,7 @@ const setAllLaborRightsMeta = items => ({
 export const loadAllLaborRightsMeta = () => dispatch =>
   contentfulUtils.fetchAllLaborRightsMeta().then(items => {
     dispatch(setAllLaborRightsMeta(items));
-  }).then(({ items }) =>
-    items.map(({
-      sys: { id },
-      fields: {
-        title,
-        coverImage: { fields: { file: { url: coverUrl } } },
-      },
-    }) => ({
-      id,
-      title,
-      coverUrl,
-    }))
-  ).catch(() => {});
+  }).catch(() => {});
 
 const setSingleLaborRights = (id, item) => ({
   type: SET_SINGLE_LABOR_RIGHTS,
@@ -38,26 +26,4 @@ export const loadSingleLaborRights = laborRightsId => dispatch =>
     if (items.length === 1) {
       dispatch(setSingleLaborRights(laborRightsId, items.pop()));
     }
-  }).then(({ items }) =>
-    items.map(({
-      sys: { id },
-      fields: {
-        title,
-        description,
-        content,
-        coverImage: { fields: { file: { url: coverUrl } } },
-        seoTitle,
-        seoDescription,
-        hidingText,
-      },
-    }) => ({
-      id,
-      title,
-      description,
-      content,
-      coverUrl,
-      seoTitle,
-      seoDescription,
-      hidingText,
-    }))
-  ).catch(() => {});
+  }).catch(() => {});
--- src/utils/contentfulUtils.js
+++ src/utils/contentfulUtils.js
@@ -13,9 +13,43 @@ export default {
     order: 'sys.createdAt',
     content_type: 'labor-lecture',
     select: 'fields.title,fields.coverImage',
-  }),
+  }).then(({ items }) =>
+    items.map(({
+      sys: { id },
+      fields: {
+        title,
+        coverImage: { fields: { file: { url: coverUrl } } },
+      },
+    }) => ({
+      id,
+      title,
+      coverUrl,
+    }))
+  ),
   fetchSingleLaborRights: laborRightsId => client.getEntries({
     content_type: 'labor-lecture',
     'sys.id': laborRightsId,
-  }),
+  }).then(({ items }) =>
+    items.map(({
+      sys: { id },
+      fields: {
+        title,
+        description,
+        content,
+        coverImage: { fields: { file: { url: coverUrl } } },
+        seoTitle,
+        seoDescription,
+        hidingText,
+      },
+    }) => ({
+      id,
+      title,
+      description,
+      content,
+      coverUrl,
+      seoTitle,
+      seoDescription,
+      hidingText,
+    }))
+  ),
 };
