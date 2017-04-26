fdb8dff9d85daf77b46b48d1917861b7f4666291
Yen-chi Chen <mark86092@gmail.com>
2017-04-27 00:38:19 +0800

    將 #67 的 style 修改分離成兩部份
    1. 一部份是網站本體
    2. 一部份是給新舊網站綜合使用

--- src/components/Layout/Header/SiteMenu.js
+++ src/components/Layout/Header/SiteMenu.js
@@ -14,10 +14,8 @@ Item.propTypes = {
 
 const SiteMenu = () => (
   <ul className={styles.menu}>
-    <Item to="/#section-form" text="開始參與" />
-    <Item to="/show" text="查看薪時" />
-    <Item to="/#section-faq" text="常見問答" />
-    <Item to="/about" text="關於我們" />
+    <Item to="/" text="薪資工時" />
+    <Item to="/experiences/search" text="面試‧工作經驗" />
     <Item to="/labor-rights" text="勞動小教室" />
   </ul>
 );
