5e5f066811dbf91d1c8bbbc10371f9013a66b513
peter <peter@droide-MBP.wireless.droitp.localnet>
2017-04-26 09:19:44 +0800

    fix typo

--- src/reducers/LaborRightsMenu.js
+++ src/reducers/LaborRightsMenu.js
@@ -3,7 +3,7 @@ import createReducer from 'utils/createReducer';
 import {
   SET_ALL_LABOR_RIGHTS_META_STATUS,
   SET_ALL_LABOR_RIGHTS_META,
-} from '../actions/LaborRightsSingle';
+} from '../actions/LaborRightsMenu';
 import status from '../constants/status';
 
 const preloadedState = Map({
@@ -14,7 +14,7 @@ const preloadedState = Map({
 export default createReducer(preloadedState, {
   [SET_ALL_LABOR_RIGHTS_META_STATUS]: (state, { nextStatus, err }) =>
     state
-      .set('metaLisStatus', nextStatus)
+      .set('metaListStatus', nextStatus)
       .set('metaListError', err),
   [SET_ALL_LABOR_RIGHTS_META]: (state, { metaList }) =>
     state.set('metaList', metaList),
