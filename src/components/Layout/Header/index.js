664d905df6e86174df9f8f7aa75a274ee2e6b108
peteranny <peteranny2@gmail.com>
2017-05-06 01:06:21 +0800

    maintain is-nav-open in Header.state

--- src/components/Layout/Header/index.js
+++ src/components/Layout/Header/index.js
@@ -5,22 +5,40 @@ import styles from './Header.module.css';
 import HeaderButton from './HeaderButton';
 import SiteMenu from './SiteMenu';
 
-const Header = ({ isNavOpen, toggleHeaderButton }) => (
-  <header className={styles.header}>
-    <div className={cn(styles.inner, 'wrapperL')}>
-      <HeaderButton
-        isNavOpen={isNavOpen}
-        toggle={toggleHeaderButton}
-      />
-      <a href="/" className={styles.logo} title="goodjob 工時薪資透明化運動">
-        <i.GjLogo />
-      </a>
-      <nav className={cn(styles.nav, { [styles.isNavOpen]: isNavOpen })}>
-        <SiteMenu />
-      </nav>
-    </div>
-  </header>
-);
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isNavOpen: false,
+    };
+    this.toggleNav = this.toggleNav.bind(this);
+  }
+
+  toggleNav() {
+    this.setState({
+      isNavOpen: !this.state.isNavOpen,
+    });
+  }
+
+  render() {
+    return (
+      <header className={styles.header}>
+        <div className={cn(styles.inner, 'wrapperL')}>
+          <HeaderButton
+            isNavOpen={this.state.isNavOpen}
+            toggle={this.toggleNav}
+          />
+          <a href="/" className={styles.logo} title="goodjob 工時薪資透明化運動">
+            <i.GjLogo />
+          </a>
+          <nav className={cn(styles.nav, { [styles.isNavOpen]: this.state.isNavOpen })}>
+            <SiteMenu />
+          </nav>
+        </div>
+      </header>
+    );
+  }
+}
 
 Header.propTypes = {
   isNavOpen: PropTypes.bool.isRequired,
