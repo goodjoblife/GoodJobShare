eb476c6ec85eff9f909e3f90459fe0e6687dcd6a
peteranny <peteranny2@gmail.com>
2017-05-05 21:50:18 +0800

    put on mobile header

---
+++ src/components/Layout/Header/HeaderButton.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './HeaderButton.module.css';
+
+const HeaderButton = () => (
+  <div
+    className={styles.mHeaderButton}
+    id="header-button"
+  >
+    <span />
+  </div>
+);
+
+export default HeaderButton;
--- src/components/Layout/Header/index.js
+++ src/components/Layout/Header/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import cn from 'classnames';
 import i from 'common/icons';
 import styles from './Header.module.css';
+import HeaderButton from './HeaderButton';
 import SiteMenu from './SiteMenu';
 
 const Header = () => (
   <header className={styles.header}>
     <div className={cn(styles.inner, 'wrapperL')}>
+      <HeaderButton />
       <a href="/" className={styles.logo} title="goodjob 工時薪資透明化運動">
         <i.GjLogo />
       </a>
