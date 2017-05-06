aedf72ab012ac13c4cd04b5a0e19795cefd12af2
chejen <jkopre.qek@gmail.com>
2017-05-06 22:29:40 +0800

    added a Button component

---
+++ src/components/common/button/Button.js
@@ -0,0 +1,55 @@
+import React, { PropTypes } from 'react';
+
+import ArrowLeft from '../../images/arrow-left.svg';
+
+import styles from './Button.module.css';
+
+const Button = ({ circleSize, btnStyle, children, onClick }) => {
+  let cnCircleSize = '';
+  let cnBtnStyle;
+
+  if (circleSize === 'lg') {
+    cnCircleSize = 'buttonCircleL';
+  } else if (circleSize === 'md') {
+    cnCircleSize = 'buttonCircleM';
+  }
+
+  switch (btnStyle) {
+    case 'black':
+      cnBtnStyle = 'buttonBlack';
+      break;
+    case 'gray':
+      cnBtnStyle = 'buttonGray';
+      break;
+    case 'hoverYellow':
+      cnBtnStyle = 'buttonHoverYellow';
+      break;
+    case 'submit':
+      cnBtnStyle = 'buttonSubmit';
+      break;
+    case 'back':
+      cnBtnStyle = 'formLabel';
+      break;
+    default:
+      cnBtnStyle = '';
+  }
+
+  return (
+    <button
+      className={`${cnCircleSize} ${cnBtnStyle} ${styles.button}`}
+      onClick={onClick}
+    >
+      {btnStyle === 'back' && <ArrowLeft className={styles.arrowLeft} />}
+      {children}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  circleSize: PropTypes.string,
+  btnStyle: PropTypes.string,
+  children: PropTypes.node,
+  onClick: PropTypes.func,
+};
+
+export default Button;
--- src/containers/AnotherPage.js
+++ src/containers/AnotherPage.js
@@ -1,16 +1,20 @@
 import React from 'react';
 
+import Button from 'common/button/Button';
 
 const AnotherPage = () => (
-  <div>
+  <div style={{ padding: 10 }}>
+    <Button circleSize="md" btnStyle="black" onClick={() => { console.log('test'); }}>
+      立即註冊
+    </Button>
     <br /><br />
-    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<button className="buttonCircleM buttonBlack">立即註冊</button>
+    <Button circleSize="lg" btnStyle="gray">看更多</Button>
     <br /><br />
-    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<button className="buttonCircleL buttonGray">看更多</button>
+    <Button circleSize="lg" btnStyle="hoverYellow">分享面試經驗</Button>
     <br /><br />
-    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<button className="buttonCircleL buttonHoverYellow">分享面試經驗</button>
+    <Button btnStyle="submit">送出資料</Button>
     <br /><br />
-    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<button className="buttonSubmit">送出資料</button>
+    <Button btnStyle="back">返回列表</Button>
     <br /><br />
 
     <div className="headingL">熱門分享 (headingL)</div>
