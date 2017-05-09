18a6bacf70c54d608b75c7db66bdfb9a3cc3863f
Yen-chi Chen <mark86092@gmail.com>
2017-05-09 10:41:17 +0800

    Fix url may not a string

--- src/utils/helmetHelper.js
+++ src/utils/helmetHelper.js
@@ -7,7 +7,7 @@ export const formatCanonicalPath = path =>
   `https://www.goodjob.life${path}`;
 
 export const formatUrl = url => {
-  if (url.lastIndexOf('//', 0) === 0) {
+  if (url && url.lastIndexOf('//', 0) === 0) {
     return `https:${url}`;
   }
   return url;
