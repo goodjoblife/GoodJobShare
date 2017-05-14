7d23a40fcb9a36e2f39ff7516165ff3584fd1844
Wendell Liu <cuk.bas@gmail.com>
2017-05-14 17:32:42 +0800

    make submit area checkbox fit style

--- src/components/ShareExperience/common/SubmitArea.js
+++ src/components/ShareExperience/common/SubmitArea.js
@@ -4,38 +4,73 @@ import PropTypes from 'prop-types';
 import ButtonSubmit from 'common/button/ButtonSubmit';
 import Checkbox from 'common/form/Checkbox';
 
-const SubmitArea = ({ onSubmit }) => (
-  <div
-    style={{
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-    }}
-  >
-    <div
-      style={{
-        display: 'flex',
-        marginBottom: '28px',
-        alignItems: 'center',
-      }}
-    >
-      <Checkbox />
-      <p
+class SubmitArea extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.handleAgree = this.handleAgree.bind(this);
+
+    this.state = {
+      agree: false,
+    };
+  }
+
+  handleAgree(agree) {
+    this.setState(() => ({
+      agree,
+    }));
+  }
+
+  render() {
+    const {
+      onSubmit,
+    } = this.props;
+
+    const {
+      agree,
+    } = this.state;
+
+    return (
+      <div
         style={{
-          color: '#3B3B3B',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          marginTop: '57px',
         }}
       >
-        我分享的是真實資訊，並且遵守中華民國法律以及本站使用者條款。
+        <div
+          style={{
+            display: 'flex',
+            marginBottom: '28px',
+            alignItems: 'center',
+          }}
+        >
+          <Checkbox
+            margin={'0'}
+            value={''}
+            label={''}
+            checked={agree}
+            onChange={e => this.handleAgree(e.target.checked)}
+          />
+          <p
+            style={{
+              color: '#3B3B3B',
+            }}
+          >
+            我分享的是真實資訊，並且遵守中華民國法律以及本站使用者條款。
       </p>
-    </div>
-    <div>
-      <ButtonSubmit
-        text="送出資料"
-        onClick={onSubmit}
-      />
-    </div>
-  </div>
-);
+        </div>
+        <div>
+          <ButtonSubmit
+            text="送出資料"
+            onClick={onSubmit}
+          />
+        </div>
+      </div>
+    );
+  }
+}
 
 SubmitArea.propTypes = {
   onSubmit: PropTypes.func,
