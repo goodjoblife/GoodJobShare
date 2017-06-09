ee9e7389d1b5a2e8ab76969e79a20beb5d014834
peteranny <peteranny2@gmail.com>
2017-06-10 01:14:47 +0800

    reducers: is-fetching & error

--- src/reducers/laborRightsSingle.js
+++ src/reducers/laborRightsSingle.js
@@ -1,30 +1,29 @@
-import { List, Map, fromJS } from 'immutable';
+import { Map, fromJS } from 'immutable';
 import createReducer from 'utils/createReducer';
 import {
-  SET_META_LIST_STATUS,
+  SET_META_LIST_IS_FETCHING,
+  SET_META_LIST_ERROR,
   SET_META_LIST,
-  SET_DATA_STATUS,
+  SET_DATA_IS_FETCHING,
+  SET_DATA_ERROR,
   SET_DATA,
 } from '../actions/laborRightsSingle';
-import status from '../constants/status';
 
 const preloadedState = Map({
-  metaList: List(),
-  metaListStatus: status.UNFETCHED,
-  dataMapById: Map(),
+  metaListIsFetching: false,
 });
 
 export default createReducer(preloadedState, {
-  [SET_META_LIST_STATUS]: (state, { nextStatus, err }) =>
-    state
-      .set('metaListStatus', nextStatus)
-      .set('metaListError', err),
+  [SET_META_LIST_IS_FETCHING]: (state, { isFetching }) =>
+    state.set('metaListIsFetching', isFetching),
+  [SET_META_LIST_ERROR]: (state, { error }) =>
+    state.set('metaListError', error),
   [SET_META_LIST]: (state, { metaList }) =>
     state.set('metaList', fromJS(metaList)),
-  [SET_DATA_STATUS]: (state, { id, nextStatus, err }) =>
-    state
-      .setIn(['dataMapById', id, 'dataStatus'], nextStatus)
-      .setIn(['dataMapById', id, 'dataError'], err),
+  [SET_DATA_IS_FETCHING]: (state, { id, isFetching }) =>
+    state.setIn(['dataMapById', id, 'dataIsFetching'], isFetching),
+  [SET_DATA_ERROR]: (state, { id, error }) =>
+    state.setIn(['dataMapById', id, 'dataError'], error),
   [SET_DATA]: (state, { id, data }) =>
     state.setIn(['dataMapById', id, 'data'], fromJS(data)),
 });
