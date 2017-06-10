261250a9bfaee27362a3ba609127a52a7171b06d
Tin <wutingy@gmail.com>
2017-06-11 02:26:31 +0800

    use export default

--- src/components/common/base/index.js
+++ src/components/common/base/index.js
@@ -1,11 +1,4 @@
-import _Wrapper from './Wrapper';
-import _Section from './Section';
-import _Heading from './Heading';
-import _P from './P';
-
-export const Wrapper = _Wrapper;
-export const Section = _Section;
-export const Heading = _Heading;
-export const P = _P;
-
-export default '';
+export { default as Wrapper } from './Wrapper';
+export { default as Section } from './Section';
+export { default as Heading } from './Heading';
+export { default as P } from './P';
