d4f936d6a4b7b21df822f8243a63f2858f5a9cda
Wendell Liu <cuk.bas@gmail.com>
2017-06-18 11:03:08 +0800

    表單送出與FB 登入整合 (#148)
    * integrate FB login with InterviewForm
    
    * add wording below fb login button(submit)

--- src/components/common/button/ButtonSubmit.js
+++ src/components/common/button/ButtonSubmit.js
@@ -1,22 +1,74 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ImmutablePropTypes from 'react-immutable-proptypes';
 
 import styles from './ButtonSubmit.module.css';
 
-const ButtonSubmit = ({ text, onClick, disabled }) => (
-  <button
-    className={styles.container}
-    onClick={onClick}
-    disabled={disabled}
-  >
-    {text}
-  </button>
-);
+const isLogin = auth =>
+  auth.get('status') === 'connected';
+
+class ButtonSubmit extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.login = this.login.bind(this);
+  }
+  login() {
+    return this.props.login(this.props.FB)
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    const { text, onSubmit, disabled, auth } = this.props;
+    return (
+      isLogin(auth) ?
+        <button
+          className={styles.container}
+          onClick={onSubmit}
+          disabled={disabled}
+        >
+          {text}
+        </button> :
+        <div>
+          <button
+            className={styles.container}
+            onClick={() =>
+              this.login()
+                .then(onSubmit)
+            }
+            disabled={disabled}
+          >
+            {`以F認證，${text}`}
+          </button>
+          <div
+            style={{
+              textAlign: 'center',
+              marginTop: '21px',
+            }}
+          >
+            <p
+              className="pMbold"
+              style={{
+                color: '#C0C0C0',
+              }}
+            >
+              為什麼需要 Facebook 帳戶驗證？
+            </p>
+          </div>
+        </div>
+    );
+  }
+}
 
 ButtonSubmit.propTypes = {
   text: PropTypes.string,
-  onClick: PropTypes.func,
+  onSubmit: PropTypes.func,
   disabled: PropTypes.bool,
+  auth: ImmutablePropTypes.map,
+  login: PropTypes.func.isRequired,
+  FB: PropTypes.object,
 };
 
 export default ButtonSubmit;
