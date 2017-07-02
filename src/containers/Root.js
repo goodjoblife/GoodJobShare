1ddd22eb1822d015bb2c1df7d2c7d90c7ac97a7a
Yen-chi Chen <mark86092@gmail.com>
2017-07-03 00:06:38 +0800

    埋設 GA (#173)
    * init GA & send page view
    
    * Yarn package

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,20 +1,40 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import { Provider } from 'react-redux';
 import { applyRouterMiddleware, Router } from 'react-router';
 import { useScroll } from 'react-router-scroll';
+import ReactGA from 'react-ga';
 
 import routes from '../routes';
 
+class Root extends Component {
+  constructor(props) {
+    super(props);
+    this.initGA();
+  }
 
-const Root = ({ store, history }) => (
-  <Provider store={store}>
-    <Router
-      history={history}
-      routes={routes(store)}
-      render={applyRouterMiddleware(useScroll())}
-    />
-  </Provider>
-);
+  initGA = () => {
+    ReactGA.initialize('UA-79990667-1');
+  }
+
+  logPageView = () => {
+    ReactGA.set({ page: window.location.pathname + window.location.search });
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }
+
+  render() {
+    const { store, history } = this.props;
+    return (
+      <Provider store={store}>
+        <Router
+          history={history}
+          routes={routes(store)}
+          render={applyRouterMiddleware(useScroll())}
+          onUpdate={this.logPageView}
+        />
+      </Provider>
+    );
+  }
+}
 
 Root.propTypes = {
   store: PropTypes.object.isRequired,
