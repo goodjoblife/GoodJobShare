efb65c19d2e64dc3d230bddcd39a18c6ab1859e5
wutingy <wutingy@gmail.com>
2017-07-09 14:51:26 +0800

    add page routes

---
+++ src/components/Privacy/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Section, Wrapper } from 'common/base';
+
+const Privacy = () => (
+  <Section Tag="main" pageTop paddingBottom>
+    <Wrapper size="l">
+      Privacy
+    </Wrapper>
+  </Section>
+);
+
+export default Privacy;
---
+++ src/components/Terms/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Section, Wrapper } from 'common/base';
+
+const Terms = () => (
+  <Section Tag="main" pageTop paddingBottom>
+    <Wrapper size="l">
+      Terms
+    </Wrapper>
+  </Section>
+);
+
+export default Terms;
