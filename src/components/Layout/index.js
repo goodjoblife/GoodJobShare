25488a916f5aded1545e2d84b89caa7945c0c535
Wei-Ming <barry800414@gmail.com>
2017-07-18 00:51:04 +0800

    add default head informat in Layout

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,13 +1,15 @@
 import React, { PropTypes } from 'react';
+import Helmet from 'react-helmet';
 
 import styles from './App.module.css';
 import Header from '../../containers/Layout/Header';
 import Footer from './Footer';
-
+import helmetData from '../../constants/helmetData';
 
 const App = ({ children }) => (
   <div className={styles.App}>
     <Header />
+    <Helmet {...helmetData.DEFAULT} />
     <div className={styles.content}>
       {children}
     </div>
