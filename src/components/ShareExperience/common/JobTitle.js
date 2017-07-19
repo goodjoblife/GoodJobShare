99544fe9fb9e91e6464c5782f4b8b5893301bdd7
Wei-Ming <barry800414@gmail.com>
2017-07-19 22:17:55 +0800

    add autocomplete function to job-title

--- src/components/ShareExperience/common/JobTitle.js
+++ src/components/ShareExperience/common/JobTitle.js
@@ -1,28 +1,81 @@
 import React, { PropTypes } from 'react';
 
-import TextInput from 'common/form/TextInput';
+import AutoCompleteTextInput from 'common/form/AutoCompleteTextInput';
 
 import {
-  lteLength,
-} from 'utils/dataCheckUtil';
+  debounce,
+} from 'utils/streamUtils';
 
 import InputTitle from './InputTitle';
+import {
+  getJobTitlesSearch,
+} from '../../../apis/jobTitleSearchApi';
 
-const JobTitle = ({ inputTitle, jobTitle, onChange }) => (
-  <div>
-    <InputTitle
-      text={inputTitle}
-      must
-    />
-    <TextInput
-      placeholder="硬體工程師"
-      value={jobTitle}
-      onChange={e => onChange(e.target.value)}
-      isWarning={!lteLength(10)(jobTitle)}
-      warningWording="請輸入10個字以內"
-    />
-  </div>
-);
+const getItemValue = item => item.label;
+
+const mapToAutocompleteList = l => ({
+  label: l.des,
+  value: l.des,
+});
+
+class JobTitle extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleAutocompleteItems = this.handleAutocompleteItems.bind(this);
+
+    const search = debounce(
+      (e, value) => {
+        if (value) {
+          return getJobTitlesSearch(value)
+            .then(r => (Array.isArray(r) ? this.handleAutocompleteItems(r.map(mapToAutocompleteList)) : this.handleAutocompleteItems([])))
+            .catch(() => this.handleAutocompleteItems([]));
+        }
+        return this.handleAutocompleteItems([]);
+      }
+      , 800
+    );
+
+    this.handleOnChange = (e, value) => {
+      props.onChange(e.target.value);
+      return search(e, value);
+    };
+
+    this.state = {
+      autocompleteItems: [],
+    };
+  }
+
+  handleAutocompleteItems(autocompleteItems) {
+    return this.setState(() => ({
+      autocompleteItems,
+    }));
+  }
+
+  render() {
+    const { autocompleteItems } = this.state;
+    const { inputTitle, jobTitle, onChange } = this.props;
+    return (
+      <div>
+        <InputTitle
+          text={inputTitle}
+          must
+        />
+        <AutoCompleteTextInput
+          placeholder="硬體工程師"
+          value={jobTitle}
+          getItemValue={getItemValue}
+          items={autocompleteItems}
+          onChange={this.handleOnChange}
+          onSelect={value => {
+            this.handleAutocompleteItems([]);
+            return onChange(value);
+          }}
+        />
+      </div>
+    );
+  }
+}
 
 JobTitle.propTypes = {
   inputTitle: PropTypes.string,
