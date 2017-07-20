7885ba1fe0f52d34d5ed3fb25b226682c1f4f9d6
wutingy <wutingy@gmail.com>
2017-07-20 19:17:30 +0800

    format number

--- src/components/ExperienceSearch/OvertimeBlock.js
+++ src/components/ExperienceSearch/OvertimeBlock.js
@@ -4,6 +4,13 @@ import { Clock, Coin2 } from 'common/icons';
 import { P } from 'common/base';
 import styles from './OvertimeBlock.module.css';
 
+const formatNum = num => {
+  if (num === 0 || num === undefined) {
+    return '-';
+  }
+  return num;
+};
+
 const OvertimeBlock = ({ type, heading, data }) => (
   <div className={styles.overtimeBlock}>
     <P Tag="h4" size="m" bold className={styles.heading}>
@@ -18,18 +25,18 @@ const OvertimeBlock = ({ type, heading, data }) => (
             <ul className={`${styles.positiveStat}`}>
               <li>
                 <div className={styles.statHeading}>優於或符合勞基法</div>
-                <div className={styles.num}>{data.is_overtime_salary_legal_count.yes || '-'}</div>
+                <div className={styles.num}>{formatNum(data.is_overtime_salary_legal_count.ye)}</div>
               </li>
               <li>
                 <div className={styles.statHeading}>不符合勞基法</div>
-                <div className={styles.num}>{data.is_overtime_salary_legal_count.no || '-'}</div>
+                <div className={styles.num}>{formatNum(data.is_overtime_salary_legal_count.no)}</div>
               </li>
               <li>
                 <div className={styles.statHeading}>不清楚是否符合勞基法</div>
-                <div className={styles.num}>{data.is_overtime_salary_legal_count["don't know"] || '-'}</div>
+                <div className={styles.num}>{formatNum(data.is_overtime_salary_legal_count["don't know"])}</div>
               </li>
             </ul>
-          : <div className={styles.num}>{data.has_compensatory_dayoff_count.yes}</div> }
+          : <div className={styles.num}>{formatNum(data.has_compensatory_dayoff_count.yes)}</div> }
         </div>
       </div>
       <div className={styles.negative}>
@@ -38,16 +45,16 @@ const OvertimeBlock = ({ type, heading, data }) => (
             <div className={styles.statName}>沒有</div>
             <div className={styles.num}>
               { type === 'salary'
-                ? data.has_overtime_salary_count.no
-                : data.has_compensatory_dayoff_count.no }
+                ? formatNum(data.has_overtime_salary_count.no)
+                : formatNum(data.has_compensatory_dayoff_count.no) }
             </div>
           </div>
           <div className={styles.negativeStat}>
             <div className={styles.statName}>不知道</div>
             <div className={styles.num}>
               { type === 'salary'
-                ? data.has_overtime_salary_count["don't know"]
-                : data.has_compensatory_dayoff_count["don't know"] }
+                ? formatNum(data.has_overtime_salary_count["don't know"])
+                : formatNum(data.has_compensatory_dayoff_count["don't know"]) }
             </div>
           </div>
         </div>
