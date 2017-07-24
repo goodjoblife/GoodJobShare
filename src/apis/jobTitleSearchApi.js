924e94128723457beb6d7c638a5f3ea53ae60c86
Ming <barry800414@users.noreply.github.com>
2017-07-24 19:31:51 +0800

    Job title 加上 autocomplete 功能 (#196)
    * add job title autocomplete api
    
    * add autocomplete function to job-title

---
+++ src/apis/jobTitleSearchApi.js
@@ -0,0 +1,7 @@
+import fetchUtil from 'utils/fetchUtil';
+import qs from 'qs';
+
+const endpoint = '/jobs/search';
+
+export const getJobTitlesSearch = key => fetchUtil(`${endpoint}?${qs.stringify({ key })}`)('get');
+export const foo = 1;
--- src/components/ShareExperience/common/JobTitle.js
+++ src/components/ShareExperience/common/JobTitle.js
@@ -1,24 +1,83 @@
 import React, { PropTypes } from 'react';
 
-import TextInput from 'common/form/TextInput';
+import AutoCompleteTextInput from 'common/form/AutoCompleteTextInput';
+
+import {
+  debounce,
+} from 'utils/streamUtils';
 
 import InputTitle from './InputTitle';
+import {
+  getJobTitlesSearch,
+} from '../../../apis/jobTitleSearchApi';
 
-const JobTitle = ({ inputTitle, jobTitle, onChange, validator, submitted }) => (
-  <div>
-    <InputTitle
-      text={inputTitle}
-      must
-    />
-    <TextInput
-      placeholder="硬體工程師"
-      value={jobTitle}
-      onChange={e => onChange(e.target.value)}
-      isWarning={submitted && !validator(jobTitle)}
-      warningWording="需填寫職稱"
-    />
-  </div>
-);
+const getItemValue = item => item.label;
+
+const mapToAutocompleteList = l => ({
+  label: l.des,
+  value: l.des,
+});
+
+class JobTitle extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleAutocompleteItems = this.handleAutocompleteItems.bind(this);
+
+    const search = debounce(
+      (e, value) => {
+        if (value) {
+          return getJobTitlesSearch(value)
+            .then(r => (Array.isArray(r) ? this.handleAutocompleteItems(r.map(mapToAutocompleteList)) : this.handleAutocompleteItems([])))
+            .catch(() => this.handleAutocompleteItems([]));
+        }
+        return this.handleAutocompleteItems([]);
+      }
+      , 800
+    );
+
+    this.handleOnChange = (e, value) => {
+      props.onChange(e.target.value);
+      return search(e, value);
+    };
+
+    this.state = {
+      autocompleteItems: [],
+    };
+  }
+
+  handleAutocompleteItems(autocompleteItems) {
+    return this.setState(() => ({
+      autocompleteItems,
+    }));
+  }
+
+  render() {
+    const { autocompleteItems } = this.state;
+    const { inputTitle, jobTitle, onChange, validator, submitted } = this.props;
+    return (
+      <div>
+        <InputTitle
+          text={inputTitle}
+          must
+        />
+        <AutoCompleteTextInput
+          placeholder="硬體工程師"
+          value={jobTitle}
+          getItemValue={getItemValue}
+          items={autocompleteItems}
+          onChange={this.handleOnChange}
+          onSelect={value => {
+            this.handleAutocompleteItems([]);
+            return onChange(value);
+          }}
+          isWarning={submitted && !validator(jobTitle)}
+          warningWording="需填寫職稱"
+        />
+      </div>
+    );
+  }
+}
 
 JobTitle.propTypes = {
   inputTitle: PropTypes.string,
