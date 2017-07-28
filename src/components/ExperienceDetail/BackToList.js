200e971f98b17528402154401611ba318d3c2411
Wendell Liu <cuk.bas@gmail.com>
2017-07-28 10:11:42 +0800

    修正單篇經驗頁面返回不會正確回到列表的問題 (#229)
    * fix BackList goBack bug by query string
    
    * handle is backable by props

--- src/components/ExperienceDetail/BackToList.js
+++ src/components/ExperienceDetail/BackToList.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { browserHistory } from 'react-router';
 import i from 'common/icons';
 import { P } from 'common/base';
 import styles from './BackToList.module.css';
 
-const BackToList = () => (
-  <button onClick={() => browserHistory.goBack()} className={styles.backBtn}>
+const backOrPush = backable => {
+  if (backable) {
+    return browserHistory.goBack();
+  }
+
+  return browserHistory.push('/experiences/search');
+};
+
+const BackToList = ({ backable }) => (
+  <button onClick={() => backOrPush(backable)} className={styles.backBtn}>
     <i.ArrowGo /><P size="m" bold>返回列表</P>
   </button>
 );
+
+BackToList.propTypes = {
+  backable: PropTypes.bool,
+};
 
 export default BackToList;
