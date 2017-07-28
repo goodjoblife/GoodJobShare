b4099522dda664c9820ddc28ccc59dd7c17ab1cf
Wendell Liu <cuk.bas@gmail.com>
2017-07-28 20:25:46 +0800

    refetch ExperienceDetail data after logining 1. add an auth status selector 2. connect auth.status to ExperienceDetail 3. refetch experienceDetail data after logining

--- src/containers/ExperienceDetailPage.js
+++ src/containers/ExperienceDetailPage.js
@@ -4,8 +4,13 @@ import { connect } from 'react-redux';
 import ExperienceDetail from '../components/ExperienceDetail';
 import * as ExperienceDetailActions from '../actions/experienceDetail';
 
+import {
+  statusSelector,
+} from '../selectors/authSelector';
+
 const mapStateToProps = state => ({
   experienceDetail: state.experienceDetail,
+  authStatus: statusSelector(state),
 });
 
 const mapDispatchToProps = dispatch =>
