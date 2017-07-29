20171c3eaeffaafc10cfb2179841e1291c4ccc6e
peteranny <peteranny2@gmail.com>
2017-07-30 00:52:02 +0800

    info-salary content

--- src/components/ExperienceSearch/InfoSalaryModal.js
+++ src/components/ExperienceSearch/InfoSalaryModal.js
@@ -1,28 +1,52 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Modal from 'common/Modal';
+import Question from 'common/icons/Question';
 import HtmlEditor from 'common/HtmlEditor';
+import Button from 'common/button/Button';
 
 const InfoSalaryModal = ({ isOpen, close }) => (
   <Modal isOpen={isOpen} hasClose close={close}>
-    {/*
-      <svg role="img">
-        <use xlinkhref="#icon-question" />
-      </svg>
-  */}
-    <h5 className="modal-content__heading">
+    <div>
+      <Question
+        style={{
+          fill: '#FCD406',
+          height: '82px',
+          width: '82px',
+          marginBottom: '32px',
+        }}
+      />
+    </div>
+    <h2
+      style={{
+        fontSize: '2rem',
+        marginBottom: '47px',
+      }}
+    >
       時薪估計方式
-    </h5>
+    </h2>
     <HtmlEditor className="alignLeft">
       <ul>
         <li>當薪資種類為「時薪」：無需估算</li>
         <li>當薪資種類為「日薪」：以 <bold>日薪 ÷ 工作日實際工時</bold> 估算</li>
         <li>當薪資種類為「月薪」：以 <bold>(月薪 × 12) ÷ (52 × 每週平均工時 - (12天國假 + 7天特休) × 工作日實際工時)</bold> 估算</li>
         <li>當薪資種類為「年薪」：以 <bold>年薪 ÷ (52 × 每週平均工時 - (12天國假+7天特休) × 工作日實際工時)</bold> 估算</li>
-        <button className="modal-content__btn btn-black btn-m js-close-data-modal" onClick={close}>
-           OK，我瞭解了
-        </button>
       </ul>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          marginTop: '40px',
+        }}
+      >
+        <Button
+          btnStyle="black"
+          circleSize="md"
+          onClick={close}
+        >
+         OK，我瞭解了
+        </Button>
+      </div>
     </HtmlEditor>
   </Modal>
 );
