14fac43f30ac649b5b30342eba700b36ac2f7ada
Tin <peteranny2@gmail.com>
2017-07-30 21:54:51 +0800

    RWD表格 (#230)
    * company-table
    
    * column styles
    
    * column working-hour-block content on small device
    
    * remove working-hour-block style
    
    * fix gray-light
    
    * align-right
    
    * fix classname prop-type
    
    * make alignRight local in Table.module.css

--- src/components/ExperienceSearch/WorkingHourTable.js
+++ src/components/ExperienceSearch/WorkingHourTable.js
@@ -121,8 +121,9 @@ class WorkingHourTable extends Component {
     const { data } = this.props;
 
     return (
-      <Table data={data} primaryKey="created_at">
+      <Table className={styles.companyTable} data={data} primaryKey="created_at">
         <Table.Column
+          className={styles.colPosition}
           dataField="job_title"
           dataFormatter={WorkingHourTable.getTitle}
         >
@@ -130,6 +131,7 @@ class WorkingHourTable extends Component {
         </Table.Column>
 
         <Table.Column
+          className={styles.colType}
           dataField="employment_type"
           dataFormatter={WorkingHourTable.getEmploymentType}
         >
@@ -137,6 +139,7 @@ class WorkingHourTable extends Component {
         </Table.Column>
 
         <Table.Column
+          className={styles.colDayTime}
           dataField="day_promised_work_time"
           dataFormatter={WorkingHourTable.getWorkingHour}
         >
@@ -144,6 +147,7 @@ class WorkingHourTable extends Component {
         </Table.Column>
 
         <Table.Column
+          className={styles.colWeekTime}
           dataField="week_work_time"
           dataFormatter={WorkingHourTable.getWorkingTime}
         >
@@ -151,26 +155,34 @@ class WorkingHourTable extends Component {
         </Table.Column>
 
         <Table.Column
+          className={styles.colFrequency}
           dataField="overtime_frequency"
           dataFormatter={WorkingHourTable.getFrequency}
         >
           加班頻率
         </Table.Column>
 
-        <Table.Column dataField="experience_in_year">
+        <Table.Column
+          className={styles.colExperience}
+          dataField="experience_in_year"
+        >
           業界工作經歷
         </Table.Column>
 
         <Table.Column
+          className={styles.colSalary}
           dataField="salary"
           dataFormatter={WorkingHourTable.getSalary}
+          alignRight
         >
           薪資
         </Table.Column>
 
         <Table.Column
+          className={styles.colHourly}
           dataField="estimated_hourly_wage"
           dataFormatter={WorkingHourTable.getWage}
+          alignRight
         >
           <InfoSalaryModal
             isOpen={this.state.infoSalaryModal.isOpen}
@@ -182,6 +194,7 @@ class WorkingHourTable extends Component {
         </Table.Column>
 
         <Table.Column
+          className={styles.colDataTime}
           dataField="data_time"
           dataFormatter={WorkingHourTable.getDate}
         >
