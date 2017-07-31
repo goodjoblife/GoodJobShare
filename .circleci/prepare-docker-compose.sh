1b12eecbacf53046b80a1cc32cc67397fb62af5a
Yen-chi Chen <mark86092@gmail.com>
2017-07-31 21:11:43 +0800

    seperate the tool script

---
+++ .circleci/prepare-docker-compose.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -x
+
+# install docker
+VER="17.03.0-ce"
+curl -L -o /tmp/docker-$VER.tgz https://get.docker.com/builds/Linux/x86_64/docker-$VER.tgz
+tar -xz -C /tmp -f /tmp/docker-$VER.tgz
+mv /tmp/docker/* /usr/bin
+
+# prepare docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.13.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
