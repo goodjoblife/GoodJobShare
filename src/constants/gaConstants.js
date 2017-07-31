a9987570ca0815fa8e7da2a40ba1faaeacfadc52
Wei-Ming <barry800414@gmail.com>
2017-07-31 21:08:06 +0800

    add ga constants

---
+++ src/constants/gaConstants.js
@@ -0,0 +1,26 @@
+
+export const GA_CATEGORY = {
+  SHARE_INTERVIEW: 'share_interview',
+  SHARE_WORK: 'share_work',
+  FORM_TIME: 'form_time',
+  SEARCH_EXPERIENCE: 'search_experience',
+  LOGIN: 'login',
+  HEADER: 'header',
+};
+
+export const GA_ACTION = {
+  ENTER_PAGE: 'enter_page',
+  START_WRITING: 'start_writing',
+  UPLOAD_SUCCESS: 'upload_success',
+  UPLOAD_FAIL: 'upload_fail',
+  CLICK_LATEST: 'click_latest',
+  CLICK_POPULAR: 'click_popular',
+  TOGGLE_ON: 'toggle_on',
+  TOGGLE_OFF: 'toggle_off',
+  SEARCH_BY: 'search_by',
+  FETCH_MORE: 'fetch_more',
+  CLICK_TIME_AND_SALARY: 'click_time_and_salary',
+  CLICK_EXPERIENCE_SEARCH: 'click_experience_search',
+  CLICK_LABOR_RIGHTS: 'click_labor_rights',
+  CLICK_SHARE_DATA: 'click_share_data',
+};
