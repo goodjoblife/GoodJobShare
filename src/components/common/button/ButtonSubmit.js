94d57b67fbf901a47a640c326c8261ef6ffd26ef
Wendell Liu <cuk.bas@gmail.com>
2017-08-01 00:20:03 +0800

    加上送出區塊必須的連結、提示 (#252)
    * update user terms link in SubmitArea
    
    * submit area in MessageBoard
    
    * link to guidelines
    
    * add WhyFacebookAuth modal

--- src/components/common/button/ButtonSubmit.js
+++ src/components/common/button/ButtonSubmit.js
@@ -1,17 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ImmutablePropTypes from 'react-immutable-proptypes';
+import cn from 'classnames';
+import Modal from 'common/Modal';
 
 import authStatus from '../../../constants/authStatus';
+
+import WhyFacebookAuth from './WhyFacebookAuth';
 
 import styles from './ButtonSubmit.module.css';
 
 const isLogin = auth =>
   auth.get('status') === authStatus.CONNECTED;
 
+const getWhyFacebookAuth = onClick => (
+  <WhyFacebookAuth
+    buttonClick={onClick}
+  />
+);
+
 class ButtonSubmit extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isOpen: false,
+      feedback: null,
+    };
+  }
+
+  handleIsOpen = isOpen => (
+    this.setState({
+      isOpen,
+    })
+  )
+
+  handleFeedback = feedback => (
+    this.setState({
+      feedback,
+    })
+  )
+
   render() {
     const { text, onSubmit, disabled, auth, login } = this.props;
+    const { isOpen, feedback } = this.state;
     return (
       <div
         style={{
@@ -27,7 +59,13 @@ class ButtonSubmit extends React.PureComponent {
             >
               {text}
             </button> :
-            <div>
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            >
               <button
                 className={styles.container}
                 onClick={login}
@@ -37,21 +75,31 @@ class ButtonSubmit extends React.PureComponent {
               </button>
               <div
                 style={{
-                  textAlign: 'center',
                   marginTop: '21px',
+                  cursor: 'pointer',
+                }}
+                onClick={() => {
+                  this.handleIsOpen(true);
+                  return this.handleFeedback(getWhyFacebookAuth(
+                    () => this.handleIsOpen(false)
+                  ));
                 }}
               >
                 <p
-                  className="pMbold"
-                  style={{
-                    color: '#C0C0C0',
-                  }}
+                  className={cn('pMbold', styles.whyFB)}
                 >
                   為什麼需要 Facebook 帳戶驗證？
               </p>
               </div>
             </div>
         }
+        <Modal
+          isOpen={isOpen}
+          close={() => this.handleIsOpen(!isOpen)}
+          hasClose={false}
+        >
+          {feedback}
+        </Modal>
       </div>
     );
   }
