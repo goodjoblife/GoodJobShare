9e976e72077a0b5348c5545ea6419893ef06faec
wutingy <wutingy@gmail.com>
2017-08-03 20:24:50 +0800

    smaller loader on page

--- src/components/common/Loader/index.js
+++ src/components/common/Loader/index.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import cn from 'classnames';
 import styles from './Loader.module.css';
 
-const Loader = () => (
-  <div className={styles.wrapper}>
+const Loader = ({ size }) => (
+  <div className={cn(styles.wrapper, styles[size])}>
     <div className={styles.loader} />
   </div>
 );
+Loader.propTypes = {
+  size: PropTypes.oneOf(['s', 'l']),
+};
+Loader.defaultProps = {
+  size: 'l',
+};
 
 export default Loader;
