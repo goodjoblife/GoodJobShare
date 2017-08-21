c74489d187567087ea54213842b01983a27165aa
Yen-chi Chen <mark86092@gmail.com>
2017-08-21 18:26:49 +0800

    dev api

--- src/config/config.dev.js
+++ src/config/config.dev.js
@@ -1,5 +1,5 @@
 module.exports = {
-  API_HOST: process.env.API_HOST || 'https://api-stage-v4.goodjob.life',
+  API_HOST: process.env.API_HOST || 'https://api-dev.goodjob.life',
   CONTENTFUL_API_HOST: process.env.CONTENTFUL_API_HOST || 'https://content-stage.goodjob.life',
   FACEBOOK_APP_ID: process.env.FACEBOOK_APP_ID || '1750608541889151',
 };
