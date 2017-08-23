c504915b34f49956e5e3cf9b2e2f70456f0b2c0e
peteranny <peteranny2@gmail.com>
2017-08-23 22:55:47 +0800

    rewrite report-inspect-modal into class component

--- src/components/ExperienceDetail/ReportInspectModal.js
+++ src/components/ExperienceDetail/ReportInspectModal.js
@@ -1,19 +1,31 @@
 import React, { PropTypes } from 'react';
 import Modal from 'common/Modal';
 
-const ReportInspectModal = ({ isOpen, toggleReportInspectModal }) => (
-  <Modal
-    isOpen={isOpen}
-    close={() => toggleReportInspectModal(false)}
-    hasClose
-  >
-    123
-  </Modal>
-);
+class ReportInspectModal extends React.Component {
+  static propTypes = {
+    isOpen: PropTypes.bool.isRequired,
+    toggleReportInspectModal: PropTypes.func.isRequired,
+  }
 
-ReportInspectModal.propTypes = {
-  isOpen: PropTypes.bool.isRequired,
-  toggleReportInspectModal: PropTypes.func.isRequired,
-};
+  render() {
+    const { isOpen, toggleReportInspectModal } = this.props;
+    return (
+      <Modal
+        isOpen={isOpen}
+        close={() => toggleReportInspectModal(false)}
+        hasClose
+      >
+        <h2
+          style={{
+            fontSize: '2rem',
+            marginBottom: '30px',
+          }}
+        >
+          查看檢舉
+        </h2>
+      </Modal>
+    );
+  }
+}
 
 export default ReportInspectModal;
