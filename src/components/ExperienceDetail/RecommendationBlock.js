373c5d7f2246c3074f2cefc84b74f2837de104dd
Yen-chi Chen <mark86092@gmail.com>
2017-09-10 21:46:24 +0800

    Add link, date

--- src/components/ExperienceDetail/RecommendationBlock.js
+++ src/components/ExperienceDetail/RecommendationBlock.js
@@ -1,31 +1,40 @@
 import React, { PropTypes } from 'react';
-
+import { Link } from 'react-router';
+import qs from 'qs';
 import ThumbsUp from 'common/reaction/ThumbsUp';
 import Comment from 'common/reaction/Comment';
 import styles from './RecommendationBlock.module.css';
 
-const RecommendationBlock = ({ experience }) => (
-  <section className={styles.container}>
-    <div>
-      <div className={`pS ${styles.createdDate}`}>
-        { experience.type === 'interview' ? '面試' : '工作' }
+const RecommendationBlock = ({ experience }) => {
+  const formattedType = experience.type === 'interview' ? '面試' : '工作';
+  const createdAt = new Date(Date.parse(experience.created_at));
+  const year = createdAt.getFullYear();
+  const month = createdAt.getMonth() + 1;
+  const splitter = ' ・ ';
+  const backable = true;
+  return (
+    <Link to={`/experiences/${experience._id}?${qs.stringify({ backable })}`} className={styles.container}>
+      <div>
+        <div className={`pS ${styles.createdDate}`}>
+          {`${formattedType}${splitter}${year} 年 ${month} 月`}
+        </div>
+
+        <div className={`pMBold ${styles.heading}`}>
+          { experience.title }
+        </div>
+
+        <div className={`pS ${styles.createdDate}`}>
+          { experience.job_title }
+        </div>
       </div>
 
-      <div className={`pMBold ${styles.heading}`}>
-        { experience.title }
+      <div className={`pS ${styles.reaction}`}>
+        <ThumbsUp count={experience.like_count} />
+        <Comment count={experience.reply_count} />
       </div>
-
-      <div className={`pS ${styles.createdDate}`}>
-        { experience.job_title }
-      </div>
-    </div>
-
-    <div className={`pS ${styles.reaction}`}>
-      <ThumbsUp count={experience.like_count} toggled onClick={e => { console.log(e); }} />
-      <Comment count={experience.reply_count} />
-    </div>
-  </section>
-);
+    </Link>
+  );
+};
 
 RecommendationBlock.propTypes = {
   experience: PropTypes.object.isRequired,
