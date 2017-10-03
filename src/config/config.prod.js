8bf4fe72655c49b22364a208cc70d0ff1ed6ce19
Yen-chi Chen <mark86092@gmail.com>
2017-10-03 15:14:44 +0800

    Migrate to api.goodjob.life (#287)

--- src/config/config.prod.js
+++ src/config/config.prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-  API_HOST: process.env.API_HOST || 'https://tranquil-fortress-92731.herokuapp.com',
+  API_HOST: process.env.API_HOST || 'https://api.goodjob.life',
   CONTENTFUL_API_HOST: process.env.CONTENTFUL_API_HOST || 'https://content.goodjob.life',
   FACEBOOK_APP_ID: process.env.FACEBOOK_APP_ID || '1750216878594984',
 };
