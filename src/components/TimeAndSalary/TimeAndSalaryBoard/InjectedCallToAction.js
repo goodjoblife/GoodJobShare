4341b1d04f042db744a8401bfe5e1f85bc87c3b6
peteranny <peteranny2@gmail.com>
2017-10-21 03:19:12 +0800

    go button

--- src/components/TimeAndSalary/TimeAndSalaryBoard/InjectedCallToAction.js
+++ src/components/TimeAndSalary/TimeAndSalaryBoard/InjectedCallToAction.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 import styles from './InjectedCallToAction.module.css';
+import headerStyles from '../../Layout/Header/Header.module.css';
+
+const GoButton = () => (
+  <div className={styles.go}>
+    <Link to="/share" className={headerStyles.leaveDataBtn}>
+      GO!
+    </Link>
+  </div>
+);
 
 export default () => (
   <tr className={styles.row}>
     <td colSpan="7" className={styles.col}>
       <img className={styles.eye} src="https://image.goodjob.life/banners/eyes.png" role="presentation" />
       你已經看了 100 筆了，該留下你的了拔  (´_ゝ`)
+      <GoButton />
     </td>
   </tr>
 );
