2f49d905296fc24c84cf952dd8aa4669eabd77d2
peteranny <peteranny2@gmail.com>
2017-10-21 02:55:38 +0800

    injected style

--- src/components/TimeAndSalary/TimeAndSalaryBoard/InjectedCallToAction.js
+++ src/components/TimeAndSalary/TimeAndSalaryBoard/InjectedCallToAction.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
+import styles from './InjectedCallToAction.module.css';
+
 export default () => (
-  <tr><td colSpan="7">Injected AD</td></tr>
+  <tr className={styles.row}>
+    <td colSpan="7" className={styles.col}>
+      <img className={styles.eye} src="https://image.goodjob.life/banners/eyes.png" role="presentation" />
+      你已經看了 100 筆了，該留下你的了拔  (´_ゝ`)
+    </td>
+  </tr>
 );
