5a9c72c2c448456410acf56118b7a20322741aa9
Wei-Ming <barry800414@gmail.com>
2017-11-13 23:17:05 +0800

    connect experienceCount to LaborRightSingle banner

--- src/components/ExperienceSearch/Banners/Banner1.js
+++ src/components/ExperienceSearch/Banners/Banner1.js
@@ -4,17 +4,18 @@ import { Link } from 'react-router';
 import ProgressBar from 'common/ProgressBar';
 import styles from './Banners.module.css';
 
-const Banner1 = ({ className }) => (
+const Banner1 = ({ experienceCount, className }) => (
   <Link to="/share/interview" className={className}>
     <img
       src="https://image.goodjob.life/banners/banner2_2x.jpg"
       alt="好工作評論網募資中"
       className={styles.banner1}
     />
-    <ProgressBar totalData={250} size="m" theme="gray" rootClassName={styles.progress} />
+    <ProgressBar totalData={experienceCount} size="m" theme="gray" rootClassName={styles.progress} />
   </Link>
 );
 Banner1.propTypes = {
+  experienceCount: PropTypes.number,
   className: PropTypes.string,
 };
 
--- src/components/LaborRightsSingle/Body.js
+++ src/components/LaborRightsSingle/Body.js
@@ -6,7 +6,7 @@ import MarkdownParser from './MarkdownParser';
 import styles from './Body.module.css';
 import LeftBanner from '../ExperienceSearch/Banners/Banner1';
 
-const Body = ({ title, seoText, description, content }) => (
+const Body = ({ title, seoText, description, content, experienceCount }) => (
   <Section Tag="main" pageTop>
     <Wrapper size="m">
       <Heading size="l" bold marginBottom>{title}</Heading>
@@ -20,7 +20,7 @@ const Body = ({ title, seoText, description, content }) => (
           <Sticky disableCompensation>
             {({ style }) => (
               <div style={style}>
-                <LeftBanner />
+                <LeftBanner experienceCount={experienceCount} />
               </div>
             )}
           </Sticky>
@@ -41,6 +41,7 @@ Body.propTypes = {
   seoText: PropTypes.string,
   description: PropTypes.string.isRequired,
   content: PropTypes.string.isRequired,
+  experienceCount: PropTypes.number,
 };
 Body.defaultProps = {
   title: '',
