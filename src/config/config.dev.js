045922ebc96eee36187040324a9afe5834457c6f
Yen-chi Chen <mark86092@gmail.com>
2017-12-02 16:13:05 +0800

    Seperate GA tracking ID from production online and stage/dev environment

--- src/config/config.dev.js
+++ src/config/config.dev.js
@@ -2,4 +2,5 @@ module.exports = {
   API_HOST: process.env.API_HOST || 'https://api-dev.goodjob.life',
   CONTENTFUL_API_HOST: process.env.CONTENTFUL_API_HOST || 'https://content-stage.goodjob.life',
   FACEBOOK_APP_ID: process.env.FACEBOOK_APP_ID || '1750608541889151',
+  GA_ID: process.env.GA_ID || 'UA-79990667-2',
 };
