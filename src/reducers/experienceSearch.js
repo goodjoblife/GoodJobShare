c3e8d4938de09205c6338392256609977dc6a292
Yen-chi Chen <mark86092@gmail.com>
2017-12-10 19:05:49 +0800

    改善選單切換速度 (#379)
    * 在 fetchExperiences 完成前，先 dispatch 一些參數並清空資料
    
    * Bugfix: when loading, expect not to display any data related information
    like `尚未有「工程師」的經驗分享`
    
    * 將改變 Experiences 與 Status 的 action 分離
    
    * 移除不需要的 action 與 state

--- src/reducers/experienceSearch.js
+++ src/reducers/experienceSearch.js
@@ -1,35 +1,30 @@
 import { Map, fromJS } from 'immutable';
 
 import createReducer from 'utils/createReducer';
-import status from '../constants/status';
+import statusConstant from '../constants/status';
 import {
   SET_SEARCH_BY,
-  SET_EXPERIENCES,
   SET_WORKINGS,
   SET_KEYWORD,
   SET_KEYWORDS,
-  SET_KEYWORDS_AND_EXPERIENCES,
   SET_SORT_AND_EXPERIENCES,
-  SET_CURRENT_PAGE,
   SET_LOADING_STATUS,
 } from '../actions/experienceSearch';
 
 const preloadedState = Map({
   sort: 'created_at',
-  searchType: ['interview', 'work'],
-  industry: 'all',
-  searchBy: 'job_title',
-  searchQuery: '', // query & result 用
-  keyword: '', // input value 用
-  keywords: [],
+  searchType: ['interview', 'work'], // 過濾 面試、工作經驗 --> 正確是 type
+  searchBy: 'job_title', // 搜尋類別（公司、職稱）
+  searchQuery: '', // 搜尋類別的關鍵字
+  currentPage: 1,
+
   experiences: [],
   experienceCount: 0,
-  prevCond: 'sort',
-  preValue: '',
-  currentPage: 1,
-  hasMore: false,
+
+  keyword: '', // input value 用
+  keywords: [],
   workings: [],
-  loadingStatus: status.UNFETCHED,
+  loadingStatus: statusConstant.UNFETCHED,
   error: null,
 });
 
@@ -39,12 +34,6 @@ const experienceSearch = createReducer(preloadedState, {
 
   [SET_KEYWORD]: (state, action) =>
     state.update('keyword', () => action.keyword),
-
-  [SET_EXPERIENCES]: (state, action) =>
-    state.merge({
-      experiences: fromJS(action.experiences || []),
-      experienceCount: action.experienceCount,
-    }),
 
   [SET_WORKINGS]: (state, action) =>
     state.update('workings', () => fromJS(action.workings || [])),
@@ -56,37 +45,21 @@ const experienceSearch = createReducer(preloadedState, {
 
   [SET_SORT_AND_EXPERIENCES]: (state, action) =>
     state.merge({
-      loadingStatus: action.payload.loadingStatus,
-      error: action.payload.error,
       sort: action.payload.sort,
       keyword: action.payload.searchQuery,
       searchQuery: action.payload.searchQuery,
-      workings: action.payload.workings,
       salary: action.payload.salary,
       experienceCount: action.payload.experienceCount,
       experiences: fromJS(action.payload.experiences || []),
-      hasMore: action.payload.hasMore,
-      prevCond: action.payload.prevCond,
-      prevValue: action.payload.prevValue,
       currentPage: action.payload.currentPage,
       searchType: action.payload.searchType,
+      searchBy: action.payload.searchBy,
     }),
 
-  [SET_KEYWORDS_AND_EXPERIENCES]: (state, action) =>
+  [SET_LOADING_STATUS]: (state, { payload: { status, error } }) =>
     state.merge({
-      keyword: fromJS(action.keywords || []),
-      experienceCount: action.experienceCount,
-      experiences: fromJS(action.experiences || []),
-    }),
-
-  [SET_CURRENT_PAGE]: (state, action) =>
-    state.merge({
-      currentPage: action.payload.currentPage,
-    }),
-
-  [SET_LOADING_STATUS]: (state, action) =>
-    state.merge({
-      loadingStatus: action.payload.loadingStatus,
+      loadingStatus: status,
+      error,
     }),
 });
 
