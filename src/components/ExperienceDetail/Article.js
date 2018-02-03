8217c2de8e46c12d01de9c3a466ffb51b0f746b2
Wei-Ming <barry800414@gmail.com>
2018-02-04 01:33:23 +0800

    hide experienceDetail content if didn't have permission

--- src/components/ExperienceDetail/Article.js
+++ src/components/ExperienceDetail/Article.js
@@ -8,152 +8,193 @@ import InfoBlock from './InfoBlock';
 import SectionBlock from './SectionBlock';
 import QABlock from './QABlock';
 import { overallRatingDialogMap } from '../ShareExperience/common/optionMap';
+import { MAX_WORDS_IF_HIDDEN } from '../../constants/hideContent';
 
-const Article = ({ experience }) => (
-  <div className={styles.container}>
-    <aside className={styles.aside}>
-      {experience.type === 'interview' && (
-      <ul className={styles.infoList}>
-        <InfoBlock label="面試地區">
-          {experience.region}
-        </InfoBlock>
-        <InfoBlock label="應徵職稱">
-          {experience.job_title}
-        </InfoBlock>
-        {
-          experience.experience_in_year &&
-          <InfoBlock label="自身相關職務工作經驗">
-            {experience.experience_in_year} 年
-          </InfoBlock>
-        }
-        {
-          experience.education &&
-          <InfoBlock label="最高學歷">
-            {experience.education}
-          </InfoBlock>
-        }
-        {
-          experience.interview_time &&
-          <InfoBlock label="面試時間">
-            {`${experience.interview_time.year} 年 ${experience.interview_time.month} 月`}
-          </InfoBlock>
-        }
-        <InfoBlock label="面試結果">
-          {experience.interview_result}
-        </InfoBlock>
-        {
-          experience.salary &&
-          <InfoBlock label="待遇">
-            {`${experience.salary.amount} / ${Article.getTimeUnit(experience.salary.type)}`}
-          </InfoBlock>
-        }
-        <InfoBlock label="對公司的面試整體滿意度">
-          <div className={styles.ratingContainer}>
-            {[1, 2, 3, 4, 5].map(el => (
-              <Glike
-                key={el}
-                className={cn(rateButtonStyles.container, styles.autoCursor, {
-                  [rateButtonStyles.active]: (el <= experience.overall_rating),
-                })}
-              />
-            ))}
-            <div className={`${styles.ratingText} pS`}>
-              {overallRatingDialogMap[experience.overall_rating]}
-            </div>
-          </div>
-        </InfoBlock>
-        {
-          experience.interview_sensitive_questions && experience.interview_sensitive_questions.length
-          ? (<InfoBlock label="有以下特殊問題">
-            <ul>
-              {
-                experience.interview_sensitive_questions.map((o, idx) => (
-                  <li key={idx}>{o}</li>
-                ))
+class Article extends React.Component {
+
+  renderSections = () => {
+    const { experience, hideContent } = this.props;
+    let toHide = false;
+    let currentTotalWords = 0;
+
+    if (hideContent) {
+      return (
+        <div>
+          {experience.sections && (
+            experience.sections.map(({ subtitle, content }, idx) => {
+              if (toHide) { return null; }
+              currentTotalWords += content.length;
+              if (currentTotalWords > MAX_WORDS_IF_HIDDEN) {
+                toHide = true;
+                const showLength = content.length - (currentTotalWords - MAX_WORDS_IF_HIDDEN);
+                const newContent = `${content.substring(0, showLength)}...`;
+                return (
+                  <SectionBlock key={idx} subtitle={subtitle} content={newContent} />
+                );
               }
-            </ul>
-          </InfoBlock>)
-          : null
-        }
-      </ul>
-      )}
-
-      {experience.type === 'work' && (
-      <ul className={styles.infoList}>
-        <InfoBlock label="工作地區">
-          {experience.region}
-        </InfoBlock>
-        <InfoBlock label="職稱">
-          {experience.job_title}
-        </InfoBlock>
-        {
-          experience.experience_in_year &&
-          <InfoBlock label="自身相關職務工作經驗">
-            {experience.experience_in_year} 年
-          </InfoBlock>
-        }
-        {
-          experience.education &&
-          <InfoBlock label="最高學歷">
-            {experience.education}
-          </InfoBlock>
-        }
-        {
-          experience.week_work_time &&
-          <InfoBlock label="一週工時">
-            {experience.week_work_time}
-          </InfoBlock>
-        }
-        {
-          experience.salary &&
-          <InfoBlock label="待遇">
-            {`${experience.salary.amount} / ${Article.getTimeUnit(experience.salary.type)}`}
-          </InfoBlock>
-        }
-        {
-          experience.recommend_to_others &&
-          <InfoBlock label="是否推薦此工作">
-            {experience.recommend_to_others === 'yes' ?
-              <div className={styles.recommendIcon}><Good />推</div>
-              : <div className={styles.recommendIcon}><Bad /> 不推</div>
-            }
-          </InfoBlock>
-        }
-      </ul>
-      )}
-    </aside>
-    <section className={styles.main}>
-      <div className={styles.article}>
-        <Heading size="m" className={styles.heading}>{experience.title}</Heading>
+              return (
+                <SectionBlock key={idx} subtitle={subtitle} content={content} />
+              );
+            })
+          )}
+        </div>
+      );
+    }
+    return (
+      <div>
         {experience.sections && (
           experience.sections.map(({ subtitle, content }, idx) => (
-            <SectionBlock
-              key={idx}
-              subtitle={subtitle}
-              content={content}
-            />
+            <SectionBlock key={idx} subtitle={subtitle} content={content} />
           ))
         )}
       </div>
+    );
+  }
 
-      {(experience.type === 'interview' && experience.interview_qas
-        && experience.interview_qas.length) ?
-        (
-          <div className={styles.qaWrapper}>
-            <P size="l" bold>面試問答</P>
-            {experience.interview_qas.map(({ question, answer }, idx) => (
-              <QABlock key={idx} question={question} answer={answer} />
-            ))}
+  render() {
+    const { experience, hideContent } = this.props;
+    return (
+      <div className={styles.container}>
+        <aside className={styles.aside}>
+          {experience.type === 'interview' && (
+          <ul className={styles.infoList}>
+            <InfoBlock label="面試地區">
+              {experience.region}
+            </InfoBlock>
+            <InfoBlock label="應徵職稱">
+              {experience.job_title}
+            </InfoBlock>
+            {
+              experience.experience_in_year &&
+              <InfoBlock label="自身相關職務工作經驗">
+                {experience.experience_in_year} 年
+              </InfoBlock>
+            }
+            {
+              experience.education &&
+              <InfoBlock label="最高學歷">
+                {experience.education}
+              </InfoBlock>
+            }
+            {
+              experience.interview_time &&
+              <InfoBlock label="面試時間">
+                {`${experience.interview_time.year} 年 ${experience.interview_time.month} 月`}
+              </InfoBlock>
+            }
+            <InfoBlock label="面試結果">
+              {experience.interview_result}
+            </InfoBlock>
+            {
+              experience.salary &&
+              <InfoBlock label="待遇">
+                {`${experience.salary.amount} / ${Article.getTimeUnit(experience.salary.type)}`}
+              </InfoBlock>
+            }
+            <InfoBlock label="對公司的面試整體滿意度">
+              <div className={styles.ratingContainer}>
+                {[1, 2, 3, 4, 5].map(el => (
+                  <Glike
+                    key={el}
+                    className={cn(rateButtonStyles.container, styles.autoCursor, {
+                      [rateButtonStyles.active]: (el <= experience.overall_rating),
+                    })}
+                  />
+                ))}
+                <div className={`${styles.ratingText} pS`}>
+                  {overallRatingDialogMap[experience.overall_rating]}
+                </div>
+              </div>
+            </InfoBlock>
+            {
+              experience.interview_sensitive_questions && experience.interview_sensitive_questions.length
+              ? (<InfoBlock label="有以下特殊問題">
+                <ul>
+                  {
+                    experience.interview_sensitive_questions.map((o, idx) => (
+                      <li key={idx}>{o}</li>
+                    ))
+                  }
+                </ul>
+              </InfoBlock>)
+              : null
+            }
+          </ul>
+          )}
+
+          {experience.type === 'work' && (
+          <ul className={styles.infoList}>
+            <InfoBlock label="工作地區">
+              {experience.region}
+            </InfoBlock>
+            <InfoBlock label="職稱">
+              {experience.job_title}
+            </InfoBlock>
+            {
+              experience.experience_in_year &&
+              <InfoBlock label="自身相關職務工作經驗">
+                {experience.experience_in_year} 年
+              </InfoBlock>
+            }
+            {
+              experience.education &&
+              <InfoBlock label="最高學歷">
+                {experience.education}
+              </InfoBlock>
+            }
+            {
+              experience.week_work_time &&
+              <InfoBlock label="一週工時">
+                {experience.week_work_time}
+              </InfoBlock>
+            }
+            {
+              experience.salary &&
+              <InfoBlock label="待遇">
+                {`${experience.salary.amount} / ${Article.getTimeUnit(experience.salary.type)}`}
+              </InfoBlock>
+            }
+            {
+              experience.recommend_to_others &&
+              <InfoBlock label="是否推薦此工作">
+                {experience.recommend_to_others === 'yes' ?
+                  <div className={styles.recommendIcon}><Good />推</div>
+                  : <div className={styles.recommendIcon}><Bad /> 不推</div>
+                }
+              </InfoBlock>
+            }
+          </ul>
+          )}
+        </aside>
+        <section className={styles.main}>
+          <div className={styles.article}>
+            <Heading size="m" className={styles.heading}>{experience.title}</Heading>
+            {this.renderSections()}
           </div>
-        ) :
-        null
-      }
-    </section>
-  </div>
-);
+          {(experience.type === 'interview' &&
+            experience.interview_qas &&
+            experience.interview_qas.length &&
+            !hideContent
+          ) ?
+            (
+              <div className={styles.qaWrapper}>
+                <P size="l" bold>面試問答</P>
+                {experience.interview_qas.map(({ question, answer }, idx) => (
+                  <QABlock key={idx} question={question} answer={answer} />
+                ))}
+              </div>
+            ) :
+            null
+          }
+        </section>
+      </div>
+    );
+  }
+}
 
 Article.propTypes = {
   experience: PropTypes.object.isRequired,
+  hideContent: PropTypes.bool.isRequired,
 };
 
 Article.getTimeUnit = type => {
---
+++ src/constants/hideContent.js
@@ -0,0 +1,3 @@
+export const MAX_WORDS_IF_HIDDEN = 50;
+export const MAX_IMAGES_IF_HIDDEN = 2;
+export const MAX_ROWS_IF_HIDDEN = 3;
