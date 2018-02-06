dfd484da58af134fb18962ba6c5a59ee68dcc38e
Wei-Ming <barry800414@gmail.com>
2018-02-07 01:27:55 +0800

    fetch permission again while authStatus changed to connected

--- src/containers/Layout/Header/index.js
+++ src/containers/Layout/Header/index.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import withFB from 'common/withFB';
 import Header from '../../../components/Layout/Header';
 import { login, logout, getLoginStatus, getMe } from '../../../actions/auth';
+import { fetchMyPermission } from '../../../actions/me';
 
 
 const mapStateToProps = state => ({
@@ -12,6 +13,6 @@ const mapStateToProps = state => ({
 
 
 const mapDispatchToProps = dispatch =>
-  bindActionCreators({ login, logout, getLoginStatus, getMe }, dispatch);
+  bindActionCreators({ login, logout, getLoginStatus, getMe, fetchMyPermission }, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(withFB(Header));
