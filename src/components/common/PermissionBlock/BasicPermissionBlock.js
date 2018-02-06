510d8b7b533bdfa27070709afbb07e6b4e9df249
Wei-Ming <barry800414@gmail.com>
2018-02-06 23:11:51 +0800

    refactor: change structure and style for BasicPermissionBlock

--- src/components/common/PermissionBlock/BasicPermissionBlock.js
+++ src/components/common/PermissionBlock/BasicPermissionBlock.js
@@ -1,9 +1,13 @@
 import React, { PropTypes } from 'react';
+import cn from 'classnames';
+import { Link } from 'react-router';
 
+import P from 'common/base/P';
 import styles from './PermissionBlock.module.css';
 
 class BasicPermissionBlock extends React.Component {
   static propTypes = {
+    rootClassName: PropTypes.string,
     experienceCount: PropTypes.number.isRequired,
     timeAndSalaryCount: PropTypes.number.isRequired,
     laborRightsCount: PropTypes.number.isRequired,
@@ -13,6 +17,10 @@ class BasicPermissionBlock extends React.Component {
     queryExperienceCount: PropTypes.func.isRequired,
     queryTimeAndSalaryCount: PropTypes.func.isRequired,
     queryLaborRightsCount: PropTypes.func.isRequired,
+  }
+
+  static defaultProps = {
+    rootClassName: '',
   }
 
   componentDidMount() {
@@ -28,12 +36,23 @@ class BasicPermissionBlock extends React.Component {
   }
 
   render() {
-    const { experienceCount, timeAndSalaryCount, laborRightsCount } = this.props;
+    const { rootClassName, experienceCount, timeAndSalaryCount, laborRightsCount } = this.props;
     return (
-      <div className={styles['permission-block']}>
-        <span>{`只要 40 秒，分享一筆你的 薪資 或 工時 或 面試 或 工作經驗，就能查看全站超過 ${timeAndSalaryCount} 筆薪資工時資訊、${experienceCount} 篇面試及工作經驗分享，以及 ${laborRightsCount} 篇的勞動權益懶人包哦！`}</span>
-        <span>若你已經分享過資訊，登入即可查看全文！</span>
-        <button> CTA button </button>
+      <div className={cn(styles.permissionBlock, rootClassName)}>
+        <div className={styles.container}>
+          <P size="l" className={styles.ctaText}>
+            <strong>只要 40 秒</strong>，分享一筆你的 薪資<strong> 或 </strong>工時
+            <strong> 或 </strong>面試<strong> 或 </strong> 工作經驗，就能查看
+            <strong>{`全站 ${timeAndSalaryCount} 筆`}</strong>薪資工時資訊、
+            <strong>{`${experienceCount} 篇`}</strong>面試及工作經驗分享，以及
+            <strong>{` ${laborRightsCount} 篇`}</strong>勞動權益懶人包哦！
+          </P>
+          <br />
+          <P size="l"><strong>若你已經分享過資訊，登入即可查看全文！</strong></P>
+          <div className={styles.ctaButtonContainer}>
+            <Link to={'/share'} className={cn('buttonCircleM', 'buttonBlack2')}>立即登入並分享</Link>
+          </div>
+        </div>
       </div>
     );
   }
