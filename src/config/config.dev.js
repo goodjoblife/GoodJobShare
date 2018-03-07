38aa074ffa1687622b19cc5dfb7a01cc4703eb49
Wei-Ming <barry800414@gmail.com>
2018-03-07 22:37:23 +0800

    add configuration for facebook pixel

--- src/config/config.dev.js
+++ src/config/config.dev.js
@@ -3,4 +3,5 @@ module.exports = {
   CONTENTFUL_API_HOST: process.env.CONTENTFUL_API_HOST || 'https://content-stage.goodjob.life',
   FACEBOOK_APP_ID: process.env.FACEBOOK_APP_ID || '1750608541889151',
   GA_ID: process.env.GA_ID || 'UA-79990667-2',
+  PIXEL_ID: process.env.PIXEL_ID || '603414113402034',
 };
