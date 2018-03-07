5fcf6c95527775c61a8a309b8f3ea7d0398daf95
Yen-chi Chen <mark86092@gmail.com>
2018-03-08 02:40:11 +0800

    移轉 LandingPage & About

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
+import LandingPage from './containers/LandingPage';
+import About from './components/About';
 
 const routes = [
+  {
+    path: '/',
+    exact: true,
+    component: LandingPage,
+  },
+  {
+    path: '/about',
+    exact: true,
+    component: About,
+  },
   {
     render: () => (<div>Not Found!</div>),
   },
