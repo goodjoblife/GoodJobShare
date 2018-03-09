5229af58a0e5dab708449615215471086dc63903
Yen-chi Chen <mark86092@gmail.com>
2018-03-09 22:39:41 +0800

    改用 selector

--- src/containers/PermissionBlock/selectors.js
+++ src/containers/PermissionBlock/selectors.js
@@ -1,6 +1,10 @@
 import R from 'ramda';
 
 import fetchingStatus from '../../constants/status';
+import {
+  menuEntriesSelector,
+  menuStatusSelector,
+} from '../../selectors/laborRightsSelector';
 
 export const experienceCountSelector = state =>
   state.experiences.get('count');
@@ -10,10 +14,12 @@ export const hasFetchedexperienceCountSelector = R.compose(
   state => state.experiences.get('countStatus')
 );
 
-export const laborRightsCountSelector = state =>
-  state.laborRights.get('menuEntries').size;
+export const laborRightsCountSelector = R.compose(
+  entries => entries.size,
+  menuEntriesSelector,
+);
 
 export const hasFetchedLaborRightsCountSelector = R.compose(
   R.equals(fetchingStatus.FETCHED),
-  state => state.laborRights.get('menuStatus')
+  menuStatusSelector,
 );
