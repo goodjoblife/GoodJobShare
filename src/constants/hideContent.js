daabd1a4f591930082ffffe1dcddb55bdd8fcc86
Ming <barry800414@users.noreply.github.com>
2018-03-14 11:20:43 +0900

    Update: 權限機制 (#416)
    1. 網頁連結以工時薪資為主
    2. 無權限時，允許看到更多的顯示列數
    3. 小教室顯示頁數以 contentful 來源的限制為主

--- src/constants/hideContent.js
+++ src/constants/hideContent.js
@@ -1,4 +1,4 @@
 export const MAX_WORDS_IF_HIDDEN = 50;
 export const MAX_IMAGES_IF_HIDDEN = 3;
-export const MAX_ROWS_IF_HIDDEN = 3;
+export const MAX_ROWS_IF_HIDDEN = 8;
 export const MARKDOWN_DIVIDER = '---';
