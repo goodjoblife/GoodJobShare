2af2f4ff0e643180e73b35234bd4e2dff27e70af
peteranny <peteranny2@gmail.com>
2018-04-08 00:52:21 +0800

    action: add campaign-name in campaign-time-salary state; prevent changes for inconsistent campaign-name

--- src/reducers/campaignTimeAndSalaryBoard.js
+++ src/reducers/campaignTimeAndSalaryBoard.js
@@ -5,6 +5,7 @@ import { SET_BOARD_DATA, SET_BOARD_STATUS, SET_BOARD_EXTREME_DATA, SET_BOARD_EXT
 import fetchingStatus from '../constants/status';
 
 const preloadedState = fromJS({
+  campaignName: '',
   sortBy: null,
   order: null,
   data: [],
@@ -16,8 +17,9 @@ const preloadedState = fromJS({
 });
 
 export default createReducer(preloadedState, {
-  [SET_BOARD_DATA]: (state, { sortBy, order, data, status, error }) =>
+  [SET_BOARD_DATA]: (state, { campaignName, sortBy, order, data, status, error }) =>
     state
+      .set('campaignName', campaignName)
       .set('data', fromJS(data))
       .set('status', status)
       .set('error', error)
