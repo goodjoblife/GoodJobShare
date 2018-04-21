94f1c8a2107a726768bc27799bff21eafb034bde
Tin <peteranny2@gmail.com>
2018-04-21 16:24:04 +0800

    [Feature] 職業別薪資工時大調查 Campaign 填寫頁面  (#456)
    * form: init from time-salary-form
    
    * form: local-storage-key for draft
    
    * form: form-title/info/ending
    
    * form: extra fields
    
    * form: default-job-title
    
    * form: job-content
    
    * form: job-content using textarea
    
    * form: form-check on submit
    
    * form: get-campaign-time-and-salary-form on submit
    
    * remove comments
    
    * remove draft mechanism
    
    * fix proptypes

---
+++ src/containers/ShareExperience/CampaignTimeAndSalaryFormContainer.js
@@ -0,0 +1,10 @@
+import { connect } from 'react-redux';
+
+import CampaignTimeAndSalaryForm from '../../components/ShareExperience/CampaignTimeAndSalaryForm';
+
+
+const mapStateToProps = () => ({});
+
+const mapDispatchToProps = () => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CampaignTimeAndSalaryForm);
