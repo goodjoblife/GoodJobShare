c5aa0dcc7ea4320d4bf0e11f67977695af24bece
peteranny <peteranny2@gmail.com>
2018-05-03 22:57:57 +0800

    about-this-job modal style

--- src/components/TimeAndSalary/common/AboutThisJobModal.js
+++ src/components/TimeAndSalary/common/AboutThisJobModal.js
@@ -4,8 +4,8 @@ import Modal from 'common/Modal';
 
 const AboutThisJobModal = ({ isOpen, close, title, aboutThisJob }) => (
   <Modal isOpen={isOpen} hasClose close={close}>
-    <h1>{title}</h1>
-    {aboutThisJob && aboutThisJob.split('\n').map((line, i) => <p key={i}>{line}</p>)}
+    <h1 style={{ textAlign: 'center', fontSize: '150%', marginBottom: '1em' }}>{title}</h1>
+    {aboutThisJob && aboutThisJob.split('\n').map((line, i) => <p key={i} style={{ lineHeight: 1.5, marginTop: '0.5em' }}>{line}</p>)}
   </Modal>
 );
 
