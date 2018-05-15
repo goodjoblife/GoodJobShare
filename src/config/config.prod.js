ec27bea177f5557181778855b4cb5cf0f236da7b
Yen-chi Chen <mark86092@gmail.com>
2018-05-15 21:21:56 +0800

    Point default prod env to dev setting

--- src/config/config.prod.js
+++ src/config/config.prod.js
@@ -1,7 +1,7 @@
 module.exports = {
-  API_HOST: process.env.API_HOST || 'https://api.goodjob.life',
-  CONTENTFUL_API_HOST: process.env.CONTENTFUL_API_HOST || 'https://content.goodjob.life',
-  FACEBOOK_APP_ID: process.env.FACEBOOK_APP_ID || '1750216878594984',
-  GA_ID: process.env.GA_ID || 'UA-79990667-1',
+  API_HOST: process.env.API_HOST || 'https://api-dev.goodjob.life',
+  CONTENTFUL_API_HOST: process.env.CONTENTFUL_API_HOST || 'https://content-dev.goodjob.life',
+  FACEBOOK_APP_ID: process.env.FACEBOOK_APP_ID || '1750608541889151',
+  GA_ID: process.env.GA_ID || 'UA-79990667-2',
   PIXEL_ID: process.env.PIXEL_ID || '603414113402034',
 };
