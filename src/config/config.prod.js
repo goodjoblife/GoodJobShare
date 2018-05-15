08e0cf6ca78437c006d77278690753ed6d462ad7
Yen-chi Chen <mark86092@gmail.com>
2018-05-15 11:21:34 +0800

    Rename envs

--- src/config/config.prod.js
+++ src/config/config.prod.js
@@ -1,7 +1,7 @@
 module.exports = {
-  API_HOST: process.env.API_HOST || 'https://api.goodjob.life',
-  CONTENTFUL_API_HOST: process.env.CONTENTFUL_API_HOST || 'https://content.goodjob.life',
-  FACEBOOK_APP_ID: process.env.FACEBOOK_APP_ID || '1750216878594984',
-  GA_ID: process.env.GA_ID || 'UA-79990667-1',
-  PIXEL_ID: process.env.PIXEL_ID || '603414113402034',
+  API_HOST: process.env.RAZZLE_API_HOST || 'https://api.goodjob.life',
+  CONTENTFUL_API_HOST: process.env.RAZZLE_CONTENTFUL_API_HOST || 'https://content.goodjob.life',
+  FACEBOOK_APP_ID: process.env.RAZZLE_FACEBOOK_APP_ID || '1750216878594984',
+  GA_ID: process.env.RAZZLE_GA_ID || 'UA-79990667-1',
+  PIXEL_ID: process.env.RAZZLE_PIXEL_ID || '603414113402034',
 };
