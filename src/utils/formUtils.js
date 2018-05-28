c78c93da543905640c959d51cbadd4122b8a23ca
Wei-Ming <barry800414@gmail.com>
2018-05-28 20:55:54 +0800

    update: refactor formUtils according to comments

--- src/utils/formUtils.js
+++ src/utils/formUtils.js
@@ -1,11 +1,10 @@
 
 /**
  * convert number to [xxxx億][xxxx萬][xxxx]元 format
- * @param {*} num the number to be converted
+ * @param {Integer} num the number to be converted
  */
-export const numToChineseReadableString = num => {
-  let tmpNum = parseInt(num, 10);
-  if (isNaN(tmpNum) || num <= 0) {
+const numToChineseReadableString = num => {
+  if (num <= 0) {
     return null;
   }
   const base = [1, 10000, 100000000];
@@ -13,7 +12,8 @@ export const numToChineseReadableString = num => {
 
   let str = '';
   let c;
-  for (let i = base.length; i >= 0; i -= 1) {
+  let tmpNum = num;
+  for (let i = base.length - 1; i >= 0; i -= 1) {
     if (tmpNum >= base[i]) {
       c = Math.floor(tmpNum / base[i]);
       tmpNum -= c * base[i];
@@ -25,14 +25,10 @@ export const numToChineseReadableString = num => {
 
 /**
  * Decide whether to show salary warning
- * @param {*} salaryType the type of salary (hour, day, month, year)
- * @param {*} salaryAmount the amount of salary
+ * @param {String} salaryType the type of salary (hour, day, month, year)
+ * @param {Integer} salaryAmount the amount of salary
  */
-export const shouldShowSalaryWarning = (salaryType, salaryAmount) => {
-  const amount = parseInt(salaryAmount, 10);
-  if (isNaN(amount)) {
-    return false;
-  }
+const shouldShowSalaryWarning = (salaryType, salaryAmount) => {
   const ranges = {
     hour: [100, 500],
     day: [800, 4000],
@@ -42,7 +38,7 @@ export const shouldShowSalaryWarning = (salaryType, salaryAmount) => {
   if (!(salaryType in ranges)) {
     return false;
   }
-  if (amount < ranges[salaryType][0] || amount > ranges[salaryType][1]) {
+  if (salaryAmount < ranges[salaryType][0] || salaryAmount > ranges[salaryType][1]) {
     return true;
   }
   return false;
@@ -50,12 +46,16 @@ export const shouldShowSalaryWarning = (salaryType, salaryAmount) => {
 
 /**
  * Combine converting and checking warning task for salary field
- * @param {*} salaryType the type of salary (hour, day, month, year)
- * @param {*} salaryAmount the amount of salary
+ * @param {String} salaryType the type of salary (hour, day, month, year)
+ * @param {String} salaryAmount the amount of salary
  */
-export const salaryHint = (salaryType, salaryAmount) => {
-  const readableStr = numToChineseReadableString(salaryAmount);
-  const showWarning = shouldShowSalaryWarning(salaryType, salaryAmount);
+const salaryHint = (salaryType, salaryAmount) => {
+  const amount = parseInt(salaryAmount, 10);
+  if (isNaN(amount)) {
+    return { showWarning: false, hint: null };
+  }
+  const readableStr = numToChineseReadableString(amount);
+  const showWarning = shouldShowSalaryWarning(salaryType, amount);
   const salaryTypeWord = {
     hour: '時薪',
     day: '日薪',
@@ -63,9 +63,11 @@ export const salaryHint = (salaryType, salaryAmount) => {
     year: '年薪',
   };
 
-  let hint = null;
   if (readableStr !== null && salaryType in salaryTypeWord) {
-    hint = `${salaryTypeWord[salaryType]} ${readableStr}`;
+    const hint = `${salaryTypeWord[salaryType]} ${readableStr}`;
+    return { showWarning, hint };
   }
-  return { showWarning, hint };
+  return { showWarning, hint: null };
 };
+
+export default salaryHint;
