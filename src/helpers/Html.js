8d46761c13d719aee9f6505302af5332f1db06f7
Wei-Ming <barry800414@gmail.com>
2018-08-26 21:39:39 +0800

    update: remove hotjar

--- src/helpers/Html.js
+++ src/helpers/Html.js
@@ -58,20 +58,6 @@ export default class Html extends Component {
             charSet="UTF-8"
           />
           <script src={assets.client.js} charSet="UTF-8" />
-          {/* Hotjar Tracking Code for https://www.goodjob.life --> */}
-          <script
-            // eslint-disable-next-line react/no-danger
-            dangerouslySetInnerHTML={{
-              __html: `(function(h,o,t,j,a,r){
-              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-              h._hjSettings={hjid:648683,hjsv:6};
-              a=o.getElementsByTagName('head')[0];
-              r=o.createElement('script');r.async=1;
-              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-              a.appendChild(r);
-            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
-            }}
-          />
         </body>
       </html>
     );
