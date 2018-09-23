f115117224ee3abdedb3b56d9d22a02d31f53142
Yen-chi Chen <mark86092@gmail.com>
2018-09-23 15:52:01 +0800

    fix result may be undefined

--- src/store/middlewares.js
+++ src/store/middlewares.js
@@ -1,15 +1,19 @@
+const isPromise = obj => {
+  return typeof obj !== 'undefined' && obj.then;
+};
+
 export const errorHandlingMiddleware = ({
   dispatch,
   getState,
 }) => next => action => {
   const result = next(action);
   // if it's a promise.
-  if (result.then) {
+  if (isPromise(result)) {
     return result.catch(error => {
       // this is the default handler for each promise in redux thunk
       if (error.errorCode) {
         // if there exist errorCode, this is an expected error
-      } else if (window && window.Raven) {
+      } else if (typeof window !== 'undefined' && window.Raven) {
         // otherwise, it's unexpected, send to sentry
         window.Raven.captureException(error, {
           extra: {
