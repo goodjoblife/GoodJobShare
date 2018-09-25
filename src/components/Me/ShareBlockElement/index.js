7da39b4c50df30e65a86a8366f10f8afd7f10e9e
Tin <peteranny2@gmail.com>
2018-09-25 11:43:46 +0800

    Update/archive modal (#557)
    * pass archive and display archive status; disabled on archived
    
    * archive modal && toggle button
    
    * archive badge style

--- src/components/Me/ShareBlockElement/index.js
+++ src/components/Me/ShareBlockElement/index.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 
 import { Bookmark } from 'common/icons';
 import { Heading, P } from 'common/base';
+import Modal from 'common/Modal';
 import styles from './ShareBlockElement.module.css';
 
 const ShareBlock = ({
@@ -16,6 +17,9 @@ const ShareBlock = ({
   comment,
   disabled,
   publishHandler,
+  archive,
+  isArchiveModalOpen,
+  setArchiveModalOpen,
 }) => (
   <section
     className={cn(styles.block, {
@@ -63,14 +67,36 @@ const ShareBlock = ({
               {position && <span> - {position}</span>}
             </Link>
           )}
+          {archive &&
+            archive.is_archived && (
+              <span className={cn(styles.badge, styles.archive)}>已封存</span>
+            )}
         </Heading>
       )}
     </div>
-    <div className={styles.buttons}>
-      <button className="buttonCircleS buttonBlack2" onClick={publishHandler}>
-        {disabled ? '重新發佈' : '隱藏'}
-      </button>
-    </div>
+    {(archive &&
+      archive.is_archived && (
+        <div className={styles.buttons}>
+          <button
+            className="buttonCircleS buttonBlack2"
+            onClick={() => setArchiveModalOpen(true)}
+          >
+            封存理由
+          </button>
+          <Modal
+            isOpen={isArchiveModalOpen}
+            close={() => setArchiveModalOpen(false)}
+          >
+            {archive.reason}
+          </Modal>
+        </div>
+      )) || (
+      <div className={styles.buttons}>
+        <button className="buttonCircleS buttonBlack2" onClick={publishHandler}>
+          {disabled ? '重新發佈' : '隱藏'}
+        </button>
+      </div>
+    )}
   </section>
 );
 ShareBlock.propTypes = {
@@ -82,6 +108,34 @@ ShareBlock.propTypes = {
   comment: PropTypes.string,
   disabled: PropTypes.bool,
   publishHandler: PropTypes.func.isRequired,
+  archive: PropTypes.shape({
+    is_archived: PropTypes.bool,
+    reason: PropTypes.string,
+  }),
+  isArchiveModalOpen: PropTypes.bool.isRequired,
+  setArchiveModalOpen: PropTypes.func.isRequired,
 };
 
-export default ShareBlock;
+class ShareBlockWithState extends React.Component {
+  state = {
+    isArchiveModalOpen: false,
+  };
+
+  setArchiveModalOpen = isOpen => {
+    this.setState({
+      isArchiveModalOpen: isOpen,
+    });
+  };
+
+  render() {
+    return (
+      <ShareBlock
+        {...this.props}
+        isArchiveModalOpen={this.state.isArchiveModalOpen}
+        setArchiveModalOpen={this.setArchiveModalOpen}
+      />
+    );
+  }
+}
+
+export default ShareBlockWithState;
