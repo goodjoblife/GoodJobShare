80201954d8502dd3a3e4e87595ccebe7d5482460
Yen-chi Chen <mark86092@gmail.com>
2018-10-25 10:45:27 +0800

    Update

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,10 +1,12 @@
-import React, { Fragment } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { withRouter } from 'react-router';
 import ReactGA from 'react-ga';
 import ReactPixel from 'react-facebook-pixel';
-import { compose, lifecycle } from 'recompose';
+
 import { PermissionContextProvider } from 'common/permission-context';
-import App from '../components/App';
+
+import App from './App';
 import { GA_ID, PIXEL_ID } from '../config';
 
 const logPageView = location => {
@@ -12,39 +14,41 @@ const logPageView = location => {
   ReactGA.pageview(location.pathname + location.search);
 };
 
-const Record = compose(
-  withRouter,
-  lifecycle({
-    componentDidMount() {
-      // initialize google analytics
-      ReactGA.initialize(GA_ID);
+class Root extends Component {
+  componentDidMount() {
+    // initialize google analytics
+    ReactGA.initialize(GA_ID);
 
-      // initialize facebook pixel
-      ReactPixel.init(PIXEL_ID);
-      ReactPixel.pageView();
+    // initialize facebook pixel
+    ReactPixel.init(PIXEL_ID);
+    ReactPixel.pageView();
 
-      const { location } = this.props;
+    const { location } = this.props;
+    logPageView(location);
+  }
 
+  componentDidUpdate(prevProps) {
+    const { location } = this.props;
+    const { location: prevLocation } = prevProps;
+    if (location !== prevLocation) {
       logPageView(location);
-    },
-    componentDidUpdate(prevProps) {
-      const { location } = this.props;
-      const { location: prevLocation } = prevProps;
+    }
+  }
 
-      if (location !== prevLocation) {
-        logPageView(location);
-      }
-    },
-  })
-)(() => null);
+  render() {
+    return (
+      <PermissionContextProvider>
+        <App />
+      </PermissionContextProvider>
+    );
+  }
+}
 
-const Root = () => (
-  <Fragment>
-    <PermissionContextProvider>
-      <App />
-    </PermissionContextProvider>
-    <Record />
-  </Fragment>
-);
+Root.propTypes = {
+  location: PropTypes.shape({
+    pathname: PropTypes.string,
+    search: PropTypes.string,
+  }),
+};
 
-export default Root;
+export default withRouter(Root);
