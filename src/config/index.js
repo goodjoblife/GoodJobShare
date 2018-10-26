a72c4f3e0d8b195b1dcb6f74f9e31ebbfd362356
Yen-chi Chen <mark86092@gmail.com>
2018-10-27 00:39:00 +0800

    Feature: Sentry: Tracking release (#577)
    * Change config setting
    
    * Add SENTRY_DSN
    
    * Track release version in sentry

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,14 @@
-/* eslint-disable global-require */
-if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'test') {
-  module.exports = require('./config.prod');
-} else {
-  module.exports = require('./config.dev');
-}
+/**
+ * Default environment for development
+ */
+module.exports = {
+  API_HOST: process.env.RAZZLE_API_HOST || 'https://api-dev.goodjob.life',
+  CONTENTFUL_API_HOST:
+    process.env.RAZZLE_CONTENTFUL_API_HOST ||
+    'https://content-stage.goodjob.life',
+  FACEBOOK_APP_ID: process.env.RAZZLE_FACEBOOK_APP_ID || '1750608541889151',
+  GA_ID: process.env.RAZZLE_GA_ID || 'UA-79990667-2',
+  PIXEL_ID: process.env.RAZZLE_PIXEL_ID || '603414113402034',
+  SENTRY_DSN: process.env.RAZZLE_SENTRY_DSN,
+  GIT_SHA1: process.env.RAZZLE_GIT_SHA1,
+};
--- src/utils/sentryUtil.js
+++ src/utils/sentryUtil.js
@@ -1,9 +1,24 @@
 import config from '../config';
 
+function isEnabled() {
+  if (process.env.NODE_ENV === 'production') {
+    return true;
+  }
+  return false;
+}
+
 function initSentry() {
-  if (process.env.NODE_ENV === 'production' && config.SENTRY_DSN) {
-    if (window.Raven) {
-      window.Raven.config(config.SENTRY_DSN).install();
+  if (isEnabled()) {
+    if (config.SENTRY_DSN) {
+      if (window.Raven) {
+        if (config.GIT_SHA1) {
+          window.Raven.config(config.SENTRY_DSN, {
+            release: config.GIT_SHA1,
+          }).install();
+        } else {
+          window.Raven.config(config.SENTRY_DSN).install();
+        }
+      }
     }
   }
 }
