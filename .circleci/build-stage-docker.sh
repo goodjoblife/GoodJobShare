d080d2ea52a49e735b60de4f611443286cab9d47
Yen-chi Chen <mark86092@gmail.com>
2019-01-30 20:45:02 +0800

    [CI] Change registry to reg.goodjob.life

--- .circleci/build-stage-docker.sh
+++ .circleci/build-stage-docker.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-REGISTRY="registry.gitlab.com/goodjoblife/i/goodjobshare"
+set -euo pipefail
+
+REGISTRY="reg.goodjob.life/goodjob/goodjobshare"
 
 # Build Docker Image
 docker-compose -f .circleci/docker-compose-stage.yml build
