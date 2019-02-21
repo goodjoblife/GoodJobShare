7377188e3f759e21d970efea465f940856f4da54
Yen-chi Chen <mark86092@gmail.com>
2019-02-21 10:43:43 +0800

    Package alias
    add jsconfig.json to enhance vscode

--- src/razzle-plugins/alias.js
+++ src/razzle-plugins/alias.js
@@ -3,6 +3,10 @@ const path = require('path');
 module.exports = config => {
   // eslint-disable-next-line no-param-reassign
   config.resolve.alias = {
+    // remember to update
+    // .eslintrc.js
+    // jsconfig.json
+    // package.json
     common: path.resolve('./src/components/common'),
     utils: path.resolve('./src/utils'),
     graphql: path.resolve('./src/graphql'),
