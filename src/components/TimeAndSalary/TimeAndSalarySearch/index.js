999048d014f6ea0a8ee7853fc00462e162c963c8
peter <peter@droide-MBP.wireless.droitp.localnet>
2019-03-08 22:10:33 +0800

    search: remove permission fetch

--- src/components/TimeAndSalary/TimeAndSalarySearch/index.js
+++ src/components/TimeAndSalary/TimeAndSalarySearch/index.js
@@ -8,7 +8,6 @@ import qs from 'qs';
 import Loading from 'common/Loader';
 import { P } from 'common/base';
 import FanPageBlock from 'common/FanPageBlock';
-import { withPermission } from 'common/permission-context';
 import WorkingHourBlock from './WorkingHourBlock';
 import { queryKeyword } from '../../../actions/timeAndSalarySearch';
 import { isFetching, isFetched } from '../../../constants/status';
@@ -54,7 +53,6 @@ class TimeAndSalarySearch extends Component {
     history: PropTypes.shape({
       push: PropTypes.func.isRequired,
     }).isRequired,
-    fetchPermission: PropTypes.func.isRequired,
   };
 
   componentDidMount() {
@@ -63,7 +61,6 @@ class TimeAndSalarySearch extends Component {
     this.props
       .queryKeyword({ searchBy, keyword })
       .then(() => this.redirectOnSingleResult());
-    this.props.fetchPermission();
   }
 
   componentDidUpdate(prevProps) {
@@ -80,7 +77,6 @@ class TimeAndSalarySearch extends Component {
       this.props
         .queryKeyword({ searchBy, keyword })
         .then(() => this.redirectOnSingleResult());
-      this.props.fetchPermission();
     }
   }
 
@@ -183,9 +179,6 @@ const ssr = setStatic('fetchData', ({ store: { dispatch }, ...props }) => {
   return dispatch(queryKeyword({ searchBy, keyword }));
 });
 
-const hoc = compose(
-  ssr,
-  withPermission,
-);
+const hoc = compose(ssr);
 
 export default hoc(TimeAndSalarySearch);
