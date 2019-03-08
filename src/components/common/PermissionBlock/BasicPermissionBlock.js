de13c9754ffc1fbdc33d2fa16b2ae05520b4f8a8
peter <peter@droide-MBP.wireless.droitp.localnet>
2019-03-09 01:17:51 +0800

    add modal on simple block

--- src/components/common/PermissionBlock/BasicPermissionBlock.js
+++ src/components/common/PermissionBlock/BasicPermissionBlock.js
@@ -3,12 +3,14 @@ import PropTypes from 'prop-types';
 import cn from 'classnames';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 import faLock from '@fortawesome/fontawesome-free-solid/faLock';
+import { compose, withState, withHandlers } from 'recompose';
 
 import P from 'common/base/P';
 import { Heading } from 'common/base';
 import CallToLoginShareButton from '../../../containers/PermissionBlock/CallToLoginShareButtonContainer';
 import getScale from '../../../utils/numberUtils';
 import styles from './PermissionBlock.module.css';
+import Modal from 'common/Modal';
 
 class BasicPermissionBlock extends React.Component {
   static propTypes = {
@@ -24,6 +26,8 @@ class BasicPermissionBlock extends React.Component {
     queryExperienceCount: PropTypes.func.isRequired,
     queryTimeAndSalaryCount: PropTypes.func.isRequired,
     queryLaborRightsCount: PropTypes.func.isRequired,
+    isModalOpen: PropTypes.bool.isRequired,
+    toggleModal: PropTypes.func.isRequired,
   };
 
   static defaultProps = {
@@ -45,27 +49,67 @@ class BasicPermissionBlock extends React.Component {
   }
 
   render() {
-    const { simple, rootClassName } = this.props;
+    const {
+      simple,
+      to,
+      rootClassName,
+      experienceCount,
+      timeAndSalaryCount,
+      laborRightsCount,
+    } = this.props;
+
+    const experienceScale = getScale(experienceCount);
+    const timeAndSalaryScale = getScale(timeAndSalaryCount);
+
     if (simple) {
+      const { isModalOpen, toggleModal } = this.props;
       return (
-        <div className={cn(styles.permissionBlock, rootClassName)}>
+        <div
+          className={cn(styles.permissionBlock, rootClassName)}
+          onClick={toggleModal}
+        >
           <div className={styles.container}>
             <div className={styles.headingContainer}>
               <FontAwesomeIcon icon={faLock} className={styles.headingIcon} />
+              <Modal
+                isOpen={isModalOpen}
+                close={toggleModal}
+                closableOnClickOutside
+              >
+                <div className={styles.headingContainer}>
+                  <Heading size="sl" Tag="h3">
+                    你暫時沒有查看完整資訊的權限
+                  </Heading>
+                </div>
+                <P size="l" className={styles.ctaText}>
+                  只需幾分鐘，
+                  <strong>分享你的職場資訊</strong>
+                  ，不僅幫助其他人不再踩雷，更能
+                  <strong>{`查看全站超過 ${timeAndSalaryScale} 筆`}</strong>
+                  薪資工時資訊、
+                  <strong>{`${experienceScale}+ 篇`}</strong>
+                  面試及工作經驗分享，以及
+                  <strong>{` ${laborRightsCount} 篇`}</strong>
+                  勞動權益懶人包哦！
+                </P>
+                <br />
+                <P size="l">
+                  <strong>若你已經分享過資訊，登入即可查看全文！</strong>
+                </P>
+                <div className={styles.ctaButtonContainer}>
+                  <CallToLoginShareButton
+                    to={to}
+                    notLoginText="立即登入並分享"
+                    isLoginText="立即分享"
+                  />
+                </div>
+              </Modal>
             </div>
           </div>
         </div>
       );
     }
 
-    const {
-      to,
-      experienceCount,
-      timeAndSalaryCount,
-      laborRightsCount,
-    } = this.props;
-    const experienceScale = getScale(experienceCount);
-    const timeAndSalaryScale = getScale(timeAndSalaryCount);
     return (
       <div className={cn(styles.permissionBlock, rootClassName)}>
         <div className={styles.container}>
@@ -103,4 +147,13 @@ class BasicPermissionBlock extends React.Component {
   }
 }
 
-export default BasicPermissionBlock;
+const enhance = compose(
+  withState('isModalOpen', 'setModalOpen', false),
+  withHandlers({
+    toggleModal: ({ isModalOpen, setModalOpen }) => () => {
+      setModalOpen(!isModalOpen);
+    },
+  }),
+);
+
+export default enhance(BasicPermissionBlock);
