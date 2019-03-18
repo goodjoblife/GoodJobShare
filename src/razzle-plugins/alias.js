abb1507b307073443643eb3dab0e2b1f18b44361
Yen-chi Chen <mark86092@gmail.com>
2019-03-18 13:36:54 +0800

    Update flow (#627)
    * update flow
    
    * update flow-typed
    
    `npx flow-typed install express ramda`

--- src/razzle-plugins/alias.js
+++ src/razzle-plugins/alias.js
@@ -7,6 +7,7 @@ module.exports = config => {
     // .eslintrc.js
     // jsconfig.json
     // package.json
+    // .flowconfig
     common: path.resolve('./src/components/common'),
     utils: path.resolve('./src/utils'),
     graphql: path.resolve('./src/graphql'),
