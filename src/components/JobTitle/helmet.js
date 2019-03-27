db988bd8e279b30de18b02bc069bbee0c4176a71
Wei-Ming Chen <barry800414@gmail.com>
2019-03-27 23:29:14 +0800

    update: update JobTitle SalaryWorkTime helmet

--- src/components/JobTitle/helmet.js
+++ src/components/JobTitle/helmet.js
@@ -3,16 +3,27 @@ import Helmet from 'react-helmet';
 import { formatTitle, formatCanonicalPath } from 'utils/helmetHelper';
 import { imgHost, SITE_NAME } from '../../constants/helmetData';
 
-export default ({ title, pathname, jobTitle }) => {
+export default ({
+  title,
+  pathname,
+  page,
+  jobTitle,
+  dataNum,
+  avgWeekWorkTime,
+  avgHourWage,
+}) => {
+  const helmetTitle = `${title} - 第${page}頁`;
   const url = formatCanonicalPath(pathname);
-  const description = `馬上查看${jobTitle}的薪資、工時資訊以及加班狀況，協助您找到更好的工作！`;
+  const description = `${jobTitle} 的薪水平均 ${avgHourWage} 元/小時，平均每週工作 ${avgWeekWorkTime} 小時，總共 ${dataNum} 筆的薪水、加班狀況資料。`;
+  const keywords = `${jobTitle}薪水, ${jobTitle}薪資, ${jobTitle}加班狀況, ${jobTitle}工時`;
 
   return (
     <Helmet
-      title={title}
+      title={helmetTitle}
       meta={[
         { name: 'description', content: description },
-        { property: 'og:title', content: formatTitle(title, SITE_NAME) },
+        { name: 'keywords', content: keywords },
+        { property: 'og:title', content: formatTitle(helmetTitle, SITE_NAME) },
         { property: 'og:description', content: description },
         { property: 'og:url', content: url },
         {
