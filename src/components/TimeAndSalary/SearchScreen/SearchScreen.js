4c63dc1e08d4be5205a859dac00bd338bb755e7e
Wei-Ming Chen <barry800414@gmail.com>
2019-03-27 23:29:40 +0800

    update: update SalaryWorkTime Search page helmet

--- src/components/TimeAndSalary/SearchScreen/SearchScreen.js
+++ src/components/TimeAndSalary/SearchScreen/SearchScreen.js
@@ -47,7 +47,7 @@ function getTitle(keyword) {
     if (keyword.length < keywordMinLength) {
       return '請輸入更長的搜尋關鍵字';
     } else {
-      return `查詢「${keyword}」的結果`;
+      return `查詢「${keyword}」薪水的結果`;
     }
   } else {
     return '請輸入搜尋條件！';
@@ -153,7 +153,7 @@ class SearchScreen extends Component {
 
     return (
       <section className={styles.searchResult}>
-        {renderHelmet({ title, pathname, keyword })}
+        {renderHelmet({ title, pathname, page, keyword })}
         <h2 className={styles.heading}>{title}</h2>
         {isFetching(status) && <Loading size="s" />}
         {isFetched(status) &&
--- src/components/TimeAndSalary/SearchScreen/helmet.js
+++ src/components/TimeAndSalary/SearchScreen/helmet.js
@@ -3,16 +3,17 @@ import Helmet from 'react-helmet';
 import { formatTitle, formatCanonicalPath } from 'utils/helmetHelper';
 import { imgHost, SITE_NAME } from '../../../constants/helmetData';
 
-export default ({ title, pathname, keyword }) => {
+export default ({ title, pathname, page, keyword }) => {
+  const helmetTitle = `${title} - 第${page}頁`;
   const url = formatCanonicalPath(pathname);
-  const description = `馬上查看${keyword}的薪資、工時資訊以及加班狀況，協助您找到更好的工作！`;
+  const description = `馬上查看${keyword}的薪水、工時資訊以及加班狀況，協助您找到更好的工作！`;
 
   return (
     <Helmet
-      title={title}
+      title={helmetTitle}
       meta={[
         { name: 'description', content: description },
-        { property: 'og:title', content: formatTitle(title, SITE_NAME) },
+        { property: 'og:title', content: formatTitle(helmetTitle, SITE_NAME) },
         { property: 'og:description', content: description },
         { property: 'og:url', content: url },
         {
