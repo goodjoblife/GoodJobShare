00262dc0aa04e5dd257fb0d9fbed28af2efe8e51
Wei-Ming Chen <barry800414@gmail.com>
2019-03-29 01:51:43 +0800

    fix: feed statistic data to OvertimeBlock correctly

--- src/components/TimeAndSalary/common/OvertimeBlock.js
+++ src/components/TimeAndSalary/common/OvertimeBlock.js
@@ -12,13 +12,13 @@ const formatNum = num => {
   return num;
 };
 
-const OvertimeBlock = ({ type, heading, data }) => (
+const OvertimeBlock = ({ type, heading, statistics }) => (
   <div className={styles.overtimeBlock}>
     <P Tag="h4" size="m" bold className={styles.heading}>
       {type === 'salary' ? <Coin2 /> : <Clock />}
       <span>{heading}</span>
     </P>
-    {data.count > 5 ? (
+    {statistics.count >= 5 ? (
       <div className={styles.item}>
         <div className={styles.positive}>
           <div>
@@ -28,27 +28,27 @@ const OvertimeBlock = ({ type, heading, data }) => (
                 <li>
                   <div className={styles.statHeading}>優於或符合勞基法</div>
                   <div className={styles.num}>
-                    {formatNum(data.is_overtime_salary_legal_count.ye)}
+                    {formatNum(statistics.is_overtime_salary_legal_count.yes)}
                   </div>
                 </li>
                 <li>
                   <div className={styles.statHeading}>不符合勞基法</div>
                   <div className={styles.num}>
-                    {formatNum(data.is_overtime_salary_legal_count.no)}
+                    {formatNum(statistics.is_overtime_salary_legal_count.no)}
                   </div>
                 </li>
                 <li>
                   <div className={styles.statHeading}>不清楚是否符合勞基法</div>
                   <div className={styles.num}>
                     {formatNum(
-                      data.is_overtime_salary_legal_count["don't know"],
+                      statistics.is_overtime_salary_legal_count.unknown,
                     )}
                   </div>
                 </li>
               </ul>
             ) : (
               <div className={styles.num}>
-                {formatNum(data.has_compensatory_dayoff_count.yes)}
+                {formatNum(statistics.has_compensatory_dayoff_count.yes)}
               </div>
             )}
           </div>
@@ -59,16 +59,16 @@ const OvertimeBlock = ({ type, heading, data }) => (
               <div className={styles.statName}>沒有</div>
               <div className={styles.num}>
                 {type === 'salary'
-                  ? formatNum(data.has_overtime_salary_count.no)
-                  : formatNum(data.has_compensatory_dayoff_count.no)}
+                  ? formatNum(statistics.has_overtime_salary_count.no)
+                  : formatNum(statistics.has_compensatory_dayoff_count.no)}
               </div>
             </div>
             <div className={styles.negativeStat}>
               <div className={styles.statName}>不知道</div>
               <div className={styles.num}>
                 {type === 'salary'
-                  ? formatNum(data.has_overtime_salary_count["don't know"])
-                  : formatNum(data.has_compensatory_dayoff_count["don't know"])}
+                  ? formatNum(statistics.has_overtime_salary_count.unknown)
+                  : formatNum(statistics.has_compensatory_dayoff_count.unknown)}
               </div>
             </div>
           </div>
@@ -82,7 +82,7 @@ const OvertimeBlock = ({ type, heading, data }) => (
 
 OvertimeBlock.propTypes = {
   type: PropTypes.oneOf(['salary', 'dayoff']).isRequired,
-  data: PropTypes.object.isRequired,
+  statistics: PropTypes.object.isRequired,
   heading: PropTypes.string.isRequired,
 };
 
