57617f8007a344d412bc7be6442a7abf283d1244
Tin <peteranny2@gmail.com>
2019-04-03 13:31:18 +0800

    inject rwd columns (#646)

--- src/components/TimeAndSalary/common/injectHideContentBlock.js
+++ src/components/TimeAndSalary/common/injectHideContentBlock.js
@@ -1,31 +1,42 @@
 import React from 'react';
 import BasicPermissionBlock from '../../../containers/PermissionBlock/BasicPermissionBlockContainer';
 import styles from './injectHideContentBlock.module.css';
+import cn from 'classnames';
 
 export default hideRange => rows => {
   const hideIndex = hideRange[0];
   const nHides = hideRange.length;
 
-  rows.forEach((row, i) => {
-    if (i === 0) {
-      row.props.children.splice(
-        hideIndex,
-        nHides,
-        <td
-          data-th="薪資"
-          key="__hideContent"
-          colSpan={nHides}
-          rowSpan={rows.length}
-          className={styles.cell}
-        >
+  rows.forEach(row => {
+    row.props.children.splice(
+      hideIndex,
+      nHides,
+      ...row.props.children.slice(hideIndex, hideIndex + nHides).map(col => {
+        return React.cloneElement(
+          col,
+          {
+            className: cn(col.props.className, styles.cell, styles.mobile),
+          },
           <BasicPermissionBlock
             rootClassName={styles.hideContentBlock}
             simple
-          />
-        </td>,
-      );
-      return;
-    }
-    rows[i].props.children.splice(hideIndex, nHides);
+          />,
+        );
+      }),
+    );
   });
+  if (rows.length > 0) {
+    rows[0].props.children.splice(
+      hideIndex,
+      0,
+      <td
+        key="__hideContent"
+        colSpan={nHides}
+        rowSpan={rows.length}
+        className={styles.cell}
+      >
+        <BasicPermissionBlock rootClassName={styles.hideContentBlock} simple />
+      </td>,
+    );
+  }
 };
