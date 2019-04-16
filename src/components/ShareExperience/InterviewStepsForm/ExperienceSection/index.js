c1109dfb30b588e16df235c02f9b3f5b51463932
peteranny <peteranny2@gmail.com>
2019-04-17 00:17:04 +0800

    init experience-section

--- src/components/ShareExperience/InterviewStepsForm/ExperienceSection/index.js
+++ src/components/ShareExperience/InterviewStepsForm/ExperienceSection/index.js
@@ -1,37 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import ButtonAdd from 'common/button/ButtonAdd';
-import AddButton from 'common/button/AddButton';
-import { Comment2 } from 'common/icons';
 import subscribeValidation from 'common/subscribeValidation';
-import IconHeadingBlock from 'common/IconHeadingBlock';
+import Block from 'common/Block';
 
-import Title from '../../common/Title';
 import Sections from '../../common/Sections';
-import InterviewQas from './InterviewQas';
-import InterviewSensitiveQuestions from './InterviewSensitiveQuestions';
 
-import shareStyles from '../../common/share.module.css';
+import { sections as sectionsValidator } from '../formCheck';
 
-import {
-  title as titleValidator,
-  sections as sectionsValidator,
-  interviewSensitiveQuestions as interviewSensitiveQuestionsValidator,
-} from '../formCheck';
-
-import { interviewSectionSubtitleOptions } from '../../common/optionMap';
-
-import {
-  TITLE,
-  SECTIONS,
-  INTERVIEW_SENSITIVE_QUESTIONS,
-} from '../../../../constants/formElements';
-
-const TitleWithValidation = subscribeValidation(
-  Title,
-  props => props.validator(props.title),
-  TITLE,
-);
+import { SECTIONS } from '../../../../constants/formElements';
 
 const SectionsWithValidation = subscribeValidation(
   Sections,
@@ -39,50 +15,17 @@ const SectionsWithValidation = subscribeValidation(
   SECTIONS,
 );
 
-const InterviewSensitiveQuestionsWithValidation = subscribeValidation(
-  InterviewSensitiveQuestions,
-  props => props.validator(props.interviewSensitiveQuestions),
-  INTERVIEW_SENSITIVE_QUESTIONS,
-);
-
-class InterviewExperience extends Component {
+class ExperienceSection extends Component {
   render() {
     const {
-      handleState,
-      title,
       sections,
-      appendSection,
       removeSection,
       editSection,
-      interviewQas,
-      appendQa,
-      removeQa,
-      editQa,
-      interviewSensitiveQuestions,
       submitted,
       changeValidationStatus,
     } = this.props;
     return (
-      <IconHeadingBlock
-        heading="面試經驗"
-        Icon={Comment2}
-        marginTop
-        requiredText
-      >
-        <div
-          style={{
-            marginBottom: '50px',
-          }}
-        >
-          <TitleWithValidation
-            title={title}
-            onChange={handleState('title')}
-            placeholder="ＯＯ 股份有限公司面試經驗分享"
-            validator={titleValidator}
-            submitted={submitted}
-            changeValidationStatus={changeValidationStatus}
-          />
-        </div>
+      <Block heading="當時面試過程是如何呢？（＋100 積分）">
         <div
           style={{
             position: 'relative',
@@ -97,57 +40,13 @@ class InterviewExperience extends Component {
             submitted={submitted}
             changeValidationStatus={changeValidationStatus}
           />
-          <div className={shareStyles.button__add}>
-            <ButtonAdd
-              options={interviewSectionSubtitleOptions}
-              custimizedValues={[interviewSectionSubtitleOptions[0].value]}
-              disabledValues={sections.map(section => section.subtitle)}
-              appendBlock={appendSection}
-            />
-          </div>
         </div>
-        <hr
-          style={{
-            border: '1px solid #E7E7E7',
-            marginBottom: '35px',
-          }}
-        />
-        <div
-          style={{
-            position: 'relative',
-            marginBottom: '80px',
-          }}
-        >
-          <InterviewQas
-            interviewQas={interviewQas}
-            editQa={editQa}
-            removeQa={removeQa}
-          />
-          <div className={shareStyles.button__add}>
-            <AddButton onClick={() => appendQa()} addQa />
-          </div>
-        </div>
-        <hr
-          style={{
-            border: '1px solid #E7E7E7',
-            marginBottom: '35px',
-          }}
-        />
-        <div>
-          <InterviewSensitiveQuestionsWithValidation
-            interviewSensitiveQuestions={interviewSensitiveQuestions}
-            onChange={handleState('interviewSensitiveQuestions')}
-            submitted={submitted}
-            validator={interviewSensitiveQuestionsValidator}
-            changeValidationStatus={changeValidationStatus}
-          />
-        </div>
-      </IconHeadingBlock>
+      </Block>
     );
   }
 }
 
-InterviewExperience.propTypes = {
+ExperienceSection.propTypes = {
   handleState: PropTypes.func,
   title: PropTypes.string,
   sections: PropTypes.arrayOf(
@@ -176,4 +75,4 @@ InterviewExperience.propTypes = {
   changeValidationStatus: PropTypes.func,
 };
 
-export default InterviewExperience;
+export default ExperienceSection;
