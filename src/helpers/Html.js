062d29ab3c811b627ac594f89ae2831a51d732fa
Ming <barry800414@users.noreply.github.com>
2019-04-29 22:57:57 +0800

    Feature: add Google Tag Manager (#667)
    * feat: add Google Tag Manager
    
    * fix: add env to dockerFile

--- src/helpers/Html.js
+++ src/helpers/Html.js
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom/server';
 import serialize from 'serialize-javascript';
 import Helmet from 'react-helmet';
 
+import { GTM_ID } from '../config';
+
 /**
  * Wrapper component containing HTML metadata and boilerplate tags.
  * Used in server-side code only to wrap the string output of the
@@ -48,8 +50,28 @@ export default class Html extends Component {
             src="https://cdn.ravenjs.com/3.26.4/raven.min.js"
             crossOrigin="anonymous"
           />
+          {/* install google tag manager */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${GTM_ID}');`,
+            }}
+          />
         </head>
         <body>
+          {/* install google tag manager */}
+          <noscript>
+            {/* eslint-disable-next-line jsx-a11y/iframe-has-title */}
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            />
+          </noscript>
           <div
             id="root"
             // eslint-disable-next-line react/no-danger
