eddd6fd3ac65f8941086eaa6de94086e05bf3ef5
施亭屹 <peteranny2@gmail.com>
2019-06-26 00:43:05 +0800

    search-no-result

--- src/components/CompanyAndJobTitle/InterviewExperiences/index.js
+++ src/components/CompanyAndJobTitle/InterviewExperiences/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Section, P } from 'common/base';
 import CompanyAndJobTitleWrapper from '../CompanyAndJobTitleWrapper';
+import styles from './InterviewExperiences.module.css';
 
 const InterviewExperiences = ({ pageType, pageName, tabName }) => (
   <CompanyAndJobTitleWrapper>
-    <h1>InterviewExperiences</h1>
-    <p>{pageType}</p>
-    <p>{pageName}</p>
-    <p>{tabName}</p>
+    <Section Tag="main" paddingBottom>
+      <P size="l" bold className={styles.searchNoResult}>
+        尚未有「聯發科股份有限公司」的經驗分享
+        <br />
+        搜尋看看其他？
+      </P>
+    </Section>
   </CompanyAndJobTitleWrapper>
 );
 
