cff00d63c1575945a5288220cd9b0f7a16cc44a9
wendellliu <cuk.bas@gmail.com>
2019-06-26 16:01:58 +0800

    remove flow annotation

--- src/components/CompanyAndJobTitle/BreadCrumb.js
+++ src/components/CompanyAndJobTitle/BreadCrumb.js
@@ -1,12 +1,5 @@
-// @flow
 import React from 'react';
 
-type Props = {
-  pageType: string,
-  pageName: string,
-  tabName: string,
-};
-
-const BreadCrumb = (props: Props) => <div>BreadCrumb</div>;
+const BreadCrumb = props => <div>BreadCrumb</div>;
 
 export default BreadCrumb;
