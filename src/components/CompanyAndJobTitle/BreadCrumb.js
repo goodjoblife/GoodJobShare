e0dd5a24ceb66182a63c3249ee9f2ec340dbee59
wendellliu <cuk.bas@gmail.com>
2019-07-01 18:06:39 +0800

    url map

--- src/components/CompanyAndJobTitle/BreadCrumb.js
+++ src/components/CompanyAndJobTitle/BreadCrumb.js
@@ -1,11 +1,42 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+
+import {
+  pageTypeTranslation,
+  tabTypeTranslation,
+  pageTypeURLMap,
+  tabTypeURLMap,
+} from '../../constants/companyJobTitle';
+
+const CrumbLink = ({ children, to }) => (
+  <Link to={to}>
+    <span
+      style={{
+        color: '#555555',
+        fontSize: '13px',
+      }}
+    >
+      {children}
+    </span>
+  </Link>
+);
 
 const BreadCrumb = ({ pageType, pageName, tabType }) => (
   <div>
-    <p>{pageType}</p>
-    <p>{pageName}</p>
-    <p>{tabType}</p>
+    <CrumbLink to="/salary-work-times/latest">
+      {pageTypeTranslation[pageType]}
+    </CrumbLink>
+    {' > '}
+    <CrumbLink to={`/${pageTypeURLMap[pageType]}/${pageName}`}>
+      {pageName}
+    </CrumbLink>
+    {' > '}
+    <CrumbLink
+      to={`/${pageTypeURLMap[pageType]}/${pageName}/${tabTypeURLMap[tabType]}`}
+    >
+      {tabTypeTranslation[tabType]}
+    </CrumbLink>
   </div>
 );
 
--- src/constants/companyJobTitle.js
+++ src/constants/companyJobTitle.js
@@ -1,7 +1,39 @@
-export const pageType = { JOB_TITLE: 'JOB_TITLE', COMPANY: 'COMPANY' };
+const JOB_TITLE = 'JOB_TITLE';
+const COMPANY = 'COMPANY';
+
+export const pageType = { JOB_TITLE, COMPANY };
+export const pageTypeTranslation = {
+  [JOB_TITLE]: '職稱',
+  [COMPANY]: '公司',
+};
+
+export const pageTypeURLMap = {
+  [JOB_TITLE]: 'job-titles',
+  [COMPANY]: 'companies',
+};
+
+const OVERVIEW = 'OVERVIEW';
+const TIME_AND_SALARY = 'TIME_AND_SALARY';
+const WORK_EXPERIENCE = 'WORK_EXPERIENCE';
+const INTERVIEW_EXPERIENCE = 'INTERVIEW_EXPERIENCE';
+
 export const tabType = {
-  OVERVIEW: 'OVERVIEW',
-  TIME_AND_SALARY: 'TIME_AND_SALARY',
-  WORK_EXPERIENCE: 'WORK_EXPERIENCE',
-  INTERVIEW_EXPERIENCE: 'INTERVIEW_EXPERIENCE',
+  OVERVIEW,
+  TIME_AND_SALARY,
+  WORK_EXPERIENCE,
+  INTERVIEW_EXPERIENCE,
+};
+
+export const tabTypeTranslation = {
+  [OVERVIEW]: '總覽',
+  [TIME_AND_SALARY]: '薪水&加班狀況',
+  [WORK_EXPERIENCE]: '工作心得',
+  [INTERVIEW_EXPERIENCE]: '面試心得',
+};
+
+export const tabTypeURLMap = {
+  [OVERVIEW]: '',
+  [TIME_AND_SALARY]: 'salary-work-times',
+  [WORK_EXPERIENCE]: 'work-experiences',
+  [INTERVIEW_EXPERIENCE]: 'interview-experiences',
 };
