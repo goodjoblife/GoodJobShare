04779d8849d29e20f7b6c31241423d42aed2be6b
wendellliu <cuk.bas@gmail.com>
2019-07-01 18:24:20 +0800

    helmet

--- src/components/CompanyAndJobTitle/CompanyAndJobTitleWrapper.js
+++ src/components/CompanyAndJobTitle/CompanyAndJobTitleWrapper.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import Helmet from 'react-helmet';
 import Heading from 'common/base/Heading';
+
+import { formatTitle } from 'utils/helmetHelper';
+import { SITE_NAME } from '../../constants/helmetData';
+import { tabTypeTranslation } from '../../constants/companyJobTitle';
 
 import BreadCrumb from './BreadCrumb';
 
@@ -11,15 +15,23 @@ const CompanyAndJobTitleWrapper = ({
   pageName,
   tabType,
 }) => {
+  const helmetTitle = `${pageName} ${tabTypeTranslation[tabType]}`;
   return (
     <div>
+      <Helmet>
+        <title itemProp="name" lang="zh-TW">
+          {helmetTitle}
+        </title>
+        <meta
+          property="og:title"
+          content={formatTitle(helmetTitle, SITE_NAME)}
+        />
+      </Helmet>
       <BreadCrumb
         pageType={pageType}
         pageName={pageName}
         tabType={tabType}
-        style={{
-          marginBottom: '20px',
-        }}
+        style={{ marginBottom: '20px' }}
       />
       <Heading style={{ color: '#000000' }}>{pageName}</Heading>
     </div>
