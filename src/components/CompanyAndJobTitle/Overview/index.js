54e8cb7508ad1be9238f66501575c39bf71b496a
peteranny <peteranny2@gmail.com>
2019-08-06 22:30:50 +0800

    move overview => overview/index

--- src/components/CompanyAndJobTitle/Overview/index.js
+++ src/components/CompanyAndJobTitle/Overview/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import CompanyAndJobTitleWrapper from './CompanyAndJobTitleWrapper';
+import CompanyAndJobTitleWrapper from '../CompanyAndJobTitleWrapper';
 
 const Overview = ({ pageType, pageName, tabType }) => (
   <CompanyAndJobTitleWrapper
