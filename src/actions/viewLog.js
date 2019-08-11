659db939cfd268c9b43e4636ae3701a02b6be10d
Wei-Ming Chen <barry800414@gmail.com>
2019-08-11 13:48:08 +0800

    feat: tracking viewing experience detail log

--- src/actions/viewLog.js
+++ src/actions/viewLog.js
@@ -9,3 +9,13 @@ export const viewSalaryWorkTimes = ({ contentIds, referrer }) => (
   const token = tokenSelector(state);
   return api.viewLog.viewSalaryWorkTimes({ token, contentIds, referrer });
 };
+
+export const viewExperiences = ({ contentIds, referrer }) => (
+  dispatch,
+  getState,
+  { api },
+) => {
+  const state = getState();
+  const token = tokenSelector(state);
+  return api.viewLog.viewExperiences({ token, contentIds, referrer });
+};
