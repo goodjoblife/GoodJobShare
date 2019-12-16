403b28917c8b8c2e36370c6044a6e4e740cb7e65
Wei Ming Chen <barry800414@gmail.com>
2019-12-16 15:24:42 +0800

    refactor: wording

--- src/components/TimeAndSalary/SearchScreen/Helmet.js
+++ src/components/TimeAndSalary/SearchScreen/Helmet.js
@@ -5,7 +5,7 @@ import { SITE_NAME } from '../../../constants/helmetData';
 
 const SearchScreenHelmet = ({ keyword, page }) => {
   const title = `查詢${keyword}的結果 - 第${page}頁`;
-  const description = `查看${keyword}的薪水、加班狀況、面試心得，以及工作心得，協助您找到更好的工作！`;
+  const description = `查詢${keyword}的薪水、加班狀況、面試心得、工作心得資料的結果`;
 
   let url = formatCanonicalPath(`/salary-work-times?q=${keyword}`);
   if (page > 1) {
