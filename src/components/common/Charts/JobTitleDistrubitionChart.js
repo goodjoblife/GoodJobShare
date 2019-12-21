d0b9eb9956d096e85e101833bea454f6d47c07f7
Ming <barry800414@users.noreply.github.com>
2019-12-22 00:36:42 +0800

    Hotfix/chart rwd (#811)
    * hotfix: fix Banner background image temporally
    
    * feat: add lodash
    
    * feat: detect window size change and dispatch event
    
    * feat: add HOC for connecting window size
    
    * hotfix: using window size variables to break words and change font-size for Y-axis
    
    * Use react-use
    
    * clean
    
    * Fix bug!!!
    
    Co-authored-by: Yen-chi Chen <mark86092@gmail.com>

--- src/components/common/Charts/JobTitleDistrubitionChart.js
+++ src/components/common/Charts/JobTitleDistrubitionChart.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import { BarChart, Bar, XAxis, YAxis, ResponsiveContainer } from 'recharts';
 import R from 'ramda';
+import { useWindowSize } from 'react-use';
+import breakpoints from '../../../constants/breakpoints';
 
 const maxNameLength = R.pipe(
   R.map(
@@ -14,19 +15,53 @@ const maxNameLength = R.pipe(
   R.reduce(R.max, -Infinity),
 );
 
-const JobTitleDistributionChart = ({ data }) => (
-  <ResponsiveContainer>
-    <BarChart
-      data={data}
-      layout="vertical"
-      margin={{ left: maxNameLength(data) * 10, bottom: -25 }}
-    >
-      <XAxis type="number" label="平均月薪" height={70} />
-      <YAxis type="category" dataKey="job_title.name" />
-      <Bar dataKey="average_salary.amount" fill="#fcd406" />
-    </BarChart>
-  </ResponsiveContainer>
-);
+const YAxisTickFormatter = (str, perNWord = 4) => {
+  let newStr = '';
+  let count = 0;
+  for (let i = 0; i < str.length; i += 1) {
+    const char = str.charAt(i);
+    if (char === ' ') {
+      newStr += char;
+      count = 0;
+    } else {
+      if (count === perNWord) {
+        newStr += ' ';
+        newStr = newStr + char;
+        count = 1;
+      } else {
+        newStr = newStr + char;
+        count += 1;
+      }
+    }
+  }
+  return newStr;
+};
+
+const JobTitleDistributionChart = ({ data }) => {
+  const { width } = useWindowSize();
+
+  return (
+    <ResponsiveContainer>
+      <BarChart
+        data={data}
+        layout="vertical"
+        margin={{
+          left: width < breakpoints.xs ? null : maxNameLength(data) * 10,
+          bottom: -25,
+        }}
+      >
+        <XAxis type="number" label="平均月薪" height={70} />
+        <YAxis
+          type="category"
+          dataKey="job_title.name"
+          tickFormatter={width < breakpoints.xs ? YAxisTickFormatter : null}
+          tick={{ fontSize: width < breakpoints.xs ? '12px' : null }}
+        />
+        <Bar dataKey="average_salary.amount" fill="#fcd406" />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
 
 JobTitleDistributionChart.propTypes = {
   data: PropTypes.arrayOf(
