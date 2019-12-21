5e801b03a05c47c76233bdb47193147355bf73cc
Wei Ming Chen <barry800414@gmail.com>
2019-12-22 02:53:50 +0800

    hotfix: fix label and tick of Chart

--- src/components/common/Charts/CompanyDistributionChart.js
+++ src/components/common/Charts/CompanyDistributionChart.js
@@ -1,24 +1,62 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useWindowSize } from 'react-use';
 
 import { BarChart, Bar, XAxis, YAxis, ResponsiveContainer } from 'recharts';
+import breakpoints from '../../../constants/breakpoints';
 
-const CompanyDistributionChart = ({ data }) => (
-  <ResponsiveContainer>
-    <BarChart data={data} margin={{ left: -30, bottom: -25 }}>
-      <XAxis
-        type="category"
-        dataKey={({ range: { from, to } }) =>
-          `${Math.floor(from / 10000)}萬~${Math.floor(to / 10000)}萬`
-        }
-        label="月薪"
-        height={70}
-      />
-      <YAxis type="number" label="人數" width={100} />
-      <Bar dataKey="data_count" fill="#fcd406" />
-    </BarChart>
-  </ResponsiveContainer>
-);
+const CustomizedLabel = ({ viewBox: { x, y, width, height } }) => {
+  return (
+    <g transform={`translate(${x + width / 2},${y + height - 20})`}>
+      <text x={0} y={0} dy={16} fill="#000">
+        <tspan textAnchor="middle" x="0">
+          月薪
+        </tspan>
+      </text>
+    </g>
+  );
+};
+
+const CustomizedAxisTick = ({ x, y, stroke, payload }) => {
+  const [from, to] = payload.value.split('~');
+  return (
+    <g transform={`translate(${x},${y})`}>
+      <text x={0} y={0} dy={16} fill="#666">
+        <tspan textAnchor="middle" x="0">
+          {from}~
+        </tspan>
+        <tspan textAnchor="middle" x="0" dy="20">
+          {to}
+        </tspan>
+      </text>
+    </g>
+  );
+};
+
+const CompanyDistributionChart = ({ data }) => {
+  const { width } = useWindowSize();
+  return (
+    <ResponsiveContainer>
+      <BarChart
+        data={data}
+        margin={{ left: -30, bottom: width < breakpoints.xs ? 0 : -25 }}
+      >
+        <XAxis
+          type="category"
+          dataKey={({ range: { from, to } }) =>
+            `${Math.floor(from / 1000) / 10}萬~${Math.floor(to / 1000) / 10}萬`
+          }
+          label={width < breakpoints.xs ? <CustomizedLabel /> : '月薪'}
+          height={70}
+          tick={width < breakpoints.xs ? <CustomizedAxisTick /> : {}}
+          interval={0}
+        />
+        <YAxis type="number" label="人數" width={100} />
+        <Bar dataKey="data_count" fill="#fcd406" />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
 
 CompanyDistributionChart.propTypes = {
   data: PropTypes.arrayOf(
