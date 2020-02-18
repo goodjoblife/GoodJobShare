ddfaff50924cd2a7fc305cedac53eb218890f70b
Wei Ming Chen <barry800414@gmail.com>
2020-02-18 23:28:48 +0800

    refactor: make it as functional component

--- src/components/common/FormBuilder/index.js
+++ src/components/common/FormBuilder/index.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import {
   string,
   bool,
@@ -45,43 +44,43 @@ const QuestionShape = shape({
   renderCustomizedQuestion: func,
 });
 
-class FormBuilder extends React.Component {
-  static propTypes = {
-    // 問卷標題
-    title: string.isRequired,
-    // 問卷描述
-    description: oneOfType([string, element]),
-    // 上傳按鈕的文字
-    submitButtonText: oneOfType([string, element]).isRequired,
-    // 上傳按鈕是否可按
-    submitButtonEnabled: bool.isRequired,
-    // 問題列表
-    questions: arrayOf(QuestionShape),
-    // 排版方式，目前只有一種，就是 typeform
-    layout: string.isRequired,
-    // 錯誤訊息的 Modal
-    errorModal: oneOfType([string, element]),
-    // 當使用者填寫內容，此函數會被觸發，且 emit 一個 object，包含被修改欄位的 key & value
-    onChange: func.isRequired,
-    // 當使用者按下送出鈕，且通過所有 question validator 驗證，此函數會被觸發
-    onSubmit: func.isRequired,
-    // 當使用者按下送出鈕，但有 question validator 驗證未過，此函數會被觸發
-    onValidateFail: func,
-    // 點擊下一步可額外觸發的函數 (可用於數據追蹤)
-    onNext: func,
-    // 點擊上一步考額外觸發的函數 (可用於數據追蹤)
-    onPrev: func,
-    // 點擊使用者條款 checkbox
-    onClickAgreement: func,
-    // 點擊關閉表單按鈕觸發的函數
-    onClickCloseBtn: func.isRequired,
-    // 真正要關閉表單前觸發的函數（可用於將資料存到 local storage）
-    onClose: func,
-  };
+const FormBuilder = () => {};
 
-  static defaultProps = {
-    layout: 'typeform',
-  };
-}
+FormBuilder.propTypes = {
+  // 問卷標題
+  title: string.isRequired,
+  // 問卷描述
+  description: oneOfType([string, element]),
+  // 上傳按鈕的文字
+  submitButtonText: oneOfType([string, element]).isRequired,
+  // 上傳按鈕是否可按
+  submitButtonEnabled: bool.isRequired,
+  // 問題列表
+  questions: arrayOf(QuestionShape),
+  // 排版方式，目前只有一種，就是 typeform
+  layout: string.isRequired,
+  // 錯誤訊息的 Modal
+  errorModal: oneOfType([string, element]),
+  // 當使用者填寫內容，此函數會被觸發，且 emit 一個 object，包含被修改欄位的 key & value
+  onChange: func.isRequired,
+  // 當使用者按下送出鈕，且通過所有 question validator 驗證，此函數會被觸發
+  onSubmit: func.isRequired,
+  // 當使用者按下送出鈕，但有 question validator 驗證未過，此函數會被觸發
+  onValidateFail: func,
+  // 點擊下一步可額外觸發的函數 (可用於數據追蹤)
+  onNext: func,
+  // 點擊上一步考額外觸發的函數 (可用於數據追蹤)
+  onPrev: func,
+  // 點擊使用者條款 checkbox
+  onClickAgreement: func,
+  // 點擊關閉表單按鈕觸發的函數
+  onClickCloseBtn: func.isRequired,
+  // 真正要關閉表單前觸發的函數（可用於將資料存到 local storage）
+  onClose: func,
+};
+
+FormBuilder.defaultProps = {
+  layout: 'typeform',
+};
 
 export default FormBuilder;
