8cd5365fd8648ef2118ccf77b90e5583abb296ce
ann800213 <ann800213@gmail.com>
2020-03-14 12:47:02 +0800

    edit form-text-css

--- src/components/common/FormBuilder/QuestionBuilder/Text.js
+++ src/components/common/FormBuilder/QuestionBuilder/Text.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './Text.module.css';
 
 const Text = ({ title, description, dataKey, required, validator }) => (
   <div>
-    {title}
-    <input type="text" />
+    <div className={styles.title}>
+      1. {title}
+      <span className={styles.necessary}> * </span>
+    </div>
+    <input
+      className={styles.textinput}
+      type="text"
+      placeholder="請輸入職業名稱"
+      required
+    />
   </div>
 );
 
