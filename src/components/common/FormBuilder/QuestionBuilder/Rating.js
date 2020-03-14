687d9b58172c3c0c0b1c49a2fe84724423e513f4
ann800213 <ann800213@gmail.com>
2020-03-14 14:41:47 +0800

    edit form-rating-css ver.1

--- src/components/common/FormBuilder/QuestionBuilder/Rating.js
+++ src/components/common/FormBuilder/QuestionBuilder/Rating.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import cn from 'classnames';
+import { Glike } from 'common/icons';
+import styles from './Rating.module.css';
 
 const Rating = ({
   title,
@@ -8,12 +11,34 @@ const Rating = ({
   required,
   validator,
   maxRating,
+  hover,
+  active,
+  onClick,
 }) => (
-  <div>
-    {title}
+  <div className={styles.ratingContainer}>
+    <div className={styles.title}>
+      5. {title}
+      <span className={styles.necessary}> * </span>
+    </div>
     {[...Array(maxRating).keys()].map((_, i) => (
-      <input key={i} type="radio" name={dataKey} value={i} />
+      <label className={styles.ratingLabel}>
+        <input
+          className={styles.ratingInput}
+          key={i}
+          type="radio"
+          name={dataKey}
+          value={i}
+        />
+        <Glike
+          className={cn(styles.glickContainer, {
+            [styles.hover]: hover,
+            [styles.active]: active,
+          })}
+          onClick={onClick}
+        />
+      </label>
     ))}
+    <span className={styles.clickNote}>點擊做評分</span>
   </div>
 );
 
