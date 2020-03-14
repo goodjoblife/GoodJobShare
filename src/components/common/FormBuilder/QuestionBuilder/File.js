2cd73e6e28d0a2aaa590072550fe3ca4a0f80936
ann800213 <ann800213@gmail.com>
2020-03-14 14:24:51 +0800

    edit form-file-css ver.1

--- src/components/common/FormBuilder/QuestionBuilder/File.js
+++ src/components/common/FormBuilder/QuestionBuilder/File.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './File.module.css';
 
 const File = ({ title, description, dataKey, required, validator }) => (
   <div>
-    {title}
-    {description}
-    <input type="file" />
+    <div className={styles.title}>
+      4. {title}
+      <span className={styles.necessary}> * </span>
+    </div>
+    <div className={styles.description}>{description}</div>
+    <label className={styles.upload}>
+      <input id="upload_img" className={styles.image} type="file" />
+      <i className={styles.icon}></i>
+      <span className={styles.uploadText}>上傳檔案</span>
+    </label>
   </div>
 );
 
