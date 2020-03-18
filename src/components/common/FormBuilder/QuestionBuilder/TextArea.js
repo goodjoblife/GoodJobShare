7bec4106ab76cc6f32ef0bb5d45d981dfcf58b2d
ann800213 <ann800213@gmail.com>
2020-03-19 00:52:27 +0800

    form-textArea-css ver3.

--- src/components/common/FormBuilder/QuestionBuilder/TextArea.js
+++ src/components/common/FormBuilder/QuestionBuilder/TextArea.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import cn from 'classnames';
 import styles from './TextArea.module.css';
 
 const Textarea = ({ title, description, dataKey, required, validator }) => (
   <div className={styles.container}>
-    <div className={styles.title}>
+    <div className={cn(styles.title, { [styles.necessary]: required })}>
       2. {title}
-      <span className={styles.necessary}> * </span>
     </div>
     <textarea className={styles.textarea} />
     <p className={styles.warning}>最少30字，現在0字</p>
