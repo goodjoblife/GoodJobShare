2209b6c1452c130be3713741f027bf3ec7ff15b1
ann800213 <ann800213@gmail.com>
2020-03-20 10:32:01 +0800

    change file-css2

--- src/components/common/FormBuilder/QuestionBuilder/File.js
+++ src/components/common/FormBuilder/QuestionBuilder/File.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import cn from 'classnames';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import faFile from '@fortawesome/fontawesome-free-solid/faFile';
 import styles from './File.module.css';
 
 const File = ({ title, description, dataKey, required, validator }) => (
   <div>
-    <div className={styles.title}>
-      4. {title}
-      <span className={styles.necessary}> * </span>
+    <div className={cn(styles.title, { [styles.necessary]: required })}>
+      <span className={styles.necessary}>4. {title}</span>
     </div>
     <div className={styles.description}>{description}</div>
     <label className={styles.upload}>
       <input id="upload_img" className={styles.image} type="file" />
-      <i className={styles.icon}></i>
+      <FontAwesomeIcon icon={faFile} className={styles.icon} />
       <span className={styles.uploadText}>上傳檔案</span>
     </label>
   </div>
