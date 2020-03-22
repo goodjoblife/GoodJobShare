e7684c40898dd59dc77f75dc54dffc0520df7da7
peteranny <peteranny2@gmail.com>
2020-03-22 22:22:50 +0800

    user-terms

--- src/components/common/FormBuilder/SubmissionBlock.js
+++ src/components/common/FormBuilder/SubmissionBlock.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import styles from './SubmissionBlock.module.css';
@@ -14,7 +15,10 @@ const SubmissionBlock = ({ isSubmittable, onSubmit }) => {
         送出資料並解鎖
       </button>
       <div className={styles.privacyPolicy}>
-        我分享的是真實資訊，並遵守中華民國法令，以及本站使用者條款
+        我分享的是真實資訊，並遵守中華民國法令，以及本站
+        <Link to="/user-terms" target="_blank">
+          使用者條款
+        </Link>
       </div>
     </div>
   );
