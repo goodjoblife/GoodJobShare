5a49e28e7da4cbc2b291d300acbf79dd817cfa5f
peteranny <peteranny2@gmail.com>
2020-03-26 00:41:35 +0800

    offset

--- src/components/common/FormBuilder/Scrollable.js
+++ src/components/common/FormBuilder/Scrollable.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import cn from 'classnames';
 
 import styles from './Scrollable.module.css';
 
-const Scrollable = ({ children, className }) => (
-  <div className={cn(styles.frame, className)}>
-    <div className={styles.content}>{children}</div>
-  </div>
-);
+const Scrollable = ({ children, className }) => {
+  const [offset, setOffset] = useState(0);
+  const [maxOffset, setMaxOffset] = useState(0);
+  const handleRef = useCallback(el => {
+    if (el) {
+      setMaxOffset(el.scrollHeight - el.offsetHeight);
+    }
+  }, []);
+  const handleScroll = useCallback(e => {
+    setOffset(e.target.scrollTop);
+  }, []);
+  return (
+    <div
+      className={cn(
+        styles.frame,
+        { [styles.end]: offset === maxOffset },
+        className,
+      )}
+    >
+      <div ref={handleRef} onScroll={handleScroll} className={styles.content}>
+        {children}
+      </div>
+    </div>
+  );
+};
 
 export default Scrollable;
