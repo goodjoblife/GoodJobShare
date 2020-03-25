8c44190af05492f26b091f760264f20ce493e1d7
peteranny <peteranny2@gmail.com>
2020-03-26 02:46:39 +0800

    file

--- src/components/common/FormBuilder/QuestionBuilder/File.js
+++ src/components/common/FormBuilder/QuestionBuilder/File.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import cn from 'classnames';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 import faFile from '@fortawesome/fontawesome-free-solid/faFile';
 import styles from './File.module.css';
+
+const getClassName = (filename, error) => {
+  if (!filename && !error) {
+    return null;
+  } else if (filename && !error) {
+    return 'finished';
+  } else if (!filename && error) {
+    return 'error';
+  }
+};
+
+const getLabelText = (filename, error) => {
+  if (!filename && !error) {
+    return '上傳檔案';
+  } else if (filename && !error) {
+    return filename;
+  } else if (!filename && error) {
+    return `錯誤：${error.message}`;
+  }
+};
 
 const File = ({
   title,
@@ -13,24 +33,50 @@ const File = ({
   value,
   onChange,
   warning,
-}) => (
-  <div>
-    <div className={cn(styles.title, { [styles.necessary]: required })}>
-      4. {title}
+}) => {
+  const [filename, setFilename] = useState(null);
+  const [error, setError] = useState(null);
+
+  const handleClick = useCallback(
+    e => {
+      e.preventDefault();
+      if (!filename && !error) {
+        setFilename('薪資單.pdf');
+      } else if (filename && !error) {
+        setFilename(null);
+        setError(new Error('格式不符'));
+      } else if (!filename && error) {
+        setFilename(null);
+        setError(null);
+      }
+    },
+    [error, filename],
+  );
+
+  return (
+    <div>
+      <div className={cn(styles.title, { [styles.necessary]: required })}>
+        4. {title}
+      </div>
+      <div className={styles.description}>{description}</div>
+      <label
+        className={cn(styles.upload, styles[getClassName(filename, error)])}
+        onClick={handleClick}
+      >
+        <input
+          className={styles.input}
+          type="file"
+          value={value}
+          onChange={onChange}
+        />
+        <FontAwesomeIcon icon={faFile} className={styles.icon} />
+        <span className={styles.uploadText}>
+          {getLabelText(filename, error)}
+        </span>
+      </label>
     </div>
-    <div className={styles.description}>{description}</div>
-    <label className={styles.upload}>
-      <input
-        className={styles.input}
-        type="file"
-        value={value}
-        onChange={onChange}
-      />
-      <FontAwesomeIcon icon={faFile} className={styles.icon} />
-      <span className={styles.uploadText}>上傳檔案</span>
-    </label>
-  </div>
-);
+  );
+};
 
 File.propTypes = {
   title: PropTypes.string.isRequired,
