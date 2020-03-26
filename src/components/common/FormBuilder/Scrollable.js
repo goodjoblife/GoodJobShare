8d90ab0033e2057f1f546fe63440c59c695eadd4
peter.shih <peteranny2@gmail.com>
2020-03-26 08:40:35 +0800

    >=

--- src/components/common/FormBuilder/Scrollable.js
+++ src/components/common/FormBuilder/Scrollable.js
@@ -24,7 +24,7 @@ const Scrollable = ({ children, className }) => {
     <div
       className={cn(
         styles.frame,
-        { [styles.end]: offset === maxOffset },
+        { [styles.end]: offset >= maxOffset },
         className,
       )}
     >
