989b2e138e6508d0a24ce5a1a1bed9b7f8c4a4e8
peteranny <peteranny2@gmail.com>
2020-03-27 00:45:02 +0800

    rollback-scrollable

--- src/components/common/FormBuilder/Scrollable.js
+++ src/components/common/FormBuilder/Scrollable.js
@@ -1,35 +1,12 @@
-import React, { useState, useCallback } from 'react';
+import React from 'react';
 import cn from 'classnames';
-import { useMeasure } from 'react-use';
 
 import styles from './Scrollable.module.css';
 
 const Scrollable = ({ children, className }) => {
-  const [ref, { height: frameHeight }] = useMeasure();
-  const [remainOffset, setRemainOffset] = useState(0);
-
-  const handleRef = useCallback(
-    el => {
-      setRemainOffset(el.scrollHeight - el.scrollTop);
-      ref(el);
-    },
-    [ref],
-  );
-  const handleScroll = useCallback(e => {
-    setRemainOffset(e.target.scrollHeight - e.target.scrollTop);
-  }, []);
-
   return (
-    <div
-      className={cn(
-        styles.frame,
-        { [styles.end]: frameHeight >= remainOffset },
-        className,
-      )}
-    >
-      <div ref={handleRef} onScroll={handleScroll} className={styles.content}>
-        {children}
-      </div>
+    <div className={cn(styles.frame, className)}>
+      <div className={styles.content}>{children}</div>
     </div>
   );
 };
