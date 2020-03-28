e860a30d8229e460ac6fce6bddb4fb207fbd3000
Wei Ming Chen <barry800414@gmail.com>
2020-03-29 00:21:35 +0800

    feat: add different version for A/B testing

--- src/components/common/PermissionBlock/BasicPermissionBlock.js
+++ src/components/common/PermissionBlock/BasicPermissionBlock.js
@@ -8,6 +8,7 @@ import { compose, withState, withHandlers } from 'recompose';
 import P from 'common/base/P';
 import Modal from 'common/Modal';
 import { Heading } from 'common/base';
+import withExperimentParameters from 'common/withExperimentParameters';
 import CallToLoginShareButton from '../../../containers/PermissionBlock/CallToLoginShareButtonContainer';
 import getScale from '../../../utils/numberUtils';
 import styles from './PermissionBlock.module.css';
@@ -28,6 +29,9 @@ class BasicPermissionBlock extends React.Component {
     queryLaborRightsCount: PropTypes.func.isRequired,
     isModalOpen: PropTypes.bool.isRequired,
     toggleModal: PropTypes.func.isRequired,
+
+    // from withExperiemntParameters
+    experimentParameters: PropTypes.object,
   };
 
   static defaultProps = {
@@ -53,34 +57,56 @@ class BasicPermissionBlock extends React.Component {
       experienceCount,
       timeAndSalaryCount,
       laborRightsCount,
+      experimentParameters,
     } = this.props;
     const experienceScale = getScale(experienceCount);
     const timeAndSalaryScale = getScale(timeAndSalaryCount);
-    return (
-      <React.Fragment>
-        <div className={styles.headingContainer}>
-          <FontAwesomeIcon icon={faLock} className={styles.headingIcon} />
-          <Heading size="sl" Tag="h3">
-            你暫時沒有查看完整資訊的權限
-          </Heading>
-        </div>
-        <P size="l" className={styles.ctaText}>
-          只需幾分鐘，
-          <strong>分享你的職場資訊</strong>
-          ，不僅幫助其他人不再踩雷，更能
-          <strong>{`查看全站超過 ${timeAndSalaryScale} 筆`}</strong>
-          薪資工時資訊、
-          <strong>{`${experienceScale}+ 篇`}</strong>
-          面試及工作經驗分享，以及
-          <strong>{` ${laborRightsCount} 篇`}</strong>
-          勞動權益懶人包哦！
-        </P>
-        <br />
-        <P size="l">
-          <strong>若你已經分享過資訊，登入即可查看全文！</strong>
-        </P>
-      </React.Fragment>
-    );
+
+    if (experimentParameters['permissionBlockType'] === '20200329-B') {
+      return (
+        <React.Fragment>
+          <div className={styles.headingContainer}>
+            <FontAwesomeIcon icon={faLock} className={styles.headingIcon} />
+            <Heading size="sl" Tag="h3">
+              留下一筆資料，馬上解鎖全站資料
+            </Heading>
+          </div>
+          <P size="l" className={styles.ctaText}>
+            解鎖全站共{' '}
+            <strong>
+              {timeAndSalaryCount + experienceCount} 筆薪資、面試資料
+            </strong>
+            。若你已經留過資料，登入即可查看全文！
+          </P>
+        </React.Fragment>
+      );
+    } else {
+      return (
+        <React.Fragment>
+          <div className={styles.headingContainer}>
+            <FontAwesomeIcon icon={faLock} className={styles.headingIcon} />
+            <Heading size="sl" Tag="h3">
+              你暫時沒有查看完整資訊的權限
+            </Heading>
+          </div>
+          <P size="l" className={styles.ctaText}>
+            只需幾分鐘，
+            <strong>分享你的職場資訊</strong>
+            ，不僅幫助其他人不再踩雷，更能
+            <strong>{`查看全站超過 ${timeAndSalaryScale} 筆`}</strong>
+            薪資工時資訊、
+            <strong>{`${experienceScale}+ 篇`}</strong>
+            面試及工作經驗分享，以及
+            <strong>{` ${laborRightsCount} 篇`}</strong>
+            勞動權益懶人包哦！
+          </P>
+          <br />
+          <P size="l">
+            <strong>若你已經分享過資訊，登入即可查看全文！</strong>
+          </P>
+        </React.Fragment>
+      );
+    }
   };
 
   render() {
@@ -134,4 +160,6 @@ const enhance = compose(
   }),
 );
 
-export default enhance(BasicPermissionBlock);
+export default enhance(
+  withExperimentParameters(['permissionBlockType'])(BasicPermissionBlock),
+);
