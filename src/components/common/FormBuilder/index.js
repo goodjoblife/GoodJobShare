1c3175f108a52811681cb3b76e10c6d4046c7457
peteranny <peteranny2@gmail.com>
2020-04-11 00:13:36 +0800

    remove unused props

--- src/components/common/FormBuilder/index.js
+++ src/components/common/FormBuilder/index.js
@@ -60,25 +60,12 @@ const findIfQuestionsAcceptDraft = draft =>
 const FormBuilder = ({
   bodyClassName,
   open,
-  title,
-  description,
-  submitButtonText,
-  submitButtonEnabled,
   header: commonHeader,
   footer: commonFooter,
   questions,
-  layout,
   onChange,
   onSubmit,
-  onValidateFail,
-  onNext,
-  onPrev,
-  onClickAgreement,
   onClose,
-  msgModalContent,
-  openMsgModal,
-  onCloseMsgModal,
-  onConfirmMsgModal,
 }) => {
   const [draft, setDraftValue, resetDraft] = useDraft(questions);
   const handleDraftChange = dataKey => value => {
@@ -220,10 +207,6 @@ FormBuilder.propTypes = {
   // 問卷頁首 & 頁尾
   header: oneOfType([string, element]),
   footer: oneOfType([string, element]),
-  // 上傳按鈕的文字
-  submitButtonText: oneOfType([string, element]).isRequired,
-  // 上傳按鈕是否可按
-  submitButtonEnabled: bool.isRequired,
   // 問題列表
   questions: arrayOf(
     shape({
@@ -237,36 +220,12 @@ FormBuilder.propTypes = {
       warning: oneOfType([func, string]),
     }),
   ).isRequired,
-  // 排版方式，目前只有一種，就是 typeform
-  layout: string.isRequired,
   // 當使用者填寫內容，此函數會被觸發，且 emit 一個 object，包含被修改欄位的 key & value
   onChange: func.isRequired,
   // 當使用者按下送出鈕，且通過所有驗證，此函數會被觸發
   onSubmit: func.isRequired,
-  // 當使用者按下送出鈕，但有驗證未過，此函數會被觸發
-  onValidateFail: func,
-  // 點擊下一步可額外觸發的函數 (可用於數據追蹤)
-  onNext: func,
-  // 點擊上一步考額外觸發的函數 (可用於數據追蹤)
-  onPrev: func,
-  // 點擊使用者條款 checkbox
-  onClickAgreement: func,
   // 關閉表單前觸發的函數
   onClose: func.isRequired,
-
-  // 訊息 Modal 的內容
-  msgModalContent: oneOfType([string, element]),
-  // 訊息 Modal 是否開啟
-  openMsgModal: bool,
-  // 使用者點擊 Modal 的關閉按鈕觸發的函數
-  onCloseMsgModal: func,
-  // 使用者點擊 Modal 的確認按鈕觸發的函數
-  onConfirmMsgModal: func,
-};
-
-FormBuilder.defaultProps = {
-  layout: 'typeform',
-  openMsgModal: false,
 };
 
 const withBackgroundMask = Modal => props => (
