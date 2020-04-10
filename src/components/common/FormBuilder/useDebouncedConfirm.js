b440b85e8cf086124e4ec75ed343b262aca2853e
peteranny <peteranny2@gmail.com>
2020-04-11 01:51:25 +0800

    comment

--- src/components/common/FormBuilder/useDebouncedConfirm.js
+++ src/components/common/FormBuilder/useDebouncedConfirm.js
@@ -2,7 +2,10 @@ import { useState, useCallback, useEffect } from 'react';
 import { useDebounce } from 'react-use';
 
 const useDebouncedConfirm = (onConfirm, delay) => {
+  // 使用者確認作答完成
   const [confirmed, setConfirmed] = useState(false);
+
+  // 作答完成後延遲一段時間才觸發 onConfirm
   const [debouncedConfirmed, setDebouncedConfirmed] = useState(false);
   useDebounce(() => setDebouncedConfirmed(confirmed), delay, [confirmed]);
   useEffect(() => {
@@ -12,6 +15,8 @@ const useDebouncedConfirm = (onConfirm, delay) => {
       onConfirm();
     }
   }, [debouncedConfirmed]); // eslint-disable-line react-hooks/exhaustive-deps
+
+  // 僅暴露確認作答完成的函數給外界使用
   const confirm = useCallback(() => setConfirmed(true), []);
   return confirm;
 };
