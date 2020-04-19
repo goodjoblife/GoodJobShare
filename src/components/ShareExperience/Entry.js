9f50a97b1eee7a45bb429b0da571e195e21d08f3
peteranny <peteranny2@gmail.com>
2020-04-19 15:22:53 +0800

    interview-experience type-form

--- src/components/ShareExperience/Entry.js
+++ src/components/ShareExperience/Entry.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router';
 import { Wrapper } from 'common/base';
@@ -6,18 +6,30 @@ import ShareExpSection from 'common/ShareExpSection';
 import i from 'common/icons';
 import styles from './Entry.module.css';
 import StaticHelmet from 'common/StaticHelmet';
+import InterviewExperienceTypeForm from './InterviewForm/TypeForm';
 
-const Entry = ({ history }) => (
-  <div>
-    <StaticHelmet.Share />
-    <Wrapper size="l" className={styles.wrapper}>
-      <button onClick={() => history.goBack()} className={styles.closeBtn}>
-        <i.X />
-      </button>
-    </Wrapper>
-    <ShareExpSection />
-  </div>
-);
+const Entry = ({ history }) => {
+  const [openModalType, setOpenModalType] = useState(null);
+  return (
+    <div>
+      <StaticHelmet.Share />
+      <Wrapper size="l" className={styles.wrapper}>
+        <button onClick={() => history.goBack()} className={styles.closeBtn}>
+          <i.X />
+        </button>
+      </Wrapper>
+      <ShareExpSection
+        onSelectInterviewExperience={() =>
+          setOpenModalType('InterviewExperience')
+        }
+      />
+      <InterviewExperienceTypeForm
+        open={openModalType === 'InterviewExperience'}
+        onClose={() => setOpenModalType(null)}
+      />
+    </div>
+  );
+};
 
 Entry.propTypes = {
   history: PropTypes.object.isRequired,
---
+++ src/components/ShareExperience/InterviewForm/TypeForm.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import R from 'ramda';
+import FormBuilder from 'common/FormBuilder';
+
+const experienceInYearOptions = R.range(0, 51).map(String);
+const questions = [
+  { title: '應徵職稱', type: 'text', dataKey: 'jobTitle' },
+  { title: '公司名稱', type: 'text', dataKey: 'companyQuery' },
+  {
+    title: '面試地區',
+    type: 'radio',
+    options: [
+      '基隆市',
+      '臺北市',
+      '新北市',
+      '桃園市',
+      '新竹市',
+      '新竹縣',
+      '苗栗縣',
+      '臺中市',
+      '彰化縣',
+      '南投縣',
+      '雲林縣',
+      '嘉義市',
+      '嘉義縣',
+      '臺南市',
+      '高雄市',
+      '屏東縣',
+      '臺東縣',
+      '花蓮縣',
+      '宜蘭縣',
+      '澎湖縣',
+      '金門縣',
+      '連江縣',
+    ],
+    dataKey: 'region',
+  },
+  {
+    title: '什麼時候去面試的呢？',
+    type: 'customized',
+    dataKey: 'interviewTime',
+    renderCustomizedQuestion: () => null,
+  },
+  {
+    title: '自身相關職務工作經驗',
+    type: 'radio',
+    dataKey: 'experienceInYear',
+    options: experienceInYearOptions,
+  },
+  {
+    title: '面試結果',
+    type: 'radio',
+    dataKey: 'interviewResult',
+    options: ['錄取', '未錄取', '沒通知', '其他'],
+  },
+  {
+    title: '待遇',
+    type: 'customized',
+    dataKey: 'salary',
+    renderCustomizedQuestion: () => null,
+  },
+  {
+    title: '為這次的面試評個分',
+    type: 'rating',
+    dataKey: 'overallRating',
+    maxRating: 5,
+  },
+  { title: '面試過程', type: 'textarea', dataKey: 'sections', minLength: 30 },
+  {
+    title: '面試中問了什麼問題呢？',
+    type: 'textarea',
+    dataKey: 'interviewQas',
+    minLength: 30,
+  },
+  {
+    title: '是否有以下特殊問題？',
+    type: 'checkbox',
+    dataKey: 'interviewSensitiveQuestions',
+    options: [
+      '詢問家庭狀況',
+      '曾詢問婚姻狀況、生育計畫',
+      '曾要求繳交身分證、保證金',
+      '其他',
+    ],
+  },
+  {
+    title: '給其他面試者的中肯建議',
+    type: 'textarea',
+    dataKey: 'suggestionSection',
+    minLength: 30,
+  },
+];
+
+const TypeForm = ({ open, onClose }) => {
+  return <FormBuilder open={open} onClose={onClose} questions={questions} />;
+};
+
+TypeForm.propTypes = {
+  open: PropTypes.bool.isRequired,
+
+  onClose: PropTypes.func.isRequired,
+};
+
+export default TypeForm;
