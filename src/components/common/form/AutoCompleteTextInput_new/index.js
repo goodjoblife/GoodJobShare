172b19ddb74ce6bc84aadf052bc71142f9a130b6
peteranny <peteranny2@gmail.com>
2020-04-24 00:37:57 +0800

    fix-autocomplete-menu

--- src/components/common/form/AutoCompleteTextInput_new/index.js
+++ src/components/common/form/AutoCompleteTextInput_new/index.js
@@ -1,9 +1,90 @@
-import React, { forwardRef } from 'react';
+import React, {
+  forwardRef,
+  useState,
+  useEffect,
+  useCallback,
+  useRef,
+} from 'react';
 import PropTypes from 'prop-types';
-import Autocomplete from 'react-autocomplete';
+import { useKey } from 'react-use';
 import cn from 'classnames';
 
 import styles from './styles.module.css';
+
+const useBoundedIndex = (bound, initialIndex) => {
+  const [index, setIndex] = useState(initialIndex);
+  const setBoundedIndex = useCallback(
+    index => setIndex((index + bound) % bound),
+    [bound],
+  );
+  const resetIndex = useCallback(() => setIndex(initialIndex), [initialIndex]);
+  return [index, setBoundedIndex, resetIndex];
+};
+
+const useKeyNavigation = (index, setIndex, isEnabled, target) => {
+  useKey(
+    'ArrowUp',
+    e => {
+      e.preventDefault();
+      if (isEnabled) setIndex(index - 1);
+    },
+    { target },
+    [isEnabled, index, setIndex],
+  );
+  useKey(
+    'ArrowDown',
+    e => {
+      e.preventDefault();
+      if (isEnabled) setIndex(index + 1);
+    },
+    { target },
+    [isEnabled, index, setIndex],
+  );
+};
+
+const useEnterConfirm = (confirm, isEnabled) => {
+  useKey(
+    'Enter',
+    () => {
+      if (isEnabled) confirm();
+    },
+    {},
+    [confirm, isEnabled],
+  );
+};
+
+const useScrollToItem = itemRef => {
+  useEffect(() => {
+    if (itemRef) {
+      itemRef.scrollIntoView({ block: 'nearest' });
+    }
+  }, [itemRef]);
+};
+
+const AutoCompleteMenu = ({ className, open, ...props }) => (
+  <div
+    className={cn(
+      styles.menu,
+      {
+        [styles.visible]: open,
+      },
+      className,
+    )}
+    {...props}
+  />
+);
+
+const AutoCompleteOption = forwardRef(({ active, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn(
+      styles.item,
+      { [styles.active]: active },
+      active ? 'pSBold' : 'pS',
+    )}
+    {...props}
+  />
+));
 
 const AutoCompleteTextInput = forwardRef(
   (
@@ -12,60 +93,104 @@ const AutoCompleteTextInput = forwardRef(
       className,
       value,
       onChange,
+      onFocus,
+      onBlur,
       autocompleteItems,
       autocompleteItemKeySelector,
       autocompleteItemLabelSelector,
       onAutocompleteItemSelected,
       ...inputProps
     },
-    ref,
-  ) => (
-    <Autocomplete
-      ref={ref}
-      value={value}
-      onChange={onChange}
-      items={autocompleteItems}
-      wrapperProps={{ className: cn(styles.wrapper, wrapperClassName) }}
-      wrapperStyle={{ display: 'flex' }}
-      inputProps={{
-        className: cn(styles.textInput, className),
-        ...inputProps,
-      }}
-      renderMenu={(items, value, style) => (
-        <div
-          className={cn(styles.menu, {
-            [styles.active]: autocompleteItems.length > 0,
-          })}
-          style={style}
-        >
-          {items}
-        </div>
-      )}
-      renderItem={(item, isHighlighted, style) => (
-        <div
-          key={autocompleteItemKeySelector(item)}
-          className={cn(
-            styles.item,
-            { [styles.active]: isHighlighted },
-            isHighlighted ? 'pSBold' : 'pS',
-          )}
-          style={style}
-        >
-          {autocompleteItemLabelSelector(item)}
-        </div>
-      )}
-      getItemValue={autocompleteItemLabelSelector}
-      onSelect={onAutocompleteItemSelected}
-      autoHighlight={false}
-    />
-  ),
-);
+    inputRef,
+  ) => {
+    const itemRefs = useRef([]);
+    const [isMenuOpen, setMenuOpen] = useState(false);
+    const [shouldIgnoreBlur, setIgnoreBlur] = useState(false);
+    const [
+      highlightedIndex,
+      setHighlightedIndex,
+      resetHighlightedIndex,
+    ] = useBoundedIndex(autocompleteItems.length + 1, autocompleteItems.length);
 
+    const selectItemAt = useCallback(
+      index => {
+        if (index < autocompleteItems.length) {
+          const selectedItem = autocompleteItems[index];
+          onAutocompleteItemSelected(selectedItem);
+          setMenuOpen(false);
+        }
+      },
+      [autocompleteItems, onAutocompleteItemSelected],
+    );
+    const selectHighlightedItem = useCallback(
+      () => selectItemAt(highlightedIndex),
+      [highlightedIndex, selectItemAt],
+    );
+
+    useKeyNavigation(
+      highlightedIndex,
+      setHighlightedIndex,
+      isMenuOpen,
+      inputRef.current,
+    );
+
+    useEnterConfirm(selectHighlightedItem, isMenuOpen);
+
+    useEffect(resetHighlightedIndex, [resetHighlightedIndex, value]);
+
+    useScrollToItem(itemRefs.current[highlightedIndex]);
+
+    return (
+      <div className={cn(styles.wrapper, wrapperClassName)}>
+        <input
+          ref={inputRef}
+          className={cn(styles.textInput, className)}
+          value={value}
+          onChange={onChange}
+          onFocus={e => {
+            setMenuOpen(true);
+            if (onFocus) onFocus(e);
+          }}
+          onBlur={e => {
+            if (shouldIgnoreBlur) return;
+            setMenuOpen(false);
+            if (onBlur) onBlur(e);
+          }}
+          {...inputProps}
+        />
+        <AutoCompleteMenu open={isMenuOpen && autocompleteItems.length > 0}>
+          {autocompleteItems.map((item, i) => (
+            <AutoCompleteOption
+              key={autocompleteItemKeySelector(item)}
+              ref={el => (itemRefs.current[i] = el)}
+              active={highlightedIndex === i}
+              onClick={e => {
+                selectItemAt(i);
+              }}
+              onMouseEnter={e => {
+                setIgnoreBlur(true);
+                setHighlightedIndex(i);
+              }}
+              onMouseLeave={e => {
+                setIgnoreBlur(false);
+                resetHighlightedIndex();
+              }}
+            >
+              {autocompleteItemLabelSelector(item)}
+            </AutoCompleteOption>
+          ))}
+        </AutoCompleteMenu>
+      </div>
+    );
+  },
+);
 AutoCompleteTextInput.propTypes = {
   wrapperClassName: PropTypes.string,
   className: PropTypes.string,
   value: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
+  onFocus: PropTypes.func,
+  onBlur: PropTypes.func,
   autocompleteItems: PropTypes.array.isRequired,
   autocompleteItemKeySelector: PropTypes.func.isRequired,
   autocompleteItemLabelSelector: PropTypes.func.isRequired,
