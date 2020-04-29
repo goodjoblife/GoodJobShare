8ecd0c1f89f4d99323f29ffbe673f69aefa9d4fb
Wei Ming Chen <barry800414@gmail.com>
2020-04-29 22:24:58 +0800

    feat: add SubmitForm event

--- src/utils/eventBasedTracking/index.js
+++ src/utils/eventBasedTracking/index.js
@@ -2,10 +2,12 @@ import * as UserModule from './user';
 import * as ViewArticleDetailModule from './viewArticleDetail';
 import * as ViewSalaryWorkTimeModule from './viewSalaryWorkTime';
 import * as EnterFormModule from './enterForm';
+import * as SubmitFormModule from './submitForm';
 
 export {
   UserModule,
   ViewArticleDetailModule,
   ViewSalaryWorkTimeModule,
   EnterFormModule,
+  SubmitFormModule,
 };
