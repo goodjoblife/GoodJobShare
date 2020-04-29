736b476b54abf5a32ef820d7a367e5bd7d07dfe5
peteranny <peteranny2@gmail.com>
2020-04-30 00:34:18 +0800

    no unlock duration

--- src/components/ShareExperience/common/TypeFormFooter.js
+++ src/components/ShareExperience/common/TypeFormFooter.js
@@ -3,8 +3,7 @@ import styles from './TypeFormFooter.module.css';
 
 const TypeFormFooter = () => (
   <div className={styles.footer}>
-    完成可解鎖全站 2 萬筆資訊{' '}
-    <span className={styles.unlockDuration}>48 小時</span>
+    完成可解鎖全站 2 萬筆資訊 <span className={styles.unlockDuration}></span>
   </div>
 );
 
