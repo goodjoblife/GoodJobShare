1b551c194c446fca2e0789e7ac15c9d1267ec038
peteranny <peteranny2@gmail.com>
2020-05-09 00:31:28 +0800

    global modal

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,29 +1,52 @@
-import React from 'react';
-import { Switch } from 'react-router-dom';
+import React, { Fragment, useMemo, useCallback } from 'react';
+import { Switch, useLocation, useHistory } from 'react-router-dom';
+import { parse, stringify } from 'qs';
+import { omit } from 'ramda';
 
 import { AppRouteWithSubRoutes } from '../route';
 import styles from './App.module.css';
 import Header from '../../containers/App/Header';
 import Footer from './Footer';
 import StaticHelmet from 'common/StaticHelmet';
-
+import ShareInterviewModal from '../ShareExperience/InterviewForm/TypeForm';
 import routes from '../../routes';
 
-const App = () => (
-  <Switch>
-    {routes.map((route, i) => (
-      <AppRouteWithSubRoutes key={i} {...route}>
-        {({ hasHeader, hasFooter, children }) => (
-          <div className={styles.App}>
-            {hasHeader ? <Header /> : null}
-            <StaticHelmet.Default />
-            <div className={styles.content}>{children}</div>
-            {hasFooter ? <Footer /> : null}
-          </div>
-        )}
-      </AppRouteWithSubRoutes>
-    ))}
-  </Switch>
-);
+const useShare = () => {
+  const location = useLocation();
+  const query = useMemo(
+    () => parse(location.search, { ignoreQueryPrefix: true }),
+    [location.search],
+  );
+  const share = query.share;
+  const history = useHistory();
+  const exitShare = useCallback(
+    () => history.push(`?${stringify(omit(['share'], query))}`),
+    [history, query],
+  );
+  return [share, exitShare];
+};
+
+const App = () => {
+  const [share, exitShare] = useShare();
+  return (
+    <Fragment>
+      <Switch>
+        {routes.map((route, i) => (
+          <AppRouteWithSubRoutes key={i} {...route}>
+            {({ hasHeader, hasFooter, children }) => (
+              <div className={styles.App}>
+                {hasHeader ? <Header /> : null}
+                <StaticHelmet.Default />
+                <div className={styles.content}>{children}</div>
+                {hasFooter ? <Footer /> : null}
+              </div>
+            )}
+          </AppRouteWithSubRoutes>
+        ))}
+      </Switch>
+      <ShareInterviewModal open={share === 'interview'} onClose={exitShare} />
+    </Fragment>
+  );
+};
 
 export default App;
--- src/components/ShareExperience/Entry.js
+++ src/components/ShareExperience/Entry.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router';
 import { Wrapper } from 'common/base';
@@ -6,10 +6,8 @@ import ShareExpSection from 'common/ShareExpSection';
 import i from 'common/icons';
 import styles from './Entry.module.css';
 import StaticHelmet from 'common/StaticHelmet';
-import InterviewExperienceTypeForm from './InterviewForm/TypeForm';
 
 const Entry = ({ history }) => {
-  const [openModalType, setOpenModalType] = useState(null);
   return (
     <div>
       <StaticHelmet.Share />
@@ -18,15 +16,7 @@ const Entry = ({ history }) => {
           <i.X />
         </button>
       </Wrapper>
-      <ShareExpSection
-        onSelectInterviewExperience={() =>
-          setOpenModalType('InterviewExperience')
-        }
-      />
-      <InterviewExperienceTypeForm
-        open={openModalType === 'InterviewExperience'}
-        onClose={() => setOpenModalType(null)}
-      />
+      <ShareExpSection />
     </div>
   );
 };
