76c7ae491b0deddfb770c662a742b8a77bf041fd
yongchensu <yongchensu0@gmail.com>
2020-05-24 16:08:31 +0800

    add button

--- src/components/LaborUnionDetail/index.js
+++ src/components/LaborUnionDetail/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Section, Wrapper, Heading, P } from 'common/base';
+import Button from 'common/button/Button';
+//import ButtonRect from 'common/button/Button';
+//import i from 'common/icons';
 
 class LaborUnionDetail extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Hello world</h1>
-      </div>
+      <Section padding>
+        <Wrapper size="m">
+          <Heading size="l">台灣電子電機資訊產業公會</Heading>
+          <P>test</P>
+          <Button btnStyle="buttonHoverYellow" circleSize="md">
+            馬上加入
+          </Button>
+        </Wrapper>
+      </Section>
     );
   }
 }
