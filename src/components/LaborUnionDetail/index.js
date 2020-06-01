ca01be79f294842fb92dc982f92f57b3d3ee9c92
YongChenSu <yongchensu0@gmail.com>
2020-06-01 21:09:26 +0800

    改為 functional component

--- src/components/LaborUnionDetail/index.js
+++ src/components/LaborUnionDetail/index.js
@@ -3,36 +3,35 @@ import { Section, Wrapper, Heading } from 'common/base';
 import ButtonRect from 'common/button/ButtonRect';
 import styles from './LaborUnionDetail.module.css';
 
-class LaborUnionDetail extends React.Component {
-  render() {
-    return (
+const LaborUnionDetail = () => {
+  return (
+    <Section>
       <Section>
-        <Section center>
-          <Wrapper className={styles.intro}>
-            <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSgEO4b5dyj36gl9Rq1IHDHFfwu2GHMiF6aB6b_U9gQ_zQLujqE&usqp=CAU"
-              className={styles.headingIcon}
-              alt="勞權圖示"
-            />
-            <Heading>
-              <div className={styles.heading}>台灣電子電機資訊產業公會</div>
-              <div className={styles.heading}>守護科技業勞工權益的第一線</div>
-            </Heading>
-          </Wrapper>
-        </Section>
-        <Section>
-          <ButtonRect btnStyle="hoverYellow" className={styles.joinBtn}>
-            馬上加入
-          </ButtonRect>
+        <Wrapper className={styles.intro}>
           <img
-            src="https://s.newtalk.tw/album/news/123/5ae8067d8a13d.JPG"
-            alt="勞權公投"
-            className={styles.banner}
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSgEO4b5dyj36gl9Rq1IHDHFfwu2GHMiF6aB6b_U9gQ_zQLujqE&usqp=CAU"
+            className={styles.headingIcon}
+            alt="勞權圖示"
           />
-        </Section>
+          <Heading>
+            <div className={styles.heading}>台灣電子電機資訊產業公會</div>
+            <div className={styles.heading}>守護科技業勞工權益的第一線</div>
+          </Heading>
+        </Wrapper>
       </Section>
-    );
-  }
-}
+
+      <Section>
+        <ButtonRect btnStyle="hoverYellow" className={styles.joinBtn}>
+          馬上加入
+        </ButtonRect>
+        <img
+          src="https://s.newtalk.tw/album/news/123/5ae8067d8a13d.JPG"
+          className={styles.banner}
+          alt="勞權公投"
+        />
+      </Section>
+    </Section>
+  );
+};
 
 export default LaborUnionDetail;
