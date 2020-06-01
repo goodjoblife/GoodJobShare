a18b48a9ad06a54bcc641f387d58330d782e3b67
YongChenSu <30387546+YongChenSu@users.noreply.github.com>
2020-06-01 10:48:00 +0630

    Feat/labor union layout (#909)
    * add LaborUnionDetail/index.js
    
    * add button
    
    * add  the css of icon
    
    * add an icon
    
    * add a heading
    
    * add a joinButton
    
    * add a banner
    
    * change the route of LaborUnionDetail th the end.

--- src/components/LaborUnionDetail/index.js
+++ src/components/LaborUnionDetail/index.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import { Section, Wrapper, Heading } from 'common/base';
+import ButtonRect from 'common/button/ButtonRect';
+import styles from './LaborUnionDetail.module.css';
 
 class LaborUnionDetail extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Hello world</h1>
-      </div>
+      <Section>
+        <Section center>
+          <Wrapper className={styles.intro}>
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSgEO4b5dyj36gl9Rq1IHDHFfwu2GHMiF6aB6b_U9gQ_zQLujqE&usqp=CAU"
+              className={styles.headingIcon}
+              alt="勞權圖示"
+            />
+            <Heading>
+              <div className={styles.heading}>台灣電子電機資訊產業公會</div>
+              <div className={styles.heading}>守護科技業勞工權益的第一線</div>
+            </Heading>
+          </Wrapper>
+        </Section>
+
+        <Section>
+          <ButtonRect btnStyle="hoverYellow" className={styles.joinBtn}>
+            馬上加入
+          </ButtonRect>
+          <img
+            src="https://s.newtalk.tw/album/news/123/5ae8067d8a13d.JPG"
+            alt="勞權公投"
+            className={styles.banner}
+          />
+        </Section>
+      </Section>
     );
   }
 }
