24997dac010ca80e05e6d13c745d697fffe0ba94
peteranny <peteranny2@gmail.com>
2020-07-06 00:21:41 +0800

    func-comp

--- src/components/App/Header/index.js
+++ src/components/App/Header/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { Link, useHistory, useLocation } from 'react-router-dom';
 import cn from 'classnames';
 import ReactGA from 'react-ga';
 import { compose } from 'recompose';
@@ -22,81 +22,44 @@ import emailStatusMap from '../../../constants/emailStatus';
 import withModal from '../../TimeAndSalary/common/withModal';
 import LoginModal from '../../common/LoginModal';
 
-class Header extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isNavOpen: false,
-    };
-    this.toggleNav = this.toggleNav.bind(this);
-    this.closeNav = this.closeNav.bind(this);
-    this.logout = this.logout.bind(this);
-    this.openLoginModal = this.openLoginModal.bind(this);
-    this.closeLoginModal = this.closeLoginModal.bind(this);
-    this.unlisten = () => {};
-  }
+const Header = ({ auth, loginModal, fetchPermission, logout }) => {
+  const history = useHistory();
+  const location = useLocation();
+  const [isNavOpen, setNavOpen] = useState(false);
 
-  componentDidMount() {
-    const { history } = this.props;
-    this.unlisten = history.listen(this.closeNav);
-  }
-
-  componentDidUpdate(prevProps) {
-    if (
-      prevProps.auth.get('status') !== this.props.auth.get('status') &&
-      this.props.auth.get('status') === authStatus.CONNECTED
-    ) {
-      this.props.fetchPermission();
+  useEffect(() => {
+    if (auth.get('status') === authStatus.CONNECTED) {
+      fetchPermission();
     }
-  }
+  }, [auth.get('status'), fetchPermission]); // eslint-disable-line react-hooks/exhaustive-deps
 
-  componentWillUnmount() {
-    this.unlisten();
-  }
-
-  onClickShareData = () => {
+  const onClickShareData = useCallback(() => {
     ReactGA.event({
       category: GA_CATEGORY.HEADER,
       action: GA_ACTION.CLICK_SHARE_DATA,
     });
-  };
+  }, []);
 
-  toggleNav() {
-    this.setState({
-      isNavOpen: !this.state.isNavOpen,
-    });
-  }
+  const toggleNav = useCallback(() => setNavOpen(!isNavOpen), [isNavOpen]);
 
-  closeNav() {
-    this.setState({
-      isNavOpen: false,
-    });
-  }
+  const closeNav = useCallback(() => setNavOpen(false), []);
 
-  openLoginModal() {
-    this.props.loginModal.setIsOpen(true);
-  }
+  const openLoginModal = useCallback(() => loginModal.setIsOpen(true), [
+    loginModal,
+  ]);
 
-  closeLoginModal() {
-    this.props.loginModal.setIsOpen(false);
-  }
+  const closeLoginModal = useCallback(() => loginModal.setIsOpen(false), [
+    loginModal,
+  ]);
 
-  logout() {
-    const { logout } = this.props;
-    logout();
-  }
+  useEffect(() => history.listen(closeNav), [closeNav, history]);
 
-  renderTop = () => {
-    const {
-      auth,
-      location: { pathname },
-    } = this.props;
-
+  const topNode = useMemo(() => {
     const isLogin = auth.get('status') === authStatus.CONNECTED;
     const emailStatus = auth.getIn(['user', 'email_status']);
     const isEmailVerified = emailStatus === emailStatusMap.VERIFIED;
 
-    if (!isLogin && pathname === '/') {
+    if (!isLogin && location.pathname === '/') {
       return null;
     }
 
@@ -117,111 +80,89 @@ class Header extends React.Component {
         <ProgressTop />
       </Top>
     );
-  };
+  }, [auth, location.pathname]);
 
-  render() {
-    return (
-      <div className={styles.root}>
-        {this.renderTop()}
-        <header className={styles.header}>
-          <Wrapper size="l" className={styles.inner}>
-            <HeaderButton
-              isNavOpen={this.state.isNavOpen}
-              toggle={this.toggleNav}
+  return (
+    <div className={styles.root}>
+      {topNode}
+      <header className={styles.header}>
+        <Wrapper size="l" className={styles.inner}>
+          <HeaderButton isNavOpen={isNavOpen} toggle={toggleNav} />
+          <div className={styles.logo}>
+            <Link to="/" title="GoodJob 職場透明化運動">
+              <GjLogo />
+            </Link>
+          </div>
+          <div className={styles.logoSm}>
+            <Link to="/" title="GoodJob 職場透明化運動">
+              <Glike />
+            </Link>
+          </div>
+          <div className={styles.searchbarWrapper}>
+            <Searchbar
+              className={styles.searchbar}
+              placeholder="輸入公司 or 職稱，查詢面試、薪水"
             />
-            <div className={styles.logo}>
-              <Link to="/" title="GoodJob 職場透明化運動">
-                <GjLogo />
-              </Link>
-            </div>
-            <div className={styles.logoSm}>
-              <Link to="/" title="GoodJob 職場透明化運動">
-                <Glike />
-              </Link>
-            </div>
-            <div className={styles.searchbarWrapper}>
-              <Searchbar
-                className={styles.searchbar}
-                placeholder="輸入公司 or 職稱，查詢面試、薪水"
-              />
-            </div>
-            <div className={cn(styles.searchbarWrapper, styles.mobile)}>
-              <Searchbar
-                className={styles.searchbar}
-                placeholder="輸入公司 or 職稱查詢"
-              />
-            </div>
-            <nav
-              className={cn(styles.nav, {
-                [styles.isNavOpen]: this.state.isNavOpen,
-              })}
-            >
-              <Link
-                to="/"
-                className={styles.logo}
-                title="GoodJob 職場透明化運動"
-              >
-                <GjLogo />
-              </Link>
-              <SiteMenu
-                isLogin={this.props.auth.get('status') === authStatus.CONNECTED}
-              />
-              <div className={styles.buttonsArea}>
-                <ShareButton onClick={this.onClickShareData} />
-                <div style={{ position: 'relative' }}>
-                  {this.props.auth.getIn(['user', 'name']) === null && (
-                    <button
-                      className={styles.loginBtn}
-                      onClick={this.openLoginModal}
-                    >
-                      登入
-                    </button>
-                  )}
-                  {this.props.auth.getIn(['user', 'name']) !== null && (
-                    <PopoverToggle
-                      popoverClassName={styles.popover}
-                      popoverContent={
-                        <ul className={styles.popoverItem}>
-                          <li>
-                            <Link to="/me">個人頁面</Link>
-                          </li>
-                          <li>
-                            <button
-                              onClick={() => {
-                                this.logout();
-                              }}
-                            >
-                              登出
-                            </button>
-                          </li>
-                        </ul>
-                      }
-                    >
-                      <div className={styles.userNameBtn}>
-                        {this.props.auth.getIn(['user', 'name'])}
-                      </div>
-                    </PopoverToggle>
-                  )}
-                </div>
+          </div>
+          <div className={cn(styles.searchbarWrapper, styles.mobile)}>
+            <Searchbar
+              className={styles.searchbar}
+              placeholder="輸入公司 or 職稱查詢"
+            />
+          </div>
+          <nav
+            className={cn(styles.nav, {
+              [styles.isNavOpen]: isNavOpen,
+            })}
+          >
+            <Link to="/" className={styles.logo} title="GoodJob 職場透明化運動">
+              <GjLogo />
+            </Link>
+            <SiteMenu isLogin={auth.get('status') === authStatus.CONNECTED} />
+            <div className={styles.buttonsArea}>
+              <ShareButton onClick={onClickShareData} />
+              <div style={{ position: 'relative' }}>
+                {auth.getIn(['user', 'name']) === null && (
+                  <button className={styles.loginBtn} onClick={openLoginModal}>
+                    登入
+                  </button>
+                )}
+                {auth.getIn(['user', 'name']) !== null && (
+                  <PopoverToggle
+                    popoverClassName={styles.popover}
+                    popoverContent={
+                      <ul className={styles.popoverItem}>
+                        <li>
+                          <Link to="/me">個人頁面</Link>
+                        </li>
+                        <li>
+                          <button onClick={logout}>登出</button>
+                        </li>
+                      </ul>
+                    }
+                  >
+                    <div className={styles.userNameBtn}>
+                      {auth.getIn(['user', 'name'])}
+                    </div>
+                  </PopoverToggle>
+                )}
               </div>
-            </nav>
-          </Wrapper>
-        </header>
-        <LoginModal
-          isOpen={this.props.loginModal.isOpen}
-          close={this.closeLoginModal}
-          loginModal={this.props.loginModal}
-        />
-      </div>
-    );
-  }
-}
+            </div>
+          </nav>
+        </Wrapper>
+      </header>
+      <LoginModal
+        isOpen={loginModal.isOpen}
+        close={closeLoginModal}
+        loginModal={loginModal}
+      />
+    </div>
+  );
+};
 
 Header.propTypes = {
   logout: PropTypes.func.isRequired,
   auth: PropTypes.object,
-  location: PropTypes.object,
-  history: PropTypes.object.isRequired,
   fetchPermission: PropTypes.func.isRequired,
 };
 
