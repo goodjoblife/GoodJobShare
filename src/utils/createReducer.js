d90cbe267520d6b03a8c01251a4c9b500d1cec39
peter.shih <peteranny2@gmail.com>
2020-09-01 19:30:40 +0800

    prevent reset on logout

--- src/utils/createReducer.js
+++ src/utils/createReducer.js
@@ -1,11 +1,12 @@
 import { LOG_OUT } from '../actions/auth';
 
-const createReducer = (initialState, handlers) => (
-  state = initialState,
-  action,
-) => {
+const createReducer = (
+  initialState,
+  handlers,
+  { resetOnLogOut = true } = {},
+) => (state = initialState, action) => {
   const reduceFn = handlers[action.type];
-  if (action.type === LOG_OUT) return initialState;
+  if (resetOnLogOut && action.type === LOG_OUT) return initialState;
   return reduceFn ? reduceFn(state, action) : state;
 };
 
