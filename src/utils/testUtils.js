7d550b17b5bc5ac61bd517bbe4f6548c01594318
Wei Ming Chen <barry800414@gmail.com>
2020-10-20 14:05:55 +0800

    chore: update comments

--- src/utils/testUtils.js
+++ src/utils/testUtils.js
@@ -1,10 +1,11 @@
 /**
- * 提供 React-Testing-Library 一個盡可能與實際環境相同的環境
- * 這些 Wrapper 由 src/client.js 取得
+ * 把 global 的 providers 引進來，讓 React-Testing-Library 在 render
+ * component 時，可以正常運作
+ * 這些 providers 由 src/client.js 取得
  *
  * Note:
  *  1. 之所以不直接把 client.js 裡面的 providers 都取出來，是因為目前測試套件沒
- *     有實作 window 的 scroll 功能，會導致測試跳大量警告訊息
+ *     有實作 window 的 scroll 功能，引入會導致測試跳大量警告訊息
  */
 
 import React from 'react';
