bca625bcf425aaca31996fa001afba1a0cbca18a
peteranny <peteranny2@gmail.com>
2022-11-17 22:51:52 +0800

    target=_blank

--- src/components/Buy/SubscriptionsSection/Captain.js
+++ src/components/Buy/SubscriptionsSection/Captain.js
@@ -7,7 +7,10 @@ const Captain = () => (
     <P>解鎖範圍：全站 8,393 筆面試、薪資資料，及期間內新增的資料</P>
     <P>使用期間：現在 ~ 2022.01.30 23:59</P>
     <P>
-      付款及同意<Link to="/user-terms">使用條款</Link>
+      付款及同意
+      <Link to="/user-terms" target="_blank">
+        使用條款
+      </Link>
     </P>
   </Section>
 );
