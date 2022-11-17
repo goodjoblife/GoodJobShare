fbfdfa8a7802286d6b011ec930395adcd0e3eae6
Yen-chi Chen <mark86092@gmail.com>
2022-11-17 13:52:10 +0000

    Rewrite LaborRights to use hooks

--- src/components/LaborRightsSingle/index.js
+++ src/components/LaborRightsSingle/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
 import Helmet from 'react-helmet';
 import ReactPixel from 'react-facebook-pixel';
 import R from 'ramda';
 import { compose, setStatic } from 'recompose';
-
+import { useParams } from 'react-router-dom';
 import Loader from 'common/Loader';
 import { Section } from 'common/base';
 import {
@@ -17,20 +18,26 @@ import NotFound from 'common/NotFound';
 import CallToActionFolder from 'common/CallToAction/CallToActionFolder';
 import FanPageBlock from 'common/FanPageBlock';
 import { withPermission } from 'common/permission-context';
+import { paramsSelector } from 'common/routing/selectors';
+import { isFetching, isUnfetched, isError, isFetched } from 'constants/status';
+import { SITE_NAME } from 'constants/helmetData';
+import PIXEL_CONTENT_CATEGORY from 'constants/pixelConstants';
 import Body from './Body';
 import Footer from './Footer';
 
-import { queryMenu, queryEntry } from '../../actions/laborRights';
 import {
-  isFetching,
-  isUnfetched,
-  isError,
-  isFetched,
-} from '../../constants/status';
-import { SITE_NAME } from '../../constants/helmetData';
-import PIXEL_CONTENT_CATEGORY from '../../constants/pixelConstants';
+  queryMenu,
+  queryEntry,
+  queryMenuIfUnfetched,
+  queryEntryIfUnfetched,
+} from '../../actions/laborRights';
 
-import { paramsSelector } from 'common/routing/selectors';
+import {
+  menuEntriesSelector,
+  entryDataSelector,
+  entryStatusSelector,
+  entryErrorSelector,
+} from '../../selectors/laborRightsSelector';
 
 import styles from './LaborRightsSingle.module.css';
 
@@ -39,103 +46,90 @@ const idSelector = R.compose(
   paramsSelector,
 );
 
-class LaborRightsSingle extends React.Component {
-  componentDidMount() {
-    this.props.queryMenuIfUnfetched();
-    this.props.queryEntryIfUnfetched(idSelector(this.props));
-    this.props.fetchPermission();
-
-    // send Facebook Pixel 'ViewContent' event
+const useTracking = entryId => {
+  /*
+   * send Facebook Pixel 'ViewContent' event
+   */
+  useEffect(() => {
     ReactPixel.track('ViewContent', {
-      content_ids: [this.props.match.params.id],
+      content_ids: [entryId],
       content_category: PIXEL_CONTENT_CATEGORY.VIEW_LABOR_RIGHT,
     });
-  }
+  }, [entryId]);
+};
 
-  componentDidUpdate(prevProps) {
-    this.props.queryMenuIfUnfetched();
-    this.props.queryEntryIfUnfetched(idSelector(this.props));
+const LaborRightsSingle = () => {
+  const params = useParams();
+  const entryId = params.id;
 
-    // send Facebook Pixel 'ViewContent' event if goto reading another labor rights unit
-    if (idSelector(prevProps) !== idSelector(this.props)) {
-      ReactPixel.track('ViewContent', {
-        content_ids: [idSelector(this.props)],
-        content_category: PIXEL_CONTENT_CATEGORY.VIEW_LABOR_RIGHT,
-      });
-    }
-  }
+  useTracking(entryId);
 
-  render() {
-    const { id, title, description, content, coverUrl, nPublicPages } = this
-      .props.entry
-      ? this.props.entry
-      : {};
-    const { seoTitle = title || '', seoDescription, seoText } = this.props.entry
-      ? this.props.entry
-      : {};
+  const dispatch = useDispatch();
 
-    const { entryStatus, entryError } = this.props;
-    return (
-      <Section>
-        <Helmet>
-          <title itemProp="name" lang="zh-TW">
-            {seoTitle}
-          </title>
-          <meta name="description" content={seoDescription} />
-          <meta
-            property="og:title"
-            content={formatTitle(seoTitle, SITE_NAME)}
+  useEffect(() => {
+    dispatch(queryMenuIfUnfetched());
+  }, [dispatch]);
+
+  useEffect(() => {
+    console.log('trigger', entryId);
+    dispatch(queryEntryIfUnfetched(entryId));
+  }, [dispatch, entryId]);
+
+  const entryStatus = useSelector(entryStatusSelector(entryId));
+  const entryError = useSelector(entryErrorSelector(entryId));
+  const entry = useSelector(entryDataSelector(entryId)) || {};
+  const menuEntries = useSelector(menuEntriesSelector);
+  const index = R.findIndex(R.propEq('id', entryId))(menuEntries);
+  const prevEntry = index > 0 ? menuEntries[index - 1] : undefined;
+  const nextEntry =
+    index < menuEntries.length - 1 ? menuEntries[index + 1] : undefined;
+
+  const { title, description, content, coverUrl, nPublicPages } = entry;
+  const { seoTitle = title || '', seoDescription, seoText } = entry;
+
+  return (
+    <Section>
+      <Helmet>
+        <title itemProp="name" lang="zh-TW">
+          {seoTitle}
+        </title>
+        <meta name="description" content={seoDescription} />
+        <meta property="og:title" content={formatTitle(seoTitle, SITE_NAME)} />
+        <meta property="og:description" content={seoDescription} />
+        <meta property="og:image" content={formatUrl(coverUrl)} />
+        <meta
+          property="og:url"
+          content={formatCanonicalPath(`/labor-rights/${entryId}`)}
+        />
+        <link
+          rel="canonical"
+          href={formatCanonicalPath(`/labor-rights/${entryId}`)}
+        />
+      </Helmet>
+      {R.anyPass([isFetching, isUnfetched])(entryStatus) && <Loader />}
+      {isError(entryStatus) && isUiNotFoundError(entryError) && <NotFound />}
+      {isFetched(entryStatus) && (
+        <div>
+          <Body
+            title={title}
+            seoText={seoText}
+            description={description}
+            content={content}
           />
-          <meta property="og:description" content={seoDescription} />
-          <meta property="og:image" content={formatUrl(coverUrl)} />
-          <meta
-            property="og:url"
-            content={formatCanonicalPath(`/labor-rights/${id}`)}
-          />
-          <link
-            rel="canonical"
-            href={formatCanonicalPath(`/labor-rights/${id}`)}
-          />
-        </Helmet>
-        {R.anyPass([isFetching, isUnfetched])(entryStatus) && <Loader />}
-        {isError(entryStatus) && isUiNotFoundError(entryError) && <NotFound />}
-        {isFetched(entryStatus) && (
-          <div>
-            <Body
-              title={title}
-              seoText={seoText}
-              description={description}
-              content={content}
-            />
-            <FanPageBlock className={styles.fanPageBlock} />
-            {nPublicPages < 0 && (
-              <Section marginTop>
-                <CallToActionFolder />
-              </Section>
-            )}
-            <Footer
-              id={id}
-              prev={this.props.prevEntry}
-              next={this.props.nextEntry}
-            />
-          </div>
-        )}
-      </Section>
-    );
-  }
-}
+          <FanPageBlock className={styles.fanPageBlock} />
+          {nPublicPages < 0 && (
+            <Section marginTop>
+              <CallToActionFolder />
+            </Section>
+          )}
+          <Footer id={entryId} prev={prevEntry} next={nextEntry} />
+        </div>
+      )}
+    </Section>
+  );
+};
 
 LaborRightsSingle.propTypes = {
-  match: PropTypes.shape({
-    params: PropTypes.object.isRequired,
-  }),
-  entry: PropTypes.object,
-  entryStatus: PropTypes.string.isRequired,
-  entryError: PropTypes.object,
-  prevEntry: PropTypes.object,
-  nextEntry: PropTypes.object,
-  queryMenuIfUnfetched: PropTypes.func.isRequired,
-  queryEntryIfUnfetched: PropTypes.func.isRequired,
   fetchPermission: PropTypes.func.isRequired,
 };
 
