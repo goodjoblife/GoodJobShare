f47f63f9537032327ed23e16d406cf824007a2e1
Ming <barry800414@users.noreply.github.com>
2022-12-14 19:40:07 +0800

    feat: 移除網站上的廣告  (#1037)
    * feat: remove all ads on website
    
    * fix: style of experience detail
    
    * fix: remove unused elements & packages

--- src/config/index.js
+++ src/config/index.js
@@ -9,10 +9,6 @@ module.exports = {
     process.env.RAZZLE_GOOGLE_APP_ID ||
     '879657963776-ksbuo26o2svuk8kv7qkmikm1kqqisrb5.apps.googleusercontent.com',
   GTM_ID: process.env.RAZZLE_GTM_ID || 'GTM-K2MRXLG',
-  GOOGLE_AD_CLIENT_ID:
-    process.env.GOOGLE_AD_CLIENT_ID || 'ca-pub-1493014011538331',
-  GOOGLE_AD_MANAGER_NETWORK_ID:
-    process.env.GoogleAdManagerNetworkId || '21909228710',
   PIXEL_ID: process.env.RAZZLE_PIXEL_ID || '603414113402034',
   AMPLITUDE_API_KEY:
     process.env.RAZZLE_AMPLITUDE_API_KEY || '22a596db91e9000a6d19ed6a29865cbd',
