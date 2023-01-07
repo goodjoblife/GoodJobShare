06526500fdd85e382ae2cfa3255ec334bbe1292e
Yen-chi Chen <mark86092@gmail.com>
2023-01-07 05:20:08 +0000

    update

--- src/components/TimeAndSalary/SearchScreen/SearchScreen.js
+++ src/components/TimeAndSalary/SearchScreen/SearchScreen.js
@@ -1,28 +1,25 @@
-import React, { Component, useEffect, useCallback, useMemo } from 'react';
-import { useHistory, useLocation } from 'react-router-dom';
+import React, { useEffect, useMemo } from 'react';
+import { useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import PropTypes from 'prop-types';
-import ImmutablePropTypes from 'react-immutable-proptypes';
-import { compose, setStatic } from 'recompose';
+import { compose } from 'recompose';
 import qs from 'qs';
 import Loading from 'common/Loader';
 import { P } from 'common/base';
 import FanPageBlock from 'common/FanPageBlock';
-import { querySelector, pathSelector } from 'common/routing/selectors';
+import { querySelector } from 'common/routing/selectors';
 import Pagination from 'common/Pagination';
+import { isFetching, isFetched } from 'constants/status';
+import { pageType } from 'constants/companyJobTitle';
+import { useQuery } from 'hooks/routing';
 import {
   queryKeyword,
   keywordMinLength,
 } from '../../../actions/timeAndSalarySearch';
-import { isFetching, isFetched } from 'constants/status';
-import { pageType } from 'constants/companyJobTitle';
-import {
-  searchCriteriaSelector,
-  searchKeywordSelector,
-  pageSelector,
-} from '../common/selectors';
-
 import { validatePage, validateSearchKeyword } from '../common/validators';
+import {
+  keywordFromQuerySelector,
+  pageFromQuerySelector,
+} from '../common/selectors';
 import WorkingHourBlock from './WorkingHourBlock';
 import Helmet from './Helmet';
 import styles from './SearchScreen.module.css';
@@ -39,66 +36,49 @@ function getTitle(keyword) {
   }
 }
 
-function getLinkForData(props, data) {
+function getLinkForData(query, data) {
   if (data.pageType === pageType.COMPANY) {
     return `/companies/${encodeURIComponent(data.name)}/overview${qs.stringify(
-      { ...querySelector(props), p: 1 },
+      { ...query, p: 1 },
       { addQueryPrefix: true },
     )}`;
   } else if (data.pageType === pageType.JOB_TITLE) {
     return `/job-titles/${encodeURIComponent(data.name)}/overview${qs.stringify(
-      { ...querySelector(props), p: 1 },
+      { ...query, p: 1 },
       { addQueryPrefix: true },
     )}`;
   }
   return '';
 }
 
-const useSearch = () => {
-  const location = useLocation();
-  return location.search;
-};
+const keywordSelector = compose(
+  validateSearchKeyword,
+  keywordFromQuerySelector,
+);
 
-const useQuery = () => {
-  const search = useSearch();
-  return useMemo(() => qs.parse(search, { ignoreQueryPrefix: true }), [search]);
-};
-
-const useKeyword = () => {
-  const query = useQuery();
-  return useMemo(() => validateSearchKeyword(query.q), [query]);
-};
-
-const usePage = () => {
-  const query = useQuery();
-  return useMemo(() => validatePage(query.p), [query]);
-};
+const pageSelector = compose(
+  validatePage,
+  pageFromQuerySelector,
+);
 
 const SearchScreen = () => {
-  const keyword = useKeyword();
-  const page = usePage();
   const query = useQuery();
+  const keyword = useMemo(() => keywordSelector(query), [query]);
+  const page = useMemo(() => pageSelector(query), [query]);
 
   const dispath = useDispatch();
 
+  const keywordFromRedux = useSelector(state =>
+    state.timeAndSalarySearch.get('keyword'),
+  );
   const data = useSelector(state => state.timeAndSalarySearch.get('data'));
   const status = useSelector(state => state.timeAndSalarySearch.get('status'));
 
-  console.log(query, keyword, page);
-
   const history = useHistory();
-  const location = useLocation();
-
-  const redirectOnSingleResult = () => {
-    if (data.size === 1) {
-      const firstData = data.get(0).toJS();
-      history.replace(getLinkForData({ location }, firstData));
-    }
-  };
 
   useEffect(() => {
-    dispath(queryKeyword({ keyword })).then(() => redirectOnSingleResult());
-  }, [keyword]);
+    dispath(queryKeyword({ keyword }));
+  }, [dispath, keyword]);
 
   const pageSize = 10;
   const totalNum = data.size;
@@ -106,6 +86,13 @@ const SearchScreen = () => {
   const title = getTitle(keyword);
 
   const raw = data.slice((page - 1) * pageSize, page * pageSize).toJS();
+
+  if (isFetched(status) && data.size === 1 && keyword === keywordFromRedux) {
+    const firstData = data.get(0).toJS();
+    history.replace(getLinkForData(query, firstData));
+    console.log('push!?');
+    return null;
+  }
 
   return (
     <section className={styles.searchResult}>
@@ -122,7 +109,7 @@ const SearchScreen = () => {
           key={i}
           pageType={o.pageType}
           name={o.name}
-          to={getLinkForData({ location }, o)}
+          to={getLinkForData(query, o)}
           dataCount={o.salary_work_time_statistics.count}
         />
       ))}
@@ -139,26 +126,10 @@ const SearchScreen = () => {
   );
 };
 
-SearchScreen.propTypes = {
-  data: ImmutablePropTypes.list,
-  status: PropTypes.string,
-  // eslint-disable-next-line react/no-unused-prop-types
-  match: PropTypes.shape({
-    path: PropTypes.string.isRequired,
-    params: PropTypes.object.isRequired,
-  }),
-  queryKeyword: PropTypes.func,
-  history: PropTypes.shape({
-    push: PropTypes.func.isRequired,
-  }).isRequired,
+SearchScreen.fetchData = ({ store: { dispatch }, ...props }) => {
+  const query = querySelector(props);
+  const keyword = keywordSelector(query);
+  return dispatch(queryKeyword({ keyword }));
 };
 
-const ssr = setStatic('fetchData', ({ store: { dispatch }, ...props }) => {
-  const keyword = validateSearchKeyword(searchKeywordSelector(props));
-
-  return dispatch(queryKeyword({ keyword }));
-});
-
-const hoc = compose(ssr);
-
-export default hoc(SearchScreen);
+export default SearchScreen;
