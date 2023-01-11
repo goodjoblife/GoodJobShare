fb2f9d2aead0f825af44d1bfbdc1ebeaf8f874e2
Wendell Liu <cuk.bas@gmail.com>
2023-01-11 23:51:23 +0800

    fix: no title

--- src/components/PlanPage/index.js
+++ src/components/PlanPage/index.js
@@ -13,30 +13,6 @@ const ssr = setStatic('fetchData', ({ store: { dispatch } }) => {
   return Promise.all([dispatch(fetchSubscriptionPlans())]);
 });
 
-// Promise.resolve([
-//   {
-//     skuId: 'submit-data',
-//     type: 'SubmitData',
-//     title: '留下一筆資料',
-//     description: '解鎖全站 7 天',
-//     amount: 0,
-//   },
-//   {
-//     skuId: '1-months-subscription',
-//     type: 'BuySubscription',
-//     title: '包月方案',
-//     description: '解鎖全站 1 個月',
-//     amount: 99,
-//   },
-//   {
-//     skuId: '3-months-subscription',
-//     type: 'BuySubscription',
-//     title: '包季方案',
-//     description: '解鎖全站 3 個月',
-//     amount: 149,
-//   },
-// ]);
-
 const PlanPage = () => {
   const dispatch = useDispatch();
 
