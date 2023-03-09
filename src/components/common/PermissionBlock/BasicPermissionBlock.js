a1cc6087bd0d54e5f07f1e2c24d5108bce7fb4bb
peteranny <peteranny2@gmail.com>
2023-03-09 19:49:43 +0800

    Move out ModalContent

--- src/components/common/PermissionBlock/BasicPermissionBlock.js
+++ src/components/common/PermissionBlock/BasicPermissionBlock.js
@@ -1,55 +1,19 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useCallback, useState } from 'react';
 import PropTypes from 'prop-types';
 import cn from 'classnames';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 import faLock from '@fortawesome/fontawesome-free-solid/faLock';
 
-import P from 'common/base/P';
 import Modal from 'common/Modal';
-import { Heading } from 'common/base';
 import CallToLoginShareButton from './CallToLoginShareButton';
 import styles from './PermissionBlock.module.css';
-import { queryExperienceCountIfUnfetched } from 'actions/experiences';
-import { queryTimeAndSalaryCountIfUnfetched } from 'actions/timeAndSalary';
-import {
-  experienceCountSelector,
-  timeAndSalaryCountSelector,
-} from 'selectors/countSelector';
-
-const ModalContent = () => {
-  const experienceCount = useSelector(experienceCountSelector);
-  const timeAndSalaryCount = useSelector(timeAndSalaryCountSelector);
-
-  return (
-    <React.Fragment>
-      <div className={styles.headingContainer}>
-        <FontAwesomeIcon icon={faLock} className={styles.headingIcon} />
-        <Heading size="sl" Tag="h3">
-          留下一筆資料，馬上解鎖全站資料
-        </Heading>
-      </div>
-      <P size="l" className={styles.ctaText}>
-        解鎖全站共{' '}
-        <strong>{timeAndSalaryCount + experienceCount} 筆薪資、面試資料</strong>
-        。若你已經留過資料，登入即可查看全文！
-      </P>
-    </React.Fragment>
-  );
-};
+import ModalContent from './ModalContent';
 
 const BasicPermissionBlock = ({ simple, to, rootClassName }) => {
   const [isModalOpen, setModalOpen] = useState(false);
   const toggleModal = useCallback(() => setModalOpen(!isModalOpen), [
     isModalOpen,
   ]);
-
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(queryExperienceCountIfUnfetched());
-    dispatch(queryTimeAndSalaryCountIfUnfetched());
-  }, [dispatch]);
 
   if (simple) {
     return (
