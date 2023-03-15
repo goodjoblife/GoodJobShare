b845b6d5340cd1532022cead1938659f5b1630a0
Wendell Liu <cuk.bas@gmail.com>
2023-03-15 21:34:43 +0800

    feat: remove the toast

--- src/components/ToastNotification/Notification.js
+++ src/components/ToastNotification/Notification.js
@@ -1,36 +1,56 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 import { NOTIFICATION_TYPE } from 'constants/toastNotification';
 import useRemoveNotification from 'hooks/toastNotification/useRemoveToast';
+import useTimer, { countingStatusMap } from 'hooks/useTimer';
 import Close from 'common/icons/Close';
 
-import { getOffsetY } from './helpers';
+import { getOffsetY, statusMap } from './helpers';
 import styles from './Notification.module.css';
 
+const FADE_OUT_TIME = 3000;
+const SUNSET_TIME_IN_SEC = 0.6;
 const HEIGHT = 100;
 const MARGIN_Y = 20;
 
 const calOffsetY = getOffsetY(HEIGHT)(MARGIN_Y);
 
 const Notification = ({ index, type, content, id }) => {
+  const [status, setStatus] = useState(statusMap.ACTIVE);
   const removeNotification = useRemoveNotification();
-  const remove = useCallback(() => {
-    removeNotification(id);
-  }, [id, removeNotification]);
+  const toSunset = useCallback(() => {
+    setStatus(statusMap.SUNSET);
+  }, [setStatus]);
+
+  useEffect(() => {
+    if (status === statusMap.SUNSET) {
+      setTimeout(() => {
+        setStatus(statusMap.INACTIVE);
+      }, SUNSET_TIME_IN_SEC * 1000);
+    }
+
+    if (status === statusMap.INACTIVE) {
+      removeNotification(id);
+    }
+  }, [status, removeNotification, id]);
+
+  useTimer(toSunset, FADE_OUT_TIME, countingStatusMap.counting);
 
   const offsetY = `${calOffsetY(index)}px`;
+  const opacity = status === statusMap.ACTIVE ? 100 : 0;
 
   return (
     <div
       className={styles.container}
       style={{
         transform: `translateY(${offsetY})`,
-        transition: 'transform 0.6s ease-in',
+        opacity,
+        transition: `transform 0.6s ease-in, opacity ${SUNSET_TIME_IN_SEC}s ease-in`,
       }}
     >
       <div>
-        <button onClick={remove}>
+        <button onClick={toSunset}>
           <Close />
         </button>
       </div>
--- src/components/ToastNotification/helpers.js
+++ src/components/ToastNotification/helpers.js
@@ -1,3 +1,9 @@
 export const getOffsetY = eleHeight => marginY => index => {
   return index * (eleHeight + marginY);
 };
+
+export const statusMap = {
+  ACTIVE: 'ACTIVE',
+  SUNSET: 'SUNSET',
+  INACTIVE: 'INACTIVE',
+};
