93e97d9dc9d4340c782c8ab4a398c3fb0861de0e
Wendell Liu <wendell.liu@houzz.com>
2023-03-30 21:36:11 +0800

    todo: test on login modal

--- src/components/common/PermissionBlock/LoginModal.js
+++ src/components/common/PermissionBlock/LoginModal.js
@@ -1,32 +1,20 @@
 import React from 'react';
-import cn from 'classnames';
-import { useFacebookLogin, useGoogleLogin } from 'hooks/login';
+import { P } from 'common/base';
+import FacebookLoginButton from 'common/Login/FacebookLoginButton';
+import GoogleLoginButton from 'common/Login/GoogleLoginButton';
+
 import styles from './PermissionBlock.module.css';
 import Modal from '../Modal';
-import { P } from 'common/base';
 
 const LoginModal = ({ ...props }) => {
-  const fbLogin = useFacebookLogin();
-  const googleLogin = useGoogleLogin();
-
   return (
     <Modal closableOnClickOutside {...props}>
       <P className={styles.loginTitle} center bold size="l">
         登入以查看文章權限
       </P>
       <div className={styles.loginBtnContainer}>
-        <button
-          className={`${cn('buttonCircleM')} ${styles.btn} ${styles.btnFb}`}
-          onClick={() => fbLogin()}
-        >
-          <pre>Facebook 登入</pre>
-        </button>
-        <button
-          className={`${cn('buttonCircleM')} ${styles.btn} ${styles.btnGoogle}`}
-          onClick={() => googleLogin()}
-        >
-          <pre>Google 登入</pre>
-        </button>
+        <FacebookLoginButton />
+        <GoogleLoginButton />
       </div>
     </Modal>
   );
