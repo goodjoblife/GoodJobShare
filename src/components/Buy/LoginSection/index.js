d103a5a70a82221574b23a8c044e94d404610be1
Wendell Liu <wendell.liu@houzz.com>
2023-04-07 23:52:26 +0800

    feat: Login Section

--- src/components/Buy/LoginSection/index.js
+++ src/components/Buy/LoginSection/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
+import { P } from 'common/base';
+import FacebookLoginButton from 'common/Login/FacebookLoginButton';
+import GoogleLoginButton from 'common/Login/GoogleLoginButton';
+
+import styles from './LoginSection.module.css';
+
 const LoginSection = () => {
-  return <div>please login</div>;
+  return (
+    <div>
+      <P className={styles.loginTitle} center bold size="l">
+        登入後以信用卡付款
+      </P>
+      <div className={styles.loginBtnContainer}>
+        <FacebookLoginButton />
+        <GoogleLoginButton />
+      </div>
+    </div>
+  );
 };
 
 export default LoginSection;
