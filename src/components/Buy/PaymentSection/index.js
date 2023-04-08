8b4d67580606828a38ad0c2f22319cd129968a17
Wendell Liu <cuk.bas@gmail.com>
2023-04-08 20:50:32 +0800

    Fix: buy page issues (#1083)
    * feat: propagate the props to apply the section style
    
    * todo: test on login modal
    
    * feat: Login Section
    
    * feat: Login Section
    
    * fix: media query mismatch

--- src/components/Buy/PaymentSection/index.js
+++ src/components/Buy/PaymentSection/index.js
@@ -6,13 +6,17 @@ import Button from 'common/button/ButtonRect';
 import Card from 'common/Card';
 import Label from 'common/form/Label';
 import TapPayContext from 'common/tappay/TapPayContext';
+import { useIsLoggedIn } from 'hooks/auth';
+
 import { CardCCV, CardExpirationDate, CardNumber } from './TappayElement';
 import Row from './Row';
 import CreditCards from './CreditCards';
 import styles from './PaymentSection.module.css';
 import useForm from './useForm';
+import LoginSection from '../LoginSection';
 
 const PaymentSection = ({ tapPayCard, loadTapPayCard, skuId, ...props }) => {
+  const isLoggedIn = useIsLoggedIn();
   const [isPrimary, setPrimary] = useState(false);
   const { activeCardType, canGetPrime, submit } = useForm({
     tapPayCard,
@@ -33,57 +37,61 @@ const PaymentSection = ({ tapPayCard, loadTapPayCard, skuId, ...props }) => {
       <Subheading className={styles.title} size="l">
         填寫信用卡資料
       </Subheading>
-      <form onSubmit={onSubmit}>
-        <Card className={styles.form}>
-          <div className={styles.inputSection}>
-            <Row>
-              <Label className={styles.label} isRequired>
-                卡號
-              </Label>
-              <div className={styles.cardNumberGroup}>
-                <div className={styles.cardIcons}>
-                  <CreditCards activeCardType={activeCardType} />
+      <Card className={styles.form}>
+        {isLoggedIn ? (
+          <form onSubmit={onSubmit}>
+            <div className={styles.inputSection}>
+              <Row>
+                <Label className={styles.label} isRequired>
+                  卡號
+                </Label>
+                <div className={styles.cardNumberGroup}>
+                  <div className={styles.cardIcons}>
+                    <CreditCards activeCardType={activeCardType} />
+                  </div>
+                  <CardNumber />
                 </div>
-                <CardNumber />
-              </div>
-            </Row>
-            <Row half>
-              <Label className={styles.label} isRequired>
-                到期日 (MM/YY)
-              </Label>
-              <CardExpirationDate />
-            </Row>
-            <Row half>
-              <Label className={styles.label} isRequired>
-                安全碼
-              </Label>
-              <CardCCV />
-            </Row>
-            <Row>
-              <Checkbox
-                label="設為主要付款方式"
-                checked={isPrimary}
-                value="primary"
-                onChange={e => setPrimary(e.target.checked)}
-                margin=""
-              />
-            </Row>
-          </div>
-          <div className={styles.submitSection}>
-            <Row>
-              <Button type="submit" disabled={!canGetPrime}>
-                付款
-              </Button>
-            </Row>
-            <Row>
-              <P className={styles.note} size="s">
-                本站採用 TapPay 金流交易系統，資料傳輸以 SSL 2048bit
-                加密技術保護
-              </P>
-            </Row>
-          </div>
-        </Card>
-      </form>
+              </Row>
+              <Row half>
+                <Label className={styles.label} isRequired>
+                  到期日 (MM/YY)
+                </Label>
+                <CardExpirationDate />
+              </Row>
+              <Row half>
+                <Label className={styles.label} isRequired>
+                  安全碼
+                </Label>
+                <CardCCV />
+              </Row>
+              <Row>
+                <Checkbox
+                  label="設為主要付款方式"
+                  checked={isPrimary}
+                  value="primary"
+                  onChange={e => setPrimary(e.target.checked)}
+                  margin=""
+                />
+              </Row>
+            </div>
+            <div className={styles.submitSection}>
+              <Row>
+                <Button type="submit" disabled={!canGetPrime}>
+                  付款
+                </Button>
+              </Row>
+              <Row>
+                <P className={styles.note} size="s">
+                  本站採用 TapPay 金流交易系統，資料傳輸以 SSL 2048bit
+                  加密技術保護
+                </P>
+              </Row>
+            </div>
+          </form>
+        ) : (
+          <LoginSection />
+        )}
+      </Card>
     </Section>
   );
 };
@@ -94,10 +102,11 @@ PaymentSection.propTypes = {
   skuId: PropTypes.string,
 };
 
-export default ({ skuId }) => (
+export default ({ skuId, ...restProps }) => (
   <TapPayContext.Consumer>
     {({ loadTapPayCard, tapPayCard }) => (
       <PaymentSection
+        {...restProps}
         loadTapPayCard={loadTapPayCard}
         tapPayCard={tapPayCard}
         skuId={skuId}
