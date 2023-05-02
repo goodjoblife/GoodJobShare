fb2de5f6ed2bdb72c5f31ef71c14c014d3ac04d8
Peter Shih <peteranny2@gmail.com>
2023-05-02 10:51:57 +0800

    Add comments

--- src/components/TimeAndSalary/common/injectHideContentBlock.js
+++ src/components/TimeAndSalary/common/injectHideContentBlock.js
@@ -8,6 +8,9 @@ export default hideRange => rows => {
   const hideIndex = hideRange[0];
   const nHides = hideRange.length;
   const shareLink = useShareLink();
+
+  // Replace original cells with locked cells
+  // on small screens
   rows.forEach(row => {
     row.props.children.splice(
       hideIndex,
@@ -26,6 +29,9 @@ export default hideRange => rows => {
       }),
     );
   });
+
+  // Add locked cells on regular screens
+  // that spans multiple columns
   if (rows.length > 0) {
     for (let i = 0; i < rows.length; i++) {
       rows[i].props.children.splice(
