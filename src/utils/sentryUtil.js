2f6068cf9d3fda20b34f4d30f7a2419857b6e7cf
Ming <barry800414@users.noreply.github.com>
2023-05-13 11:37:56 +0800

    Feat: disable sentry by default (#1100)

--- src/utils/sentryUtil.js
+++ src/utils/sentryUtil.js
@@ -14,9 +14,17 @@ function initSentry() {
         if (config.GIT_SHA1) {
           window.Raven.config(config.SENTRY_DSN, {
             release: config.GIT_SHA1,
+            /* 
+              Disable sending error message by default
+              use window.Raven.captureException to mannually
+              send error
+            */
+            enabled: false,
           }).install();
         } else {
-          window.Raven.config(config.SENTRY_DSN).install();
+          window.Raven.config(config.SENTRY_DSN, {
+            enabled: false,
+          }).install();
         }
       }
     }
