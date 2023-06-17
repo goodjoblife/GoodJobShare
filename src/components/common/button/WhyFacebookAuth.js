23fda84f0e2679e2f67ce3ae084ef01dfef0a72b
Peter Shih <peteranny2@gmail.com>
2023-06-17 20:46:39 +0800

    [工作/薪資] 修正登入上傳 (#1134)
    Co-authored-by: barry800414 <barry800414@gmail.com>

--- src/components/common/button/WhyFacebookAuth.js
+++ src/components/common/button/WhyFacebookAuth.js
@@ -8,8 +8,8 @@ import Feedback from 'common/Feedback';
 const WhyFacebookAuth = ({ buttonClick }) => (
   <Feedback
     buttonClick={buttonClick}
-    heading="為何需要 FB 帳戶驗證？"
-    info="為了避免使用者大量輸入假資訊，我們會以您的 Facebook 帳戶做驗證。但別擔心！您的帳戶資訊不會以任何形式被揭露、顯示。"
+    heading="為何需要 FB/Google 帳戶驗證？"
+    info="為了避免使用者大量輸入假資訊，我們會以您的 Facebook/Google 帳戶做驗證。但別擔心！您的帳戶資訊不會以任何形式被揭露、顯示。"
     buttonText="好，我知道了"
     Icon={Question}
   />
