4bd69d591689e06801694b2a05e41bb8e59227c3
barry800414 <barry800414@gmail.com>
2023-06-23 22:52:10 +0800

    refactor: use ENVIRONMENT, instead of ENV

--- src/utils/rollbar.js
+++ src/utils/rollbar.js
@@ -1,12 +1,12 @@
 import Rollbar from 'rollbar';
 
-import { ENV, ROLLBAR_ACCESS_TOKEN } from '../config';
+import { ENVIRONMENT, ROLLBAR_ACCESS_TOKEN } from '../config';
 
 const rollbar = new Rollbar({
   accessToken: ROLLBAR_ACCESS_TOKEN,
   captureUncaught: false, // Disable automatic capturing of uncaught errors
   captureUnhandledRejections: false, // Disable automatic capturing of unhandled promise rejections
-  environment: ENV,
+  environment: ENVIRONMENT,
 });
 
 export default rollbar;
