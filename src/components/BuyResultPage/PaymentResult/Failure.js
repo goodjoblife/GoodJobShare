e75e80078c961c1a04078ce744503cb4781c4905
Peter Shih <peteranny2@gmail.com>
2023-06-25 22:21:12 +0800

    [購買結果頁面] 新增"再試一次"按鈕 (#1141)

--- src/components/BuyResultPage/PaymentResult/Failure.js
+++ src/components/BuyResultPage/PaymentResult/Failure.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import Heading from 'common/base/Heading';
@@ -6,8 +7,13 @@ import P from 'common/base/P';
 
 import styles from './PaymentResult.module.css';
 import WarningIcon from './warning.svg';
+import Button from 'common/button/Button';
 
 const Failure = ({ publicId }) => {
+  const history = useHistory();
+  const tryAgain = useCallback(() => {
+    history.push('/plans');
+  }, [history]);
   return (
     <div className={styles.content}>
       <Heading className={styles.title}>交易失敗</Heading>
@@ -18,6 +24,14 @@ const Failure = ({ publicId }) => {
       <P Tag="p" className={styles.description}>
         {`聯絡 findyourgoodjob@gmail.com`}
       </P>
+      <Button
+        className={styles.action}
+        btnStyle="gray"
+        circleSize="md"
+        onClick={tryAgain}
+      >
+        再試一次
+      </Button>
     </div>
   );
 };
