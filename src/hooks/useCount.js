021ebaeeb49923822dd7b5cdbe67d60b20dc2830
Wendell Liu <cuk.bas@gmail.com>
2023-07-26 13:39:09 +0000

    feat: bind the fetching with data retrieving (#1151)

--- src/hooks/useCount.js
+++ src/hooks/useCount.js
@@ -1,13 +1,30 @@
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 import {
   experienceCountSelector,
   timeAndSalaryCountSelector,
 } from 'selectors/countSelector';
+import { queryExperienceCountIfUnfetched } from 'actions/experiences';
+import { queryTimeAndSalaryCountIfUnfetched } from 'actions/timeAndSalary';
 
-export const useExperienceCount = () => useSelector(experienceCountSelector);
-export const useTimeAndSalaryCount = () =>
-  useSelector(timeAndSalaryCountSelector);
+export const useExperienceCount = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(queryExperienceCountIfUnfetched());
+  }, [dispatch]);
+
+  return useSelector(experienceCountSelector);
+};
+
+export const useTimeAndSalaryCount = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(queryTimeAndSalaryCountIfUnfetched());
+  }, [dispatch]);
+
+  return useSelector(timeAndSalaryCountSelector);
+};
 
 export const useTotalCount = () => {
   const experienceCount = useExperienceCount();
