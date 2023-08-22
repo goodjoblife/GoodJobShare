6805c7a3b54d7663b9cdecc7c048d73d999fcb63
Peteranny <peteranny2@gmail.com>
2023-08-22 22:26:15 +0800

    Add route

---
+++ src/components/Me/SubscriptionsPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import SubscriptionWrapper from './SubscriptionWrapper';
+
+const SubscriptionsPage = () => {
+  return <SubscriptionWrapper />;
+};
+
+export default SubscriptionsPage;
