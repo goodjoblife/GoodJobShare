65170aff335409d93f25e995e1fcbef9aff66dc2
Yen-chi Chen <mark86092@gmail.com>
2023-09-10 13:32:41 +0800

    refactor: 改寫 ViewLog & Trace & (#1162)

--- src/components/CompanyAndJobTitle/TimeAndSalary/ViewLog.js
+++ src/components/CompanyAndJobTitle/TimeAndSalary/ViewLog.js
@@ -1,36 +1,18 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import { useViewSalaryWorkTimes } from 'hooks/viewLog';
 
-import { viewSalaryWorkTimes } from 'actions/viewLog';
-
-class ViewLog extends Component {
-  componentDidMount() {
-    const { contentIds } = this.props;
+const ViewLog = ({ pageName, page, contentIds }) => {
+  // Send view to backend
+  const viewSalaryWorkTimes = useViewSalaryWorkTimes();
+  useEffect(() => {
     const referrer = window.location.href;
+    viewSalaryWorkTimes({ contentIds, referrer });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [pageName, page, viewSalaryWorkTimes]);
 
-    this.props.viewSalaryWorkTimes({ contentIds, referrer });
-  }
-
-  componentDidUpdate(prevProps) {
-    const prevPageName = prevProps.pageName;
-    const prevPage = prevProps.page;
-    const pageName = this.props.pageName;
-    const page = this.props.page;
-
-    if (prevPageName !== pageName || prevPage !== page) {
-      const { contentIds } = this.props;
-      const referrer = window.location.href;
-
-      this.props.viewSalaryWorkTimes({ contentIds, referrer });
-    }
-  }
-
-  render() {
-    return null;
-  }
-}
+  return null;
+};
 
 ViewLog.propTypes = {
   // key
@@ -38,17 +20,6 @@ ViewLog.propTypes = {
   page: PropTypes.number.isRequired,
 
   contentIds: PropTypes.arrayOf(PropTypes.string).isRequired,
-
-  // method
-  viewSalaryWorkTimes: PropTypes.func.isRequired,
 };
 
-const mapStateToProps = state => ({});
-
-const mapDispatchToProps = dispatch =>
-  bindActionCreators({ viewSalaryWorkTimes }, dispatch);
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(ViewLog);
+export default ViewLog;
