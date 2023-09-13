a1ae60e62672e3138375ca58e2ab77b9e517f1f4
Yen-chi Chen <mark86092@gmail.com>
2023-09-14 00:05:57 +0800

    refactor: login with google now use new SDK and flow (#1108)

--- src/components/common/Login/GoogleLoginButton.js
+++ src/components/common/Login/GoogleLoginButton.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { useContext } from 'react';
 import cn from 'classnames';
-import { useGoogleLogin } from 'hooks/login';
+import GoogleContext from 'contexts/GoogleContext';
 import styles from './LoginButton.module.css';
 
 const GoogleLoginButton = () => {
-  const googleLogin = useGoogleLogin();
+  const googleAuth = useContext(GoogleContext);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    if (googleAuth && ref.current) {
+      console.log('renderButton');
+      googleAuth.renderButton(ref.current, {});
+    }
+  }, [googleAuth]);
 
   return (
-    <button
-      className={cn('buttonCircleM', styles.btn, styles.btnGoogle)}
-      onClick={googleLogin}
-    >
-      <pre>Google 登入</pre>
-    </button>
+    <div ref={ref} className={cn(styles.btn)}>
+      Google 登入
+    </div>
   );
 };
 
--- src/components/common/LoginModalContextProvider/index.js
+++ src/components/common/LoginModalContextProvider/index.js
@@ -1,12 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import LoginModalContext from '../../../contexts/LoginModalContext';
 
 const LoginModalContextProvider = ({ children }) => {
   const [isLoginModalDisplayed, setLoginModalDisplayed] = useState(false);
+
+  const contextValue = useMemo(
+    () => ({
+      isLoginModalDisplayed,
+      setLoginModalDisplayed,
+    }),
+    [isLoginModalDisplayed],
+  );
+
   return (
-    <LoginModalContext.Provider
-      value={{ isLoginModalDisplayed, setLoginModalDisplayed }}
-    >
+    <LoginModalContext.Provider value={contextValue}>
       {children}
     </LoginModalContext.Provider>
   );
