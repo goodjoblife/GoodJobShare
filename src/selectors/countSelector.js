3a1e4bb2fc8af8f11322d653c7d7fdc13e19ab44
Yen-chi Chen <mark86092@gmail.com>
2023-09-16 16:54:15 +0000

    prune

--- src/selectors/countSelector.js
+++ src/selectors/countSelector.js
@@ -1,13 +1,4 @@
-import { compose, length, prop } from 'ramda';
-import { menuStateSelector } from 'selectors/laborRightsSelector';
-
 export const experienceCountSelector = state => state.experiences.get('count');
 
 export const timeAndSalaryCountSelector = state =>
   state.timeAndSalary.get('count');
-
-export const laborRightsCountSelector = compose(
-  length,
-  prop('data'),
-  menuStateSelector,
-);
--- src/selectors/laborRightsSelector.js
+++ src/selectors/laborRightsSelector.js
@@ -1,18 +1,6 @@
 import R, { path } from 'ramda';
-import { fromJS } from 'immutable';
+
 import fetchingStatus from '../constants/status';
-
-export const menuEntriesSelector = R.path(['laborRights', 'menuEntries']);
-
-export const menuStatusSelector = R.pipe(
-  R.path(['laborRights', 'menuStatus']),
-  fromJS,
-);
-
-export const menuErrorSelector = R.pipe(
-  R.path(['laborRights', 'menuError']),
-  fromJS,
-);
 
 export const menuStateSelector = path(['laborRights', 'menuState']);
 
