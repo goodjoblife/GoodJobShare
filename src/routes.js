f5895ee52e56430cba216b0b163f1b2cfb80e2f5
Yen-chi Chen <mark86092@gmail.com>
2023-09-17 13:45:05 +0000

    Prune

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import LandingPage from './containers/LandingPage';
+import LandingPage from './components/LandingPage';
 import LaborRightsMenu from './components/LaborRightsMenu';
 import LaborRightsSingle from './components/LaborRightsSingle';
 import TimeAndSalary from './components/TimeAndSalary';
