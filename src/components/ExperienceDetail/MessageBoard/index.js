f659dc2d7b4e31e54787ea9d327b3703f427c1b0
Yen-chi Chen <mark86092@gmail.com>
2023-09-19 14:55:13 +0000

    update

--- src/components/ExperienceDetail/MessageBoard/index.js
+++ src/components/ExperienceDetail/MessageBoard/index.js
@@ -1,24 +1,16 @@
-import React, {
-  useState,
-  useEffect,
-  useCallback,
-  useMemo,
-  useContext,
-  Fragment,
-} from 'react';
+import React, { useState, useEffect, useCallback, Fragment } from 'react';
 import { Element as ScrollElement, scroller } from 'react-scroll';
 import PropTypes from 'prop-types';
 import Button from 'common/button/Button';
 import { P } from 'common/base';
 import ButtonGroup from 'common/button/ButtonGroup';
 import Loader from 'common/Loader';
-import { useLogin, useFacebookLogin } from 'hooks/login';
-import LoginModalContext from 'contexts/LoginModalContext';
 import useQueryReplies from '../hooks/useQueryReplies';
 import useLikeReply from '../hooks/useLikeReply';
 import useCreateReply from '../hooks/useCreateReply';
 import CommentBlock from './CommentBlock';
 import styles from './MessageBoard.module.css';
+import useLoginFlow from './useLoginFlow';
 
 const recommendedSentences = [
   '詳細給推',
@@ -31,67 +23,10 @@ const recommendedSentences = [
 
 const REPLIES_BOTTOM = 'REPLIES_BOTTOM';
 
-const useLoginFlow = callback => {
-  const [state, setState] = useState('init');
-  const [isLoggedIn] = useLogin();
-  const { isLoginModalDisplayed, setLoginModalDisplayed } = useContext(
-    LoginModalContext,
-  );
-
-  console.log(
-    state,
-    'isLoggedIn',
-    isLoggedIn,
-    'isLoginModalDisplayed',
-    isLoginModalDisplayed,
-  );
-
-  useEffect(() => {
-    if (state === 'submitting_check_logged_in' && isLoggedIn) {
-      setState('submitting');
-    }
-  }, [callback, isLoggedIn, state]);
-
-  useEffect(() => {
-    if (state === 'submitting') {
-      setState('submitting_check_api');
-      callback().then(() => {
-        setState('init');
-      });
-    }
-  }, [callback, state]);
-
-  useEffect(() => {
-    if (state === 'submitting_check_logged_in' && !isLoggedIn) {
-      setState('init');
-    }
-  }, [isLoggedIn, state]);
-
-  useEffect(() => {
-    if (state === 'submitting_open_modal' && !isLoginModalDisplayed) {
-      // 當 modal 關閉，檢查登入狀態
-      setState('submitting_check_logged_in');
-    }
-  }, [isLoginModalDisplayed, state]);
-
-  const startFlow = useCallback(() => {
-    if (!isLoggedIn) {
-      setLoginModalDisplayed(true);
-      setState('submitting_open_modal');
-    } else {
-      setState('submitting');
-    }
-  }, [isLoggedIn, setLoginModalDisplayed]);
-
-  const isRunning = useMemo(() => state !== 'init', [state]);
-
-  return [startFlow, isRunning];
-};
-
-const SubmitCommentBlock = ({ experienceId }) => {
+const MessageBoard = ({ experienceId }) => {
   const [comment, setComment] = useState('');
   const createReply = useCreateReply(experienceId);
-  const [, queryReplies] = useQueryReplies(experienceId);
+  const [repliesState, queryReplies] = useQueryReplies(experienceId);
 
   const submitCommentCallback = useCallback(async () => {
     await createReply(comment);
@@ -102,10 +37,15 @@ const SubmitCommentBlock = ({ experienceId }) => {
 
   const [submitComment, isSubmitting] = useLoginFlow(submitCommentCallback);
 
-  console.log('comment', comment);
+  // fetch when experienceId change
+  useEffect(() => {
+    queryReplies();
+  }, [queryReplies]);
+
+  const likeReply = useLikeReply();
 
   return (
-    <Fragment>
+    <div className={styles.container}>
       <textarea
         rows="5"
         placeholder="寫下您的留言、意見"
@@ -134,23 +74,6 @@ const SubmitCommentBlock = ({ experienceId }) => {
           發佈留言
         </Button>
       </div>
-    </Fragment>
-  );
-};
-
-const MessageBoard = ({ experienceId }) => {
-  const [repliesState, queryReplies] = useQueryReplies(experienceId);
-
-  // fetch when experienceId change
-  useEffect(() => {
-    queryReplies();
-  }, [queryReplies]);
-
-  const likeReply = useLikeReply();
-
-  return (
-    <div className={styles.container}>
-      <SubmitCommentBlock experienceId={experienceId} />
       <div className={styles.commentBlocks}>
         {repliesState.loading || !repliesState.value ? (
           <Loader size="s" />
