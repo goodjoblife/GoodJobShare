76acaf16e95d2faf3928151be9683a76a6f08ad6
Peter Shih <peteranny2@gmail.com>
2024-01-26 23:54:48 +0800

    [面試經驗] 取出可共用的上傳元件 (#1201)
    title:	[面試經驗] 取出可共用的上傳元件
    state:	OPEN
    author:	peteranny
    labels:	
    assignees:	
    reviewers:	barry800414 (Approved)
    projects:	
    milestone:	
    number:	1201
    url:	https://github.com/goodjoblife/GoodJobShare/pull/1201
    additions:	174
    deletions:	132
    auto-merge:	disabled
    --
    #886 
    
    ## 這個 PR 是？ <!-- 必填 -->
    
    面試經驗的上傳部分可以抽出，與其他表單共用
    
    
    ## 我應該如何手動測試？ <!-- 必填 -->
    
    - [ ] http://localhost:3000/share 面試表單應該不受影響

---
+++ src/components/ShareExperience/common/SubmittableFormBuilder.js
@@ -0,0 +1,135 @@
+import React, { useCallback, Fragment, useState } from 'react';
+import PropTypes from 'prop-types';
+import { useHistory } from 'react-router';
+
+import FormBuilder from 'common/FormBuilder';
+import ConfirmModal from 'common/FormBuilder/Modals/ConfirmModal';
+import Footer from './TypeFormFooter';
+import { useExperienceCount, useSalaryWorkTimeCount } from 'hooks/useCount';
+
+const SubmittableTypeForm = ({
+  open,
+  questions,
+  header,
+  onSubmit,
+  onSubmitError,
+  onClose,
+}) => {
+  const history = useHistory();
+  const [submitStatus, setSubmitStatus] = useState('unsubmitted');
+  const [errorMessage, setErrorMessage] = useState('');
+  const handleSubmit = useCallback(
+    async draft => {
+      try {
+        if (submitStatus === 'submitting') {
+          return;
+        }
+        setSubmitStatus('submitting');
+        await onSubmit(draft);
+        setSubmitStatus('success');
+      } catch (error) {
+        setErrorMessage(error.message);
+        setSubmitStatus('error');
+        await onSubmitError(error);
+      }
+    },
+    [onSubmit, onSubmitError, submitStatus],
+  );
+
+  const experienceCount = useExperienceCount();
+  const salaryCount = useSalaryWorkTimeCount();
+
+  return (
+    <Fragment>
+      <FormBuilder
+        open={open}
+        onClose={() => setSubmitStatus('quitting')}
+        questions={questions}
+        header={header}
+        footer={<Footer dataNum={salaryCount + experienceCount} />}
+        onSubmit={handleSubmit}
+      />
+      <ConfirmModal
+        isOpen={submitStatus === 'success'}
+        title="上傳成功"
+        subtitle="你已解鎖全站資訊囉！"
+        description="感謝你分享你的資訊，台灣的職場因為有你而變得更好！"
+        close={() => {
+          setSubmitStatus('unsubmitted');
+          onClose();
+          if (typeof window !== 'undefined') {
+            window.location.reload();
+          }
+        }}
+        closableOnClickOutside
+        actions={[
+          [
+            '確定',
+            () => {
+              setSubmitStatus('unsubmitted');
+              onClose();
+              if (typeof window !== 'undefined') {
+                window.location.reload();
+              }
+            },
+          ],
+        ]}
+      />
+      <ConfirmModal
+        isOpen={submitStatus === 'error'}
+        title="上傳失敗"
+        description={errorMessage}
+        close={() => {
+          setSubmitStatus('unsubmitted');
+        }}
+        closableOnClickOutside
+        actions={[
+          [
+            '確定',
+            () => {
+              setSubmitStatus('unsubmitted');
+            },
+          ],
+        ]}
+      />
+      <ConfirmModal
+        isOpen={submitStatus === 'quitting'}
+        title="確定要離開？"
+        description="離開之後資訊將會消失"
+        close={() => {
+          setSubmitStatus('unsubmitted');
+        }}
+        closableOnClickOutside
+        actions={[
+          [
+            '確定離開',
+            () => {
+              setSubmitStatus('unsubmitted');
+              onClose();
+            },
+          ],
+          [
+            '分享其他資訊',
+            () => {
+              setSubmitStatus('unsubmitted');
+              history.push('/share');
+            },
+          ],
+          [
+            '取消',
+            () => {
+              setSubmitStatus('unsubmitted');
+            },
+          ],
+        ]}
+      />
+    </Fragment>
+  );
+};
+
+SubmittableTypeForm.propTypes = {
+  open: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+};
+
+export default SubmittableTypeForm;
