03b9d0de2a1114f64dc9b7939e3ebb820754f8e8
Yen-chi Chen <mark86092@gmail.com>
2024-02-23 16:14:23 +0000

    update

--- src/components/TimeAndSalary/SearchScreen/urlBuilder.js
+++ src/components/TimeAndSalary/SearchScreen/urlBuilder.js
@@ -2,13 +2,20 @@ import qs from 'qs';
 import { generatePath } from 'react-router';
 
 // TODO: 將相似的 url builder 整合在一起
-export const companyPageOverview = (company, page, otherQuery) =>
-  `${generatePath('/companies/:company/overview', { company })}${qs.stringify(
+export const companyPageOverview = (company, page, otherQuery) => {
+  const path = generatePath('/companies/:company/overview', { company });
+  const search = qs.stringify(
     { ...otherQuery, p: 1 },
     { addQueryPrefix: true },
-  )}`;
+  );
+  return `${path}${search}`;
+};
 
-export const jobTitlePageOverview = (jobTitle, page, otherQuery) =>
-  `${generatePath('/job-titles/:jobTitle/overview', {
-    jobTitle,
-  })}${qs.stringify({ ...otherQuery, p: 1 }, { addQueryPrefix: true })}`;
+export const jobTitlePageOverview = (jobTitle, page, otherQuery) => {
+  const path = generatePath('/job-titles/:jobTitle/overview', { jobTitle });
+  const search = qs.stringify(
+    { ...otherQuery, p: 1 },
+    { addQueryPrefix: true },
+  );
+  return `${path}${search}`;
+};
