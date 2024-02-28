0b39071ded0be27f8d2c7840583877d18dfaf031
Yen-chi Chen <mark86092@gmail.com>
2024-02-28 20:24:16 +0800

    refactor: use overview page and company, job landing page (#1221)

--- src/components/CompanyAndJobTitle/Overview/Overview.js
+++ src/components/CompanyAndJobTitle/Overview/Overview.js
@@ -7,7 +7,10 @@ import SnippetBlock from './SnippetBlock';
 import WorkingHourTable from '../TimeAndSalary/WorkingHourTable';
 import WorkExperienceEntry from '../WorkExperiences/ExperienceEntry';
 import InterviewExperienceEntry from '../InterviewExperiences/ExperienceEntry';
-import { tabType as TAB_TYPE } from '../../../constants/companyJobTitle';
+import {
+  tabType as TAB_TYPE,
+  generateTabURL,
+} from '../../../constants/companyJobTitle';
 import SummaryBlock from './SummaryBlock';
 
 const SALARY_WORK_TIMES_LIMIT = 5;
@@ -32,7 +35,11 @@ const Overview = ({
       <SnippetBlock
         title="薪水&加班狀況"
         linkText={`查看 ${salaryWorkTimes.length} 筆完整的薪水、加班數據資料 >>`}
-        linkTo="salary-work-times"
+        linkTo={generateTabURL({
+          pageType,
+          pageName,
+          tabType: TAB_TYPE.TIME_AND_SALARY,
+        })}
         isEmpty={salaryWorkTimes.length === 0}
         pageType={pageType}
         pageName={pageName}
@@ -53,7 +60,11 @@ const Overview = ({
       <SnippetBlock
         title="工作心得"
         linkText={`查看 ${workExperiences.length} 篇完整的工作心得 >>`}
-        linkTo="work-experiences"
+        linkTo={generateTabURL({
+          pageType,
+          pageName,
+          tabType: TAB_TYPE.WORK_EXPERIENCE,
+        })}
         isEmpty={workExperiences.length === 0}
         pageType={pageType}
         pageName={pageName}
@@ -71,7 +82,11 @@ const Overview = ({
       <SnippetBlock
         title="面試心得"
         linkText={`查看 ${interviewExperiences.length} 篇完整的面試心得 >>`}
-        linkTo="interview-experiences"
+        linkTo={generateTabURL({
+          pageType,
+          pageName,
+          tabType: TAB_TYPE.INTERVIEW_EXPERIENCE,
+        })}
         isEmpty={interviewExperiences.length === 0}
         pageType={pageType}
         pageName={pageName}
