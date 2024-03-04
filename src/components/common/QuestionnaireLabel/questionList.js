b12b0cddea1fa0765e73ef7e084178b81a03b3ab
YongChenSu <yongchensu0@gmail.com>
2024-03-04 17:03:57 +0800

    fix: Update title text to match design specifications

--- src/components/common/QuestionnaireLabel/questionList.js
+++ src/components/common/QuestionnaireLabel/questionList.js
@@ -6,13 +6,13 @@ import styles from './ExpandedModal/NetPromoter.module.css';
 export const questionList = [
   {
     id: 1,
-    title: '你認為 GoodJob 網站對你找工作有幫助嗎？',
+    title: '1. 你認為 GoodJob 網站對你找工作有幫助嗎？',
     titleExplanation: null,
     section: <ScoreRange />,
   },
   {
     id: 2,
-    title: '你認為 GoodJob 有哪些地方可以做得更好嗎？',
+    title: '2. 你認為 GoodJob 有哪些地方可以做得更好嗎？',
     titleExplanation: '(e.g.新增ＯＯ功能，優化ＸＸ功能)',
     section: <TextArea className={styles.textArea} />,
   },
