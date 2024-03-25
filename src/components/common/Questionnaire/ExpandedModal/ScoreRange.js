55486fa3f0a2b93f76deb576143c0e6baff7dec6
YongChenSu <yongchensu0@gmail.com>
2024-03-25 15:40:31 +0800

    feat: change input range to button style

--- src/components/common/Questionnaire/ExpandedModal/ScoreRange.js
+++ src/components/common/Questionnaire/ExpandedModal/ScoreRange.js
@@ -1,40 +1,31 @@
 import React, { useState } from 'react';
 import styles from './NetPromoter.module.css';
 
-const inputRange = {
+const scoreRange = {
   min: 0,
   max: 10,
 };
 
 const ScoreRange = ({ handleUserFeedback }) => {
-  const [score, setScore] = useState(0);
-  const [rangePercentage, setRangePercentage] = useState(0);
-  const ticks = Array.from({ length: inputRange.max + 1 }, (_, i) => (
-    <div key={i}>{i}</div>
-  ));
-
-  const handleScoreChange = e => {
-    const newScore = Number(e.target.value);
-    const percentage = (newScore / (inputRange.max - inputRange.min)) * 100;
+  const [score, setScore] = useState(null);
+  const handleScoreClick = e => {
+    const newScore = Number(e.target.textContent);
     setScore(newScore);
-    setRangePercentage(percentage);
     handleUserFeedback(newScore);
   };
+  const scoreButtons = Array.from({ length: scoreRange.max + 1 }, (_, i) => (
+    <button
+      className={`${styles.button} ${score === i ? styles.activeButton : ''}`}
+      key={i}
+      onClick={handleScoreClick}
+    >
+      {i}
+    </button>
+  ));
 
   return (
     <div className={styles.range}>
-      <input
-        type="range"
-        min={inputRange.min}
-        max={inputRange.max}
-        value={score}
-        step={1}
-        onChange={handleScoreChange}
-        style={{
-          background: `linear-gradient(to right, #fcd406 ${rangePercentage}%, #C5C5C5 ${rangePercentage}%)`,
-        }}
-      />
-      <div className={styles.ticks}>{ticks}</div>
+      <div className={styles.buttons}>{scoreButtons}</div>
       <div className={styles.rangeLabels}>
         <span>完全沒幫助</span>
         <span>非常有幫助</span>
