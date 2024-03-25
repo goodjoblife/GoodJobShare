a11ba020e3c8f3e4993e9de95c21506b4422ce7f
YongChenSu <yongchensu0@gmail.com>
2024-03-25 17:20:55 +0800

    fix: use native implementation to hide CollapsedDrawer immediately on modal close

--- src/components/common/Questionnaire/CollapsedDrawer/index.js
+++ src/components/common/Questionnaire/CollapsedDrawer/index.js
@@ -1,26 +1,28 @@
 import React, { useState, Fragment, useCallback } from 'react';
 import styles from './CollapsedDrawer.module.css';
 import { LS_USER_FEEDBACK_SUBMISSION_TIME_KEY } from 'constants/localStorageKey';
-import { useLocalStorage } from 'react-use';
 
 const THIRTY_DAYS_IN_MILLISECONDS = 1000 * 60 * 60 * 24 * 30;
 
 const CollapsedDrawer = ({ title = '給我們回饋', children }) => {
   const [isExpanded, setIsExpanded] = useState(false);
-  const [lastSubmissionTime] = useLocalStorage(
-    LS_USER_FEEDBACK_SUBMISSION_TIME_KEY,
-  );
   const handleToggleModalOpen = useCallback(() => {
     setIsExpanded(prev => !prev);
   }, []);
 
-  if (lastSubmissionTime) {
-    const currentTime = new Date().getTime();
-    const isWithin30Days =
-      currentTime < lastSubmissionTime + THIRTY_DAYS_IN_MILLISECONDS;
+  if (typeof window !== 'undefined') {
+    const lastSubmissionTime = Number(
+      localStorage.getItem(LS_USER_FEEDBACK_SUBMISSION_TIME_KEY),
+    );
 
-    if (isWithin30Days) {
-      return null;
+    if (lastSubmissionTime) {
+      const currentTime = new Date().getTime();
+      const isWithin30Days =
+        currentTime < lastSubmissionTime + THIRTY_DAYS_IN_MILLISECONDS;
+
+      if (isWithin30Days) {
+        return null;
+      }
     }
   }
 
