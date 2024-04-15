9c6348ceb72f467a724febe7447701060e62cf09
YongChenSu <yongchensu0@gmail.com>
2024-04-15 14:09:22 +0800

    style: control button styles with :not(:disabled) selector

--- src/components/common/Questionnaire/ExpandedModal/NextButton.js
+++ src/components/common/Questionnaire/ExpandedModal/NextButton.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import styles from './ExpandedModal.module.css';
 import laborImage from './laborImage.png';
-import cn from 'classnames';
 
 const NextButton = ({ handleNext, buttonText, isEnabled }) => (
   <div className={styles.nextStepButtonContainer}>
     <img src={laborImage} alt="laborImage" />
     <button
       onClick={handleNext}
-      className={cn(styles.nextStepButton, { [styles.enabled]: isEnabled })}
+      className={styles.nextStepButton}
       disabled={!isEnabled}
     >
       {buttonText}
