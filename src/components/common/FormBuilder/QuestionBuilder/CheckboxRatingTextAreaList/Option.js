8fe96befa8dfea26d4495cd8c645bdb56024b006
Peteranny <peteranny2@gmail.com>
2024-04-26 01:34:48 +0800

    force line style

--- src/components/common/FormBuilder/QuestionBuilder/CheckboxRatingTextAreaList/Option.js
+++ src/components/common/FormBuilder/QuestionBuilder/CheckboxRatingTextAreaList/Option.js
@@ -3,14 +3,13 @@ import PropTypes from 'prop-types';
 import cn from 'classnames';
 import Button from 'common/button/Button';
 import Checked2 from 'common/icons/Checked2';
-import linkStyles from 'common/base/Link.module.css';
 import { ValuePropType } from '../Checkbox/PropTypes';
 import styles from './styles.module.css';
 
 const Option = ({ children, onClick, selected, isElse }) => {
   return (
     <Button
-      className={cn(styles.button, { [linkStyles.link]: isElse })}
+      className={cn(styles.button, { [styles.link]: isElse })}
       btnStyle={selected ? 'yellow' : 'lightGray'}
       circleSize="lg"
       onClick={onClick}
