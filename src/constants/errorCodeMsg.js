3a53f481bba1e87a6d5429688c31e91267643887
Ming <barry800414@users.noreply.github.com>
2024-05-12 00:10:05 +0800

    feat: remove fb login announcement & enhance toast of FB login issue (#1270)

--- src/constants/errorCodeMsg.js
+++ src/constants/errorCodeMsg.js
@@ -1,6 +1,9 @@
 const LOGIN_ERROR_MSG =
   '登入時發生錯誤，請再重試一次。若錯誤持續發生，請聯繫 findyourgoodjob@gmail.com';
 
+const LOGIN_ERROR_MSG_CLEAN_BROWSER_DATA =
+  '登入時發生錯誤，請清除瀏覽器資料，再重試一次。若錯誤持續發生，請聯繫 findyourgoodjob@gmail.com';
+
 // maintain a list of global error code
 export const ERROR_CODE_MSG = {
   ER0001: {
@@ -24,7 +27,7 @@ export const ERROR_CODE_MSG = {
     internal: 'Graphql mutation facebookLogin failed',
   },
   ER0006: {
-    external: LOGIN_ERROR_MSG,
+    external: LOGIN_ERROR_MSG_CLEAN_BROWSER_DATA,
     internal: 'FB login failed: unknown auth status',
   },
 };
