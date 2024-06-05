40ba0bb530082e83d5a4b4dda90cc0c6c3941e8f
Peteranny <peteranny2@gmail.com>
2024-06-05 23:48:42 +0800

    add busi no. to form company search

--- src/graphql/timeAndSalary.js
+++ src/graphql/timeAndSalary.js
@@ -29,6 +29,7 @@ export const getSearchCompanyQuery = `
   query($companyName:String!) {
     search_companies(query:$companyName) {
       name
+      businessNumber
       salary_work_time_statistics {
         count
       }
