0dfd886c67413336be473e3db737a9cdef2e62da
Yen-chi Chen <mark86092@gmail.com>
2024-06-10 13:05:28 +0000

    chore: remove unused api/action
    follow #1308, found some api doesn't used

--- src/apis/timeAndSalaryApi.js
+++ src/apis/timeAndSalaryApi.js
@@ -1,10 +1,7 @@
 import fetchUtil from 'utils/fetchUtil';
 import graphqlClient from 'utils/graphqlClient';
 import {
-  getSalaryWorkTimes,
   getSearchCompanyQuery,
-  getCompanyQuery,
-  getJobTitleQuery,
   getSearchJobTitleQuery,
   changeSalaryWorkTimeStatus,
 } from 'graphql/timeAndSalary';
@@ -25,38 +22,17 @@ export const fetchJobTitleCandidates = ({ key }) =>
     })
     .then(items => items.map(item => item._id));
 
-export const fetchTimeAndSalary = ({ start, limit }) =>
-  graphqlClient({
-    query: getSalaryWorkTimes,
-    variables: { start, limit },
-  });
-
-export const fetchTimeAndSalaryExtreme = ({ opt }) =>
-  fetchUtil(`${endpoint}/extreme`).get({ query: opt });
-
 export const fetchSearchCompany = ({ companyName }) =>
   graphqlClient({
     query: getSearchCompanyQuery,
     variables: { companyName },
   }).then(data => data.search_companies);
 
-export const fetchCompany = ({ companyName }) =>
-  graphqlClient({
-    query: getCompanyQuery,
-    variables: { companyName },
-  }).then(data => data.company);
-
 export const fetchSearchJobTitle = ({ jobTitle }) =>
   graphqlClient({
     query: getSearchJobTitleQuery,
     variables: { jobTitle },
   }).then(data => data.search_job_titles);
-
-export const fetchJobTitle = ({ jobTitle }) =>
-  graphqlClient({
-    query: getJobTitleQuery,
-    variables: { jobTitle },
-  }).then(data => data.job_title);
 
 export const postWorkings = ({ body, token }) =>
   fetchUtil(endpoint).post({ body, token });
--- src/graphql/timeAndSalary.js
+++ src/graphql/timeAndSalary.js
@@ -1,30 +1,3 @@
-export const getSalaryWorkTimes = /* GraphQL */ `
-  query($start: Int!, $limit: Int!) {
-    salaryWorkTimeCount
-    salary_work_times(start: $start, limit: $limit) {
-      company {
-        name
-      }
-      job_title {
-        name
-      }
-      overtime_frequency
-      salary {
-        type
-        amount
-      }
-      sector
-      week_work_time
-      data_time {
-        year
-        month
-      }
-      estimated_hourly_wage
-      about_this_job
-    }
-  }
-`;
-
 export const getSearchCompanyQuery = `
   query($companyName:String!) {
     search_companies(query:$companyName) {
@@ -36,112 +9,12 @@ export const getSearchCompanyQuery = `
   }
 `;
 
-export const getCompanyQuery = `
-  query($companyName:String!) {
-    company(name:$companyName) {
-      name
-      salary_work_times {
-        id
-        job_title {
-          name
-        }
-        sector
-        employment_type
-        day_promised_work_time
-        day_real_work_time
-        week_work_time
-        overtime_frequency
-        experience_in_year
-        salary {
-          type
-          amount
-        }
-        estimated_hourly_wage
-        data_time {
-          year
-          month
-        }
-      }
-      salary_work_time_statistics {
-        count
-        average_week_work_time
-        average_estimated_hourly_wage
-        has_compensatory_dayoff_count {
-          yes
-          no
-          unknown
-        }
-        has_overtime_salary_count {
-          yes
-          no
-          unknown
-        }
-        is_overtime_salary_legal_count {
-          yes
-          no
-          unknown
-        }
-      }
-    }
-  }
-`;
-
 export const getSearchJobTitleQuery = `
   query($jobTitle:String!) {
     search_job_titles(query:$jobTitle) {
       name
       salary_work_time_statistics {
         count
-      }
-    }
-  }
-`;
-
-export const getJobTitleQuery = `
-  query($jobTitle:String!) {
-    job_title(name:$jobTitle) {
-      name
-      salary_work_times {
-        id
-        company {
-          name
-        }
-        sector
-        employment_type
-        day_promised_work_time
-        day_real_work_time
-        week_work_time
-        overtime_frequency
-        experience_in_year
-        salary {
-          type
-          amount
-        }
-        estimated_hourly_wage
-        data_time {
-          year
-          month
-        }
-      }
-      salary_work_time_statistics {
-        count
-        average_week_work_time
-        average_estimated_hourly_wage
-        has_compensatory_dayoff_count {
-          yes
-          no
-          unknown
-        }
-        has_overtime_salary_count {
-          yes
-          no
-          unknown
-        }
-        is_overtime_salary_legal_count {
-          yes
-          no
-          unknown
-        }
       }
     }
   }
