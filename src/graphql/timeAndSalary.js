ff578bbfcb98c1e97bec99ebebd88de8cf7e46f4
Peter Shih <peteranny2@gmail.com>
2024-06-19 11:07:52 +0800

    [職稱搜尋] 補 dataCount (#1335)

--- src/graphql/timeAndSalary.js
+++ src/graphql/timeAndSalary.js
@@ -12,9 +12,7 @@ export const getSearchJobTitleQuery = `
   query($jobTitle:String!) {
     search_job_titles(query:$jobTitle) {
       name
-      salary_work_time_statistics {
-        count
-      }
+      dataCount
     }
   }
 `;
