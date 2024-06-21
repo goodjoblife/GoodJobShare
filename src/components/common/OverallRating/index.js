d7628c9a3ca4337f1bd781d47ab10f65ec9abdf4
YongChenSu <yongchensu0@gmail.com>
2024-06-22 00:22:19 +0800

    refactor: rename file from Thumb to Thumbs

--- src/components/common/OverallRating/index.js
+++ src/components/common/OverallRating/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import cn from 'classnames';
-import Thumbs from './Thumb';
+import Thumbs from './Thumbs';
 import { Rating, RatingLabel } from './Rating';
 import styles from './Rating.module.css';
 
