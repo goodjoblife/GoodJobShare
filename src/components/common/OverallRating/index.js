6a97125af3cd4a4dc21d385caaf41f3a810808d3
YongChenSu <30387546+YongChenSu@users.noreply.github.com>
2024-07-04 15:35:28 +0800

    feat: 工作心得列表卡片修改 & 工作心得單篇頁面改版 (#1354)

---
+++ src/components/common/OverallRating/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import cn from 'classnames';
+import Thumbs from './Thumbs';
+import { Rating, RatingLabel } from './Rating';
+import styles from './Rating.module.css';
+
+const OverallRating = ({ rating, hasRatingLabel }) => {
+  return (
+    <div className={cn(styles.overallRating)}>
+      <Rating rating={rating} textYellow={hasRatingLabel} />
+      <div className={styles.ratingInfo}>
+        <Thumbs rating={rating} />
+        {hasRatingLabel && <RatingLabel rating={rating} />}
+      </div>
+    </div>
+  );
+};
+
+OverallRating.propTypes = {
+  hasRatingLabel: PropTypes.bool,
+  rating: PropTypes.number.isRequired,
+};
+
+OverallRating.defaultProps = {
+  hasRatingLabel: false,
+};
+
+export default OverallRating;
