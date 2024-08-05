aa48e2164b9de7dacc4637dbfb1a4114a850113e
YongChenSu <yongchensu0@gmail.com>
2024-08-05 21:29:47 +0800

    fix: change rating to toFixed(1) inside the div

--- src/components/common/OverallRating/index.js
+++ src/components/common/OverallRating/index.js
@@ -5,19 +5,15 @@ import Thumbs from './Thumbs';
 import { Rating, RatingLabel } from './Rating';
 import styles from './Rating.module.css';
 
-const OverallRating = ({ rating: rawRating, hasRatingLabel }) => {
-  const rating = Number(rawRating.toFixed(1));
-
-  return (
-    <div className={cn(styles.overallRating)}>
-      <Rating rating={rating} textYellow={hasRatingLabel} />
-      <div className={styles.ratingInfo}>
-        <Thumbs rating={rating} />
-        {hasRatingLabel && <RatingLabel rating={rating} />}
-      </div>
+const OverallRating = ({ rating, hasRatingLabel }) => (
+  <div className={cn(styles.overallRating)}>
+    <Rating rating={rating} textYellow={hasRatingLabel} />
+    <div className={styles.ratingInfo}>
+      <Thumbs rating={rating} />
+      {hasRatingLabel && <RatingLabel rating={rating} />}
     </div>
-  );
-};
+  </div>
+);
 
 OverallRating.propTypes = {
   hasRatingLabel: PropTypes.bool,
