4aac0b4232ac3c29cf20e26a6095f7f68af530cb
YongChenSu <yongchensu0@gmail.com>
2024-08-26 23:09:30 +0800

    chore: remove console and format to original code

--- src/components/CompanyAndJobTitle/WorkExperiences/ExperienceEntry.js
+++ src/components/CompanyAndJobTitle/WorkExperiences/ExperienceEntry.js
@@ -36,86 +36,83 @@ const ExperienceEntry = ({
   },
   size,
   canView,
-}) => {
-  // console.log({ data });
-  return (
-    <div className={cn(styles.container, styles[size])}>
-      <Link to={createLinkTo({ id, pageType })}>
-        <section className={styles.contentWrapper}>
-          <div className={styles.labels}>
-            <P size="s" className={styles.date}>
-              評價 · {formatCreatedAt(createdAt)}
-            </P>
-            <div className={styles.salaryRecommendWrapper}>
-              <div className={styles.rowWrapper}>
-                {weekWorkTime && canView && (
-                  <div className={styles.weekWorkTime}>
-                    <Clock />
-                    {formatWeekWorkTime(weekWorkTime)}
-                  </div>
-                )}
-                {salary && (
-                  <div
-                    className={cn(styles.salary, {
-                      [styles.locked]: !canView,
-                    })}
-                  >
-                    {canView ? (
-                      <React.Fragment>
-                        <Coin />
-                        {formatSalary(salary)}
-                      </React.Fragment>
-                    ) : (
-                      <React.Fragment>
-                        <FontAwesomeIcon icon={faLock} />
-                        {formatSalaryRange(salary)}
-                      </React.Fragment>
-                    )}
-                  </div>
-                )}
+}) => (
+  <div className={cn(styles.container, styles[size])}>
+    <Link to={createLinkTo({ id, pageType })}>
+      <section className={styles.contentWrapper}>
+        <div className={styles.labels}>
+          <P size="s" className={styles.date}>
+            評價 · {formatCreatedAt(createdAt)}
+          </P>
+          <div className={styles.salaryRecommendWrapper}>
+            <div className={styles.rowWrapper}>
+              {weekWorkTime && canView && (
+                <div className={styles.weekWorkTime}>
+                  <Clock />
+                  {formatWeekWorkTime(weekWorkTime)}
+                </div>
+              )}
+              {salary && (
+                <div
+                  className={cn(styles.salary, {
+                    [styles.locked]: !canView,
+                  })}
+                >
+                  {canView ? (
+                    <React.Fragment>
+                      <Coin />
+                      {formatSalary(salary)}
+                    </React.Fragment>
+                  ) : (
+                    <React.Fragment>
+                      <FontAwesomeIcon icon={faLock} />
+                      {formatSalaryRange(salary)}
+                    </React.Fragment>
+                  )}
+                </div>
+              )}
 
-                {averageSectionRating ? (
-                  <div className={styles.overallRatingWrapper}>
-                    <OverallRating
-                      rating={averageSectionRating}
-                      hasRatingText
-                    />
-                  </div>
-                ) : (
-                  <div className={styles.recommendToOthers}>
-                    {recommendToOthers === 'yes' ? <Good /> : <Bad />}
-                    {recommendToOthers === 'yes' ? '推' : '不推'}
-                  </div>
-                )}
-              </div>
+              {averageSectionRating ? (
+                <div className={styles.overallRatingWrapper}>
+                  <OverallRating
+                    rating={averageSectionRating}
+                    hasRatingNumber
+                  />
+                </div>
+              ) : (
+                <div className={styles.recommendToOthers}>
+                  {recommendToOthers === 'yes' ? <Good /> : <Bad />}
+                  {recommendToOthers === 'yes' ? '推' : '不推'}
+                </div>
+              )}
             </div>
           </div>
+        </div>
 
-          <Heading
-            Tag="h2"
-            size={size === 'l' ? 'sl' : 'sm'}
-            className={styles.heading}
+        <Heading
+          Tag="h2"
+          size={size === 'l' ? 'sl' : 'sm'}
+          className={styles.heading}
+        >
+          {originalCompanyName} {jobTitle}
+        </Heading>
+
+        <div className={styles.snippetWrapper}>
+          <span className={styles.snippet}>
+            {section.content.slice(0, SNIPPET_SIZE)}....
+          </span>
+          <span
+            className={cn(styles.readmore, {
+              [styles.locked]: !canView,
+            })}
           >
-            {originalCompanyName} {jobTitle}
-          </Heading>
-
-          <div className={styles.snippetWrapper}>
-            <span className={styles.snippet}>
-              {section.content.slice(0, SNIPPET_SIZE)}....
-            </span>
-            <span
-              className={cn(styles.readmore, {
-                [styles.locked]: !canView,
-              })}
-            >
-              {`閱讀更多${canView ? '' : '並解鎖'}`}
-            </span>
-          </div>
-        </section>
-      </Link>
-    </div>
-  );
-};
+            {`閱讀更多${canView ? '' : '並解鎖'}`}
+          </span>
+        </div>
+      </section>
+    </Link>
+  </div>
+);
 
 ExperienceEntry.propTypes = {
   canView: PropTypes.bool.isRequired,
