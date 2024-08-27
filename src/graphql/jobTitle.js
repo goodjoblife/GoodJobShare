7937063f3d78852def0822d87f72e4c09da9f0e9
Yen-chi Chen <mark86092@gmail.com>
2024-08-27 19:57:50 +0800

    [公司職稱頁面] Overview use pagination api (#1419)

--- src/graphql/jobTitle.js
+++ src/graphql/jobTitle.js
@@ -6,6 +6,7 @@ export const queryJobTitles = /* GraphQL */ `
   }
 `;
 
+// TODO: DEPRECATED
 export const getJobTitleQuery = /* GraphQL */ `
   query($jobTitle: String!) {
     job_title(name: $jobTitle) {
@@ -88,6 +89,144 @@ export const getJobTitleQuery = /* GraphQL */ `
         data_time {
           month
           year
+        }
+      }
+      salary_work_time_statistics {
+        count
+        average_estimated_hourly_wage
+        average_week_work_time
+        overtime_frequency_count {
+          seldom
+          sometimes
+          usually
+          almost_everyday
+        }
+        is_overtime_salary_legal_count {
+          yes
+          no
+          unknown
+        }
+        has_compensatory_dayoff_count {
+          yes
+          no
+          unknown
+        }
+        has_overtime_salary_count {
+          yes
+          no
+          unknown
+        }
+      }
+      salary_distribution {
+        bins {
+          data_count
+          range {
+            type
+            from
+            to
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const queryJobTitleOverviewGql = /* GraphQL */ `
+  query(
+    $jobTitle: String!
+    $interviewExperiencesLimit: Int!
+    $workExperiencesLimit: Int!
+    $salaryWorkTimesLimit: Int!
+  ) {
+    job_title(name: $jobTitle) {
+      name
+      interviewExperiencesResult(start: 0, limit: $interviewExperiencesLimit) {
+        count
+        interviewExperiences {
+          id
+          type
+          originalCompanyName
+          company {
+            name
+          }
+          job_title {
+            name
+          }
+          region
+          experience_in_year
+          education
+          salary {
+            amount
+            type
+          }
+          title
+          sections {
+            subtitle
+            content
+          }
+          created_at
+          reply_count
+          like_count
+          overall_rating
+        }
+      }
+      workExperiencesResult(start: 0, limit: $workExperiencesLimit) {
+        count
+        workExperiences {
+          id
+          type
+          originalCompanyName
+          company {
+            name
+          }
+          job_title {
+            name
+          }
+          region
+          experience_in_year
+          education
+          salary {
+            amount
+            type
+          }
+          title
+          sections {
+            subtitle
+            content
+          }
+          created_at
+          reply_count
+          like_count
+          recommend_to_others
+        }
+      }
+      salaryWorkTimesResult(start: 0, limit: $salaryWorkTimesLimit) {
+        count
+        salaryWorkTimes {
+          id
+          week_work_time
+          salary {
+            type
+            amount
+          }
+          sector
+          day_real_work_time
+          day_promised_work_time
+          experience_in_year
+          estimated_hourly_wage
+          overtime_frequency
+          employment_type
+          job_title {
+            name
+          }
+          company {
+            name
+          }
+          originalCompanyName
+          data_time {
+            month
+            year
+          }
         }
       }
       salary_work_time_statistics {
