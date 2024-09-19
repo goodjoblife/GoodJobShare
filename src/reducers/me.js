42e8ff8ac44eed9fd82b2199140a683376e73134
Peteranny <peteranny2@gmail.com>
2024-09-20 01:11:13 +0800

    reset on login

--- src/reducers/me.js
+++ src/reducers/me.js
@@ -1,6 +1,7 @@
 import createReducer from 'utils/createReducer';
 import { SET_MY_PUBLISH_IDS } from 'actions/me';
 import { getUnfetched } from 'utils/fetchBox';
+import { SET_LOGIN } from 'actions/auth';
 
 const preloadedState = {
   myPublishIds: getUnfetched(),
@@ -10,6 +11,10 @@ const me = createReducer(preloadedState, {
   [SET_MY_PUBLISH_IDS]: (state, { box }) => ({
     ...state,
     myPublishIds: box,
+  }),
+  [SET_LOGIN]: state => ({
+    ...state,
+    myPublishIds: getUnfetched(),
   }),
 });
 
