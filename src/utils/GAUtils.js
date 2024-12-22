e050556b557f57ee4f384b145679750aa790237c
barry800414 <barry800414@gmail.com>
2024-12-22 23:26:15 +0800

    fix: form submit issue on Brave browser

--- src/utils/GAUtils.js
+++ src/utils/GAUtils.js
@@ -11,9 +11,16 @@ export const getUserPseudoId = ga_measurement_id => {
   return new Promise((resolve, reject) => {
     try {
       if (typeof window !== 'undefined' && typeof window.gtag !== 'undefined') {
-        window.gtag('get', ga_measurement_id, 'client_id', field =>
-          resolve(field),
-        );
+        window.gtag('get', ga_measurement_id, 'client_id', resolve);
+        /* 
+          In Brave browser, window.gtag execution will be terminated without 
+          throwing error, so this promise will not never being resolved or rejected.
+          To avoid this, we use setTimeout to reject the promise after 1s
+        */
+        setTimeout(() => {
+          console.error('Unable to resolve GA User Pseudo Id');
+          resolve(null);
+        }, 500);
       } else {
         resolve(null);
       }
