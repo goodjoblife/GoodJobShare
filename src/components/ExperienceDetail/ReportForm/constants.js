112d475767a791cb92f3d89931eaf0a1d516e7e7
YongChenSu <yongchensu0@gmail.com>
2025-01-04 23:39:06 +0800

    feat: add report form constants

---
+++ src/components/ExperienceDetail/ReportForm/constants.js
@@ -0,0 +1,47 @@
+export const salaryReportReasons = [
+  {
+    label: '我認為公司名稱或職稱有誤',
+    value: '我認為公司名稱或職稱有誤',
+  },
+  {
+    label: '我認為薪資數額並不合理、正確',
+    value: '我認為薪資數額並不合理、正確',
+  },
+  {
+    label: '我認為工時、加班狀況並不正確',
+    value: '我認為工時、加班狀況並不正確',
+  },
+  {
+    label: '其他',
+    value: '其他',
+  },
+];
+
+export const experienceReportReasons = [
+  {
+    label: '這是廣告或垃圾訊息，或沒有參考價值',
+    value: '這是廣告或垃圾訊息，或沒有參考價值',
+  },
+  {
+    label: '這篇文章涉及人身攻擊、誹謗',
+    value: '這篇文章涉及人身攻擊、誹謗',
+  },
+  {
+    label: '這篇文章內容不實',
+    value: '這篇文章內容不實',
+  },
+  {
+    label: '這篇文章是使用 AI / ChatGPT 產生出來的，沒有參考價值',
+    value: '這篇文章是使用 AI / ChatGPT 產生出來的，沒有參考價值',
+  },
+  {
+    label: '其他',
+    value: '其他',
+  },
+];
+
+export const MODAL_TYPE = {
+  REPORT_DETAIL: 'REPORT_TYPE',
+  REPORT_API_ERROR: 'REPORT_API_ERROR',
+  REPORT_SUCCESS: 'REPORT_SUCCESS',
+};
