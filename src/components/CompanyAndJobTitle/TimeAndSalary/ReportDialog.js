99e6b369fe2bf0a62b70377ccd928199f4f27ef1
YongChenSu <yongchensu0@gmail.com>
2025-01-13 00:01:09 +0800

    refactor: update ReportDialog

--- src/components/CompanyAndJobTitle/TimeAndSalary/ReportDialog.js
+++ src/components/CompanyAndJobTitle/TimeAndSalary/ReportDialog.js
@@ -3,30 +3,43 @@ import PropTypes from 'prop-types';
 import styles from './ReportDialog.module.css';
 import cn from 'classnames';
 
-const ReportDialog = ({ reportCount, isHighlighted }) => {
-  const isShowReportCount = reportCount > 0;
+const ReportDialog = ({
+  reportCount = 0,
+  isHighlighted = false,
+  isShowReportText = false,
+  reportText = '回報',
+}) => {
+  const hasReport = reportCount > 0;
   return (
-    <div
-      className={cn(styles.dialogWrapper, {
-        [styles.highlighted]: isHighlighted,
-        [styles.enabled]: isShowReportCount,
-      })}
-    >
-      <div className={styles.dialogBox}>
-        {isShowReportCount ? reportCount : '!'}
+    <div className={styles.reportDialogContainer}>
+      <div
+        className={cn(styles.dialogWrapper, {
+          [styles.highlighted]: isHighlighted,
+          [styles.enabled]: hasReport,
+        })}
+      >
+        <div className={styles.dialogBox}>{hasReport ? reportCount : '!'}</div>
+        <div className={styles.dialogTriangle}></div>
       </div>
-      <div className={styles.dialogTriangle}></div>
+      {isShowReportText && (
+        <div className={styles.reportText}>{reportText}</div>
+      )}
     </div>
   );
 };
 
 ReportDialog.propTypes = {
   isHighlighted: PropTypes.bool,
+  isShowReportText: PropTypes.bool,
   reportCount: PropTypes.number,
+  reportText: PropTypes.string,
 };
+
 ReportDialog.defaultProps = {
   isHighlighted: false,
+  isShowReportText: false,
   reportCount: 0,
+  reportText: '回報',
 };
 
 export default ReportDialog;
