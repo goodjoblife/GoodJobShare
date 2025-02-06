6879547abbfef6bbf36bab2d3b68b48cdd31cbdc
YongChenSu <yongchensu0@gmail.com>
2025-02-06 23:25:55 +0800

    feat: add graphQL and API

--- src/apis/reports.js
+++ src/apis/reports.js
@@ -4,6 +4,8 @@ import graphqlClient from 'utils/graphqlClient';
 import {
   createExperienceReportGql,
   createSalaryWorkTimeReportGql,
+  getExperienceReportsGql,
+  getSalaryWorkTimeReportsGql,
 } from 'graphql/reports';
 
 export const getReports = ({ id }) =>
@@ -46,3 +48,31 @@ export const createSalaryWorkTimeReportApi = async ({
     },
     token,
   });
+
+// TODO: 以下兩個 API 等下一個 PR 再使用並修正
+export const getExperienceReportsApi = async ({ id, token }) => {
+  const data = await graphqlClient({
+    query: getExperienceReportsGql,
+    variables: {
+      id,
+    },
+    token,
+  });
+  return data.experience.reports;
+};
+
+export const getSalaryWorkTimeReportsApi = async ({
+  token,
+  companyName,
+  salaryWorkTimesLimit = 10,
+}) => {
+  const data = await graphqlClient({
+    query: getSalaryWorkTimeReportsGql,
+    variables: {
+      companyName,
+      salaryWorkTimesLimit,
+    },
+    token,
+  });
+  return data.salaryWorkTimeReports;
+};
