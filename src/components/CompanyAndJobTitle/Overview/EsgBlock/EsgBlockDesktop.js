60cf2aa77f9139b84f1940bf1e5e7835d9787b92
Peteranny <peteranny2@gmail.com>
2025-02-07 02:21:48 +0800

    fix desktop sensor

--- src/components/CompanyAndJobTitle/Overview/EsgBlock/EsgBlockDesktop.js
+++ src/components/CompanyAndJobTitle/Overview/EsgBlock/EsgBlockDesktop.js
@@ -1,14 +1,12 @@
-import React, { useEffect, useMemo } from 'react';
+import React, { useEffect } from 'react';
+import { useMedia } from 'react-use';
 import PropTypes from 'prop-types';
 import EsgBlock from './EsgBlock';
 import usePreviewed from './usePreviewed';
 
-const useIsDesktop = () =>
-  useMemo(() => window.matchMedia('(min-width: 850px)').matches, []);
-
 const EsgBlockDesktop = ({ className }) => {
   const [hasPreviewed, setPreviewed] = usePreviewed();
-  const isDesktop = useIsDesktop();
+  const isDesktop = useMedia('(min-width: 850px)');
 
   useEffect(() => {
     if (isDesktop && !hasPreviewed) setPreviewed(true);
