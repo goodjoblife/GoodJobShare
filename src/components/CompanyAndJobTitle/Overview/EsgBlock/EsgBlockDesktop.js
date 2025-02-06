a84d294f1912ff90e5635d0fc99daabe6d968085
Peteranny <peteranny2@gmail.com>
2025-02-07 02:28:46 +0800

    move media sensor to index

--- src/components/CompanyAndJobTitle/Overview/EsgBlock/EsgBlockDesktop.js
+++ src/components/CompanyAndJobTitle/Overview/EsgBlock/EsgBlockDesktop.js
@@ -1,16 +1,14 @@
 import React, { useEffect } from 'react';
-import { useMedia } from 'react-use';
 import PropTypes from 'prop-types';
 import EsgBlock from './EsgBlock';
 import usePreviewed from './usePreviewed';
 
 const EsgBlockDesktop = ({ className }) => {
   const [hasPreviewed, setPreviewed] = usePreviewed();
-  const isDesktop = useMedia('(min-width: 850px)');
 
   useEffect(() => {
-    if (isDesktop && !hasPreviewed) setPreviewed(true);
-  }, [isDesktop, hasPreviewed, setPreviewed]);
+    if (!hasPreviewed) setPreviewed(true);
+  }, [hasPreviewed, setPreviewed]);
 
   return <EsgBlock className={className} hasPreviewed={hasPreviewed} />;
 };
--- src/components/CompanyAndJobTitle/Overview/EsgBlock/index.js
+++ src/components/CompanyAndJobTitle/Overview/EsgBlock/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import cn from 'classnames';
+import { useMedia } from 'react-use';
 
 import EsgBlockDesktop from './EsgBlockDesktop';
 import EsgBlockMobile from './EsgBlockMobile';
 import styles from './EsgBlock.module.css';
 
-const EsgBlockRoot = () => (
-  <React.Fragment>
-    <EsgBlockDesktop className={styles.root} />
-    <EsgBlockMobile className={cn(styles.root, styles.mobile)} />
-  </React.Fragment>
-);
+const EsgBlockRoot = () => {
+  const isMobile = useMedia('(max-width: 849px)');
+
+  if (isMobile) {
+    return <EsgBlockMobile className={cn(styles.root, styles.mobile)} />;
+  } else {
+    return <EsgBlockDesktop className={styles.root} />;
+  }
+};
 
 export default EsgBlockRoot;
