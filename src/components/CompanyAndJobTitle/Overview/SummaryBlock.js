c0a10f147ef61821194510f496a40e86c1fea4ec
Peteranny <peteranny2@gmail.com>
2025-02-27 01:27:19 +0800

    responsive card

--- src/components/CompanyAndJobTitle/Overview/SummaryBlock.js
+++ src/components/CompanyAndJobTitle/Overview/SummaryBlock.js
@@ -5,6 +5,7 @@ import loadable from '@loadable/component';
 import Card from 'common/Card';
 import AverageWeekWorkTimeView from './AverageWeekWorkTimeView';
 import styles from './SummaryBlock.module.css';
+import useMobile from 'hooks/useMobile';
 
 const SalaryDistributionChart = loadable(() =>
   import('common/Charts/SalaryDistributionChart'),
@@ -13,6 +14,16 @@ const JobTitleDistributionChart = loadable(() =>
   import('common/Charts/JobTitleDistributionChart'),
 );
 
+const ChartCard = ({ children }) => {
+  const isMobile = useMobile();
+  const barCardStyle = isMobile ? styles.barChartSm : styles.barChart;
+  return <Card className={barCardStyle}>{children}</Card>;
+};
+
+ChartCard.propTypes = {
+  children: PropTypes.node,
+};
+
 const SummaryBlock = ({
   salaryDistribution,
   jobAverageSalaries,
@@ -20,26 +31,14 @@ const SummaryBlock = ({
   overtimeFrequencyCount,
 }) => (
   <div className={styles.summaryBlock}>
-    {salaryDistribution && (
-      <React.Fragment>
-        <Card className={styles.barChart}>
-          <SalaryDistributionChart data={salaryDistribution} />
-        </Card>
-        <Card className={styles.barChartSm}>
-          <SalaryDistributionChart data={salaryDistribution} />
-        </Card>
-      </React.Fragment>
-    )}
-    {jobAverageSalaries && (
-      <React.Fragment>
-        <Card className={styles.barChart}>
-          <JobTitleDistributionChart data={jobAverageSalaries} />
-        </Card>
-        <Card className={styles.barChartSm}>
-          <JobTitleDistributionChart data={jobAverageSalaries} />
-        </Card>
-      </React.Fragment>
-    )}
+    <ChartCard>
+      {salaryDistribution && (
+        <SalaryDistributionChart data={salaryDistribution} />
+      )}
+      {jobAverageSalaries && (
+        <JobTitleDistributionChart data={jobAverageSalaries} />
+      )}
+    </ChartCard>
     <Card className={styles.averageWeekWorkTime}>
       <AverageWeekWorkTimeView
         averageWeekWorkTime={averageWeekWorkTime}
