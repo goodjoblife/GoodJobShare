ac14f5d0966422cd915c5a1ac9779f3a1d4817df
Peteranny <peteranny2@gmail.com>
2025-03-03 18:25:01 +0800

    toggle message

--- src/components/CompanyAndJobTitle/Experience.js
+++ src/components/CompanyAndJobTitle/Experience.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import usePermission from 'hooks/usePermission';
 import Article from 'components/ExperienceDetail/Article';
+import { Wrapper } from 'common/base';
+import MessageBoard from '../ExperienceDetail/MessageBoard';
 
 const Experience = ({ experience }) => {
   const [, , canViewPublishId] = usePermission();
+  const [messageExpanded, setMessageExpanded] = useState(false);
 
   return (
-    <Article
-      experience={experience}
-      hideContent={!canViewPublishId(experience.id)}
-    />
+    <>
+      <Article
+        experience={experience}
+        hideContent={!canViewPublishId(experience.id)}
+        onClickMsgButton={() => setMessageExpanded(expended => !expended)}
+      />
+      {messageExpanded && (
+        <Wrapper size="s">
+          <MessageBoard experienceId={experience.id} />
+        </Wrapper>
+      )}
+    </>
   );
 };
 
