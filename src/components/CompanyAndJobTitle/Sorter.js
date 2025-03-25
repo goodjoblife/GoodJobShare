729bac4f1ab5a823eb9c6de9a3b29861fa1b7c0e
Peteranny <peteranny2@gmail.com>
2025-03-25 21:50:24 +0800

    new sorter

--- src/components/CompanyAndJobTitle/Sorter.js
+++ src/components/CompanyAndJobTitle/Sorter.js
@@ -1,5 +1,8 @@
 import React, { useCallback, useState } from 'react';
-import Select from 'common/form/Select';
+import PropTypes from 'prop-types';
+import styles from './Sorter.module.css';
+import Caret from 'common/icons/Caret';
+import Sort from 'common/icons/Sort';
 
 const options = [
   { label: '近期精選貼文', value: '近期精選貼文' },
@@ -7,19 +10,36 @@ const options = [
   { label: '按照時間排序(舊->新)', value: '按照時間排序(舊->新)' },
 ];
 
-const Sorter = () => {
-  const [value, setValue] = useState('近期精選貼文');
-  const handleChange = useCallback(e => {
-    setValue(e.target.value);
-  }, []);
-  return (
-    <Select
-      options={options}
-      hasNullOption={false}
-      value={value}
-      onChange={handleChange}
-    />
+const Select = ({ value, onChange }) => {
+  const handleChange = useCallback(
+    e => {
+      onChange(e.target.value);
+    },
+    [onChange],
   );
+  return (
+    <div className={styles.sorter}>
+      <Sort className={styles.leadingIcon} />
+      <select onChange={handleChange} value={value}>
+        {options.map(({ value, label }) => (
+          <option key={value} value={value}>
+            {label}
+          </option>
+        ))}
+      </select>
+      <Caret className={styles.caret} />
+    </div>
+  );
+};
+
+Select.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+};
+
+const Sorter = () => {
+  const [value, setValue] = useState(options[0].value);
+  return <Select value={value} onChange={setValue} />;
 };
 
 export default Sorter;
