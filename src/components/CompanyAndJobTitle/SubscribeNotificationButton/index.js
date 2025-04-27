d722cc51e2c839a69e49aea5b5e6be86fa12c146
YongChenSu <yongchensu0@gmail.com>
2025-04-27 16:58:59 +0800

    feat: add  handleToggleSubscribeCompany and display logic

--- src/components/CompanyAndJobTitle/SubscribeNotificationButton/index.js
+++ src/components/CompanyAndJobTitle/SubscribeNotificationButton/index.js
@@ -1,25 +1,60 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import BellWhiteImage from 'common/icons/bellWhite.svg';
 import BellBlackImage from 'common/icons/bellBlack.svg';
 import PropTypes from 'prop-types';
 import styles from './SubscribeNotificationButton.module.css';
 import cn from 'classnames';
 import { useDispatch } from 'react-redux';
-import { createCompanySubscription } from 'actions/company';
+import { subscribeCompany, unsubscribeCompany } from 'actions/company';
+import useQueryCompanyIsSubscribed from 'components/ExperienceDetail/hooks/useQueryCompanyIsSubscribed';
+import Skeleton from 'react-loading-skeleton';
 
-const SubscribeNotificationButton = ({ hasSubscribed = false, companyId }) => {
+const SubscribeNotificationButton = ({ isFetched, companyName, companyId }) => {
   const dispatch = useDispatch();
+  const [
+    { loading, value },
+    queryCompanyIsSubscribed,
+  ] = useQueryCompanyIsSubscribed(companyName);
+  const [isSubscribed, setIsSubscribed] = useState(false);
 
-  console.log('companyId', companyId);
+  useEffect(() => {
+    if (!loading) {
+      setIsSubscribed(value);
+    }
+  }, [loading, value]);
+
+  const handleSubscribeCompany = useCallback(async () => {
+    setIsSubscribed(true);
+    await dispatch(subscribeCompany({ companyId }));
+  }, [dispatch, companyId]);
+
+  const handleUnsubscribeCompany = useCallback(async () => {
+    setIsSubscribed(false);
+    await dispatch(unsubscribeCompany({ companyId }));
+  }, [dispatch, companyId]);
+
+  const handleToggleSubscribeCompany = useCallback(async () => {
+    if (isSubscribed) {
+      await handleUnsubscribeCompany();
+    } else {
+      await handleSubscribeCompany();
+    }
+  }, [isSubscribed, handleSubscribeCompany, handleUnsubscribeCompany]);
+
+  useEffect(() => {
+    queryCompanyIsSubscribed();
+  }, [queryCompanyIsSubscribed]);
+
+  if (!isFetched || loading) {
+    return <Skeleton width={144} height={30} borderRadius={5} />;
+  }
 
   return (
     <button
       className={cn(styles.buttonContainer, {
-        [styles.subscribed]: hasSubscribed,
+        [styles.subscribed]: isSubscribed,
       })}
-      onClick={() => {
-        dispatch(createCompanySubscription({ companyId }));
-      }}
+      onClick={handleToggleSubscribeCompany}
     >
       <div className={styles.bellContainer}>
         <img
@@ -33,14 +68,15 @@ const SubscribeNotificationButton = ({ hasSubscribed = false, companyId }) => {
           alt="notificationBlackBell"
         />
       </div>
-      <div>{hasSubscribed ? '已訂閱新資料通知' : '有新資料時通知我'}</div>
+      <div>{isSubscribed ? '已訂閱新資料通知' : '有新資料時通知我'}</div>
     </button>
   );
 };
 
 SubscribeNotificationButton.propTypes = {
   companyId: PropTypes.string.isRequired,
-  hasSubscribed: PropTypes.bool,
+  companyName: PropTypes.string.isRequired,
+  isFetched: PropTypes.bool.isRequired,
 };
 
 export default SubscribeNotificationButton;
