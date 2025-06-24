fc27c2a4fe3acc2d2e8120970e0bc4e9980a4b10
Yen-chi Chen <mark86092@gmail.com>
2025-06-24 23:32:12 +0800

    chore: migrate S3 image host to repo (#1561)

--- src/components/CompanyAndJobTitle/TimeAndSalary/Helmet.js
+++ src/components/CompanyAndJobTitle/TimeAndSalary/Helmet.js
@@ -3,12 +3,13 @@ import PropTypes from 'prop-types';
 import ReactHelmet from 'react-helmet';
 import { generatePath } from 'react-router';
 import { formatTitle, formatCanonicalPath } from 'utils/helmetHelper';
-import { IMG_HOST, SITE_NAME } from 'constants/helmetData';
+import { SITE_NAME } from 'constants/helmetData';
 import {
   pageType as PAGE_TYPE,
   tabType as TAB_TYPE,
   tabTypeDetailTranslation as TAB_TYPE_DETAIL_TRANSLATION,
 } from 'constants/companyJobTitle';
+import SalaryWorkTimeOgImage from 'images/og/salary-work-time.jpg';
 
 const CompanySalaryWorkTimeHelmet = ({
   companyName,
@@ -54,10 +55,7 @@ const CompanySalaryWorkTimeHelmet = ({
         content={`${companyName}薪水, ${companyName}薪資, ${companyName}加班狀況, ${companyName}工時`}
       />
       <meta property="og:url" content={url} />
-      <meta
-        property="og:image"
-        content={`${IMG_HOST}/og/time-and-salary.jpg`}
-      />
+      <meta property="og:image" content={SalaryWorkTimeOgImage} />
       <link rel="canonical" href={url} />
     </ReactHelmet>
   );
@@ -105,10 +103,7 @@ const JobTitleSalaryWorkTimeHelmet = ({ jobTitle, page, totalCount }) => {
         content={`${jobTitle}薪水, ${jobTitle}薪資, ${jobTitle}加班狀況, ${jobTitle}工時`}
       />
       <meta property="og:url" content={url} />
-      <meta
-        property="og:image"
-        content={`${IMG_HOST}/og/time-and-salary.jpg`}
-      />
+      <meta property="og:image" content={SalaryWorkTimeOgImage} />
       <link rel="canonical" href={url} />
     </ReactHelmet>
   );
