5de0965fd9ada4a65dff841f4083b76c27996f98
peter.shih <peter.shih@cardinalblue.com>
2025-06-25 14:27:21 +0800

    remove inbox page

--- src/components/App/Header/InboxPage.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import InboxContent from './InboxContent';
-
-const InboxPage = () => {
-  return <InboxContent messages={[]} />;
-};
-
-export default InboxPage;
--- src/routes.js
+++ src/routes.js
@@ -36,7 +36,7 @@ import PlanPage from './components/PlanPage';
 import BuyResultPage from './components/BuyResultPage';
 import CurrentSubscriptionPage from './components/Me/CurrentSubscriptionPage';
 import SubscriptionsPage from './components/Me/SubscriptionsPage';
-import InboxPage from './components/App/Header/InboxPage';
+import InboxContent from './components/App/Header/InboxContent';
 
 import {
   jobTitleOverviewPath,
@@ -275,7 +275,7 @@ const routes = [
   {
     path: '/notifications',
     exact: true,
-    component: InboxPage,
+    component: InboxContent,
     hasFooter: false,
   },
   {
