964b7d0854c7db13efa6dc3373c996ecdec5f6fe
Yen-chi Chen <mark86092@gmail.com>
2025-07-11 13:50:39 +0800

    refactor: 將 company/jobTitle 的 api 改寫成 ts (#1575)

--- src/actions/company.js
+++ src/actions/company.js
@@ -20,20 +20,20 @@ import {
   companyIsSubscribedBoxSelectorByName,
 } from 'selectors/companyAndJobTitle';
 import {
-  queryCompanyOverview as queryCompanyOverviewApi,
   getCompanyTimeAndSalary,
   getCompanyInterviewExperiences,
   getCompanyWorkExperiences,
   queryCompaniesApi,
   getCompanyTimeAndSalaryStatistics,
-  queryCompanyRatingStatisticsApi,
   getCompanyTopNJobTitles,
   getCompanyEsgSalaryData,
-  queryCompanyOverviewStatistics as queryCompanyOverviewStatisticsApi,
   subscribeCompanyApi,
   unsubscribeCompanyApi,
   queryCompanyIsSubscribedApi,
 } from 'apis/company';
+import queryCompanyOverviewApi from 'apis/queryCompanyOverview';
+import queryCompanyRatingStatisticsApi from 'apis/queryCompanyRatingStatistics';
+import queryCompanyOverviewStatisticsApi from 'apis/queryCompanyOverviewStatistics';
 import { tokenSelector } from 'selectors/authSelector';
 import { setExperience } from './experience';
 
