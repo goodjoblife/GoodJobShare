5e4f5b5a9fa6043eade5c64295dfa0e60c148277
Peteranny <peteranny2@gmail.com>
2025-08-01 00:55:42 +0800

    reorder

--- src/pages/InboxPage.js
+++ src/pages/InboxPage.js
@@ -2,12 +2,14 @@ import React, { useCallback } from 'react';
 import { useHistory } from 'react-router';
 
 import { Wrapper } from 'common/base';
-import X from 'common/icons/X';
-import entryStyles from 'components/ShareExperience/Entry.module.css';
-import styles from './InboxPage.module.css';
-import InboxContent from 'components/App/Header/InboxContent';
-import { useIsLoggedIn } from 'hooks/auth';
 import LoginModal from 'common/LoginModal';
+import X from 'common/icons/X';
+
+import { useIsLoggedIn } from 'hooks/auth';
+
+import styles from './InboxPage.module.css';
+import entryStyles from 'components/ShareExperience/Entry.module.css';
+import InboxContent from 'components/App/Header/InboxContent';
 
 const InboxPage = () => {
   const isLoggedIn = useIsLoggedIn();
