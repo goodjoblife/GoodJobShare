d8c7e7ec183c9126be80453f8a8d89383a971073
Peter Shih <peteranny2@gmail.com>
2026-03-17 22:13:36 +0800

    [工作經驗] 新增職等、性別與廠區欄位 (#1626)

--- src/components/ExperienceDetail/Article/InfoBlock.js
+++ src/components/ExperienceDetail/Article/InfoBlock.js
@@ -3,17 +3,24 @@ import PropTypes from 'prop-types';
 import cn from 'classnames';
 import styles from './InfoBlock.module.css';
 
-export const InfoBlocks = ({ children }) => (
-  <div className={styles.infoBlocks}>
-    <table>
-      <tbody>
-        <tr>
-          {React.Children.map(children, child => child && <td>{child}</td>)}
-        </tr>
-      </tbody>
-    </table>
-  </div>
-);
+export const InfoBlocks = ({ children }) => {
+  const tds = React.Children.map(
+    children,
+    child => child && <td>{child}</td>,
+  ).filter(Boolean);
+
+  if (tds.length === 0) return null;
+
+  return (
+    <div className={styles.infoBlocks}>
+      <table>
+        <tbody>
+          <tr>{tds}</tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 InfoBlocks.propTypes = {
   children: PropTypes.node,
@@ -22,12 +29,12 @@ InfoBlocks.propTypes = {
 const InfoBlock = ({ className, label, children }) => (
   <div className={styles.infoBlock}>
     <span className={styles.label}>{label}</span>
-    <span className={cn(styles.value, className)}>{children}</span>
+    <span className={cn(styles.value, className)}>{children || '-'}</span>
   </div>
 );
 
 InfoBlock.propTypes = {
-  children: PropTypes.node.isRequired,
+  children: PropTypes.node,
   className: PropTypes.string,
   label: PropTypes.string,
 };
