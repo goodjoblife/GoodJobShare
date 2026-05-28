f7fbb908c717d786e460fcb22ff7ec97485f944b
Peteranny <peteranny2@gmail.com>
2026-05-29 00:03:42 +0800

    rename: WorkplaceAspectsForm → PolicyForm

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -19,7 +19,7 @@ import TabBar from './TabBar';
 import ShareInterviewModal from '../ShareExperience/InterviewForm/TypeForm';
 import ShareWorkExperienceModal from '../ShareExperience/WorkExperiencesForm/TypeForm';
 import ShareSalaryWorkTimesModal from '../ShareExperience/TimeSalaryForm/TypeForm';
-import ShareWorkplaceAspectsModal from '../ShareExperience/WorkplaceAspectsForm/TypeForm';
+import ShareWorkplaceAspectsModal from '../ShareExperience/PolicyForm/TypeForm';
 import routes from '../../routes';
 
 const useShare = () => {
