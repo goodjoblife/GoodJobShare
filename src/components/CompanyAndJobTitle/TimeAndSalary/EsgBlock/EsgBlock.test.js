3d9c2918619bc27a2fe0cfe23b5e1994b63ef4f7
YongChenSu <yongchensu0@gmail.com>
2026-06-14 17:41:12 +0800

    fix(tests): correct import order for testing utilities in EsgBlock tests [feature/esg]

--- src/components/CompanyAndJobTitle/TimeAndSalary/EsgBlock/EsgBlock.test.js
+++ src/components/CompanyAndJobTitle/TimeAndSalary/EsgBlock/EsgBlock.test.js
@@ -1,6 +1,7 @@
 import '@testing-library/jest-dom';
+import { fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+
 import EsgBlock from './EsgBlock';
 
 const esgSalaryData = {
