6b4be9f1cb4771643928c77901f835486a3f06ac
Peteranny <peteranny2@gmail.com>
2026-08-02 03:08:07 +0800

    refactor: reuse EsgBlock's EsgItemBlock for 管理職女性主管佔比
    Export EsgItemBlock from EsgBlock.js so GenderFriendly can render the
    same ESG stat cell instead of a bespoke FemaleManagerCard.

--- src/components/CompanyAndJobTitle/SalaryWorkTime/EsgBlock/EsgBlock.js
+++ src/components/CompanyAndJobTitle/SalaryWorkTime/EsgBlock/EsgBlock.js
@@ -11,7 +11,7 @@ import { formatNumberWithSign } from 'utils/stringUtil';
 import styles from './EsgBlock.module.css';
 import overviewStyles from '../../Overview/Overview.module.css';
 
-const EsgItemBlock = ({
+export const EsgItemBlock = ({
   className,
   title,
   year,
@@ -56,6 +56,12 @@ EsgItemBlock.propTypes = {
   value: PropTypes.number.isRequired,
   valueCompared: PropTypes.number,
   year: PropTypes.number.isRequired,
+};
+
+EsgItemBlock.defaultProps = {
+  className: undefined,
+  unit: undefined,
+  valueCompared: undefined,
 };
 
 const EsgBlock = ({
