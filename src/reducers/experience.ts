629e38a11c6b696979afb2f81b300d275b054068
Yen-chi Chen <mark86092@gmail.com>
2026-08-06 01:45:15 +0000

    refactor: 將 src/reducers/experience.js 遷移至 TypeScript
    experienceById 重用 apis/experience 的 WorkExperience/InterviewExperience 型別；
    relatedExperiences 與 popularExperiences 因來源 API 尚未轉 TS，先以 unknown 佔位。

---
+++ src/reducers/experience.ts
@@ -0,0 +1,89 @@
+import {
+  SET_EXPERIENCE,
+  SET_POPULAR_EXPERIENCES,
+  SET_RELATED_EXPERIENCES,
+} from 'actions/experience';
+import { InterviewExperience, WorkExperience } from 'apis/experience';
+import createReducer from 'utils/createReducer';
+import FetchBox, { getUnfetched } from 'utils/fetchBox';
+
+// TODO: replace with proper type once apis/experiencesApi.js#queryRelatedExperiences is converted to TS
+type RelatedExperience = unknown;
+
+// TODO: replace with proper type once apis/experiencesApi.js#getPopularExperiences is converted to TS
+type PopularExperience = unknown;
+
+type RelatedExperiencesState = {
+  experienceId: string | null;
+  page: number;
+  state: FetchBox<{
+    relatedExperiences: RelatedExperience[];
+    hasMore: boolean;
+  }>;
+};
+
+type State = {
+  // id --> box
+  experienceById: Record<
+    string,
+    FetchBox<WorkExperience | InterviewExperience | null>
+  >;
+
+  relatedExperiences: RelatedExperiencesState;
+
+  popularExperiences: FetchBox<PopularExperience[]>;
+};
+
+const preloadedState: State = {
+  experienceById: {},
+
+  relatedExperiences: {
+    experienceId: null,
+    page: 0,
+    // state is related to experienceId, page
+    state: getUnfetched(),
+  },
+
+  popularExperiences: getUnfetched(),
+};
+
+export default createReducer(preloadedState, {
+  [SET_EXPERIENCE]: (
+    state,
+    {
+      experienceId,
+      box,
+    }: {
+      experienceId: string;
+      box: FetchBox<WorkExperience | InterviewExperience | null>;
+    },
+  ) => ({
+    ...state,
+    experienceById: {
+      ...state.experienceById,
+      [experienceId]: box,
+    },
+  }),
+  [SET_RELATED_EXPERIENCES]: (
+    state,
+    {
+      relatedExperiences,
+    }: {
+      relatedExperiences: RelatedExperiencesState;
+    },
+  ) => ({
+    ...state,
+    relatedExperiences,
+  }),
+  [SET_POPULAR_EXPERIENCES]: (
+    state,
+    {
+      popularExperiences,
+    }: {
+      popularExperiences: FetchBox<PopularExperience[]>;
+    },
+  ) => ({
+    ...state,
+    popularExperiences,
+  }),
+});
